a91224ee91a8fbd34059d1bd15a84f5d758e42a5
Jose <jose@zeroc.com>
2021-01-28 20:08:17 +0100

    Use TheoryData

--- tests/icerpc.tests/ProxyTest.cs
+++ tests/icerpc.tests/ProxyTest.cs
@@ -40,37 +40,32 @@ namespace IceRPC.Ice.Tests
             Assert.Equal(prx, prx2); // round-trip works
         }
 
-        public class TestProxyParsingData : IEnumerable<object[]>
+        public class TestProxyParsingData : TheoryData<string>
         {
-            public IEnumerator<object[]> GetEnumerator()
+            public TestProxyParsingData()
             {
-                yield return new object[] { "ice+tcp://host.zeroc.com/identity#facet" };
-                yield return new object[] { "ice+tcp://host.zeroc.com:1000/category/name" };
-                yield return new object[] { "ice+tcp://host.zeroc.com:1000/loc0/loc1/category/name" };
-                yield return new object[] { "ice+tcp://host.zeroc.com/category/name%20with%20space" };
-                yield return new object[] { "ice+ws://host.zeroc.com//identity" };
-                yield return new object[] { "ice+ws://host.zeroc.com//identity?invocation-timeout=100ms" };
-                yield return new object[] { "ice+ws://host.zeroc.com//identity?invocation-timeout=1s" };
-                yield return new object[] { "ice+ws://host.zeroc.com//identity?alt-endpoint=host2.zeroc.com" };
-                yield return new object[] { "ice+ws://host.zeroc.com//identity?alt-endpoint=host2.zeroc.com:10000" };
-                yield return new object[] { "ice+tcp://[::1]:10000/identity?alt-endpoint=host1:10000,host2,host3,host4" };
-                yield return new object[] { "ice+tcp://[::1]:10000/identity?alt-endpoint=host1:10000&alt-endpoint=host2,host3&alt-endpoint=[::2]" };
-                yield return new object[] { "ice:location//identity#facet" };
-                yield return new object[] { "ice:location//identity?relative=true#facet" };
-                yield return new object[] { "ice+tcp://host.zeroc.com//identity" };
-                yield return new object[] { "ice+tcp://host.zeroc.com:/identity" }; // another syntax for empty port
-                yield return new object[] { "ice+universal://com.zeroc.ice/identity?transport=iaps&option=a,b%2Cb,c&option=d" };
-                yield return new object[] { "ice+universal://host.zeroc.com/identity?transport=100" };
-                yield return new object[] { "ice+universal://[::ab:cd:ef:00]/identity?transport=bt" }; // leading :: to make the address IPv6-like
-                yield return new object[] { "ice+ws://host.zeroc.com/identity?resource=/foo%2Fbar?/xyz" };
-                yield return new object[] { "ice+universal://host.zeroc.com:10000/identity?transport=tcp" };
-                yield return new object[] { "ice+universal://host.zeroc.com/identity?transport=ws&option=/foo%2520/bar" };
-                yield return new object[] { "ice:tcp -p 10000" }; // a valid URI
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                throw new NotImplementedException();
+                Add("ice+tcp://host.zeroc.com/identity#facet");
+                Add("ice+tcp://host.zeroc.com:1000/category/name");
+                Add("ice+tcp://host.zeroc.com:1000/loc0/loc1/category/name");
+                Add("ice+tcp://host.zeroc.com/category/name%20with%20space");
+                Add("ice+ws://host.zeroc.com//identity");
+                Add("ice+ws://host.zeroc.com//identity?invocation-timeout=100ms");
+                Add("ice+ws://host.zeroc.com//identity?invocation-timeout=1s");
+                Add("ice+ws://host.zeroc.com//identity?alt-endpoint=host2.zeroc.com");
+                Add("ice+ws://host.zeroc.com//identity?alt-endpoint=host2.zeroc.com:10000");
+                Add("ice+tcp://[::1]:10000/identity?alt-endpoint=host1:10000,host2,host3,host4");
+                Add("ice+tcp://[::1]:10000/identity?alt-endpoint=host1:10000&alt-endpoint=host2,host3&alt-endpoint=[::2]");
+                Add("ice:location//identity#facet");
+                Add("ice:location//identity?relative=true#facet");
+                Add("ice+tcp://host.zeroc.com//identity");
+                Add("ice+tcp://host.zeroc.com:/identity"); // another syntax for empty port
+                Add("ice+universal://com.zeroc.ice/identity?transport=iaps&option=a,b%2Cb,c&option=d");
+                Add("ice+universal://host.zeroc.com/identity?transport=100");
+                Add("ice+universal://[::ab:cd:ef:00]/identity?transport=bt"); // leading :: to make the address IPv6-like
+                Add("ice+ws://host.zeroc.com/identity?resource=/foo%2Fbar?/xyz");
+                Add("ice+universal://host.zeroc.com:10000/identity?transport=tcp");
+                Add("ice+universal://host.zeroc.com/identity?transport=ws&option=/foo%2520/bar");
+                Add("ice:tcp -p 10000"); // a valid URI
             }
         }
     }
