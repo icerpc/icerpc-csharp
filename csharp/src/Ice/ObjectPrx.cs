05227043e329a46185297d503e06cde5772fcf46
Jose <jose@zeroc.com>
2021-02-22 22:08:20 +0100

    Remove bogus condition

--- csharp/src/Ice/ObjectPrx.cs
+++ csharp/src/Ice/ObjectPrx.cs
@@ -1555,12 +1555,9 @@ namespace ZeroC.Ice
 
                     observer?.Retried();
 
-                    // If an indirect reference is using a endpoint from the cache, set endpointsMaxAge to force a new
-                    // locator lookup. With Ice1 protocol if using a well-known reference we always set endpointsMaxAge
-                    // upon receinving an OtherReplica exception to force a new lookup this is compatible with previous
-                    // behavior that triggers a clear cache under these circustances.
-                    if ((IsIndirect && endpointsAge != TimeSpan.Zero) ||
-                        (IsWellKnown && Protocol == Protocol.Ice1 && retryPolicy == RetryPolicy.OtherReplica))
+                    // If an indirect proxy is using a endpoint from the cache, set endpointsMaxAge to force a new
+                    // locator lookup.
+                    if (IsIndirect && endpointsAge != TimeSpan.Zero)
                     {
                         endpointsMaxAge = endpointsAge;
                         endpoints = null;
