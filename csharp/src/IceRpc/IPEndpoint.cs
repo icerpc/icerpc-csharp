c7bed2d467c3b5c631f56d3bf21b619b0e46e5a0
Jose <jose@zeroc.com>
2021-03-17 11:55:52 +0100

    Remove unnecessary string format

--- csharp/src/IceRpc/IPEndpoint.cs
+++ csharp/src/IceRpc/IPEndpoint.cs
@@ -176,7 +176,7 @@ namespace IceRpc
             {
                 if (IsIPv6Only)
                 {
-                    sb.Append($"ipv6-only=true");
+                    sb.Append("ipv6-only=true");
                 }
 
                 if (SourceAddress != null)
