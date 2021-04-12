b1fd2be6826194e9875d555e7ae173fe593c216d
Jose <jose@zeroc.com>
2021-04-12 19:55:37 +0200

    cleanup

--- tests/IceRpc.Tests.CodeGeneration/OptionalTests.cs
+++ tests/IceRpc.Tests.CodeGeneration/OptionalTests.cs
@@ -649,7 +649,7 @@ namespace IceRpc.Tests.CodeGeneration
 
             public ValueTask<(ReadOnlyMemory<bool> R1, ReadOnlyMemory<bool> R2)> OpBoolSeqAsync(
                 bool[]? p1, Current current, CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(byte? R1, byte? R2)> OpByteAsync(
                 byte? p1,
                 Current current,
@@ -659,7 +659,7 @@ namespace IceRpc.Tests.CodeGeneration
                 List<byte>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(ReadOnlyMemory<byte> R1, ReadOnlyMemory<byte> R2)> OpByteSeqAsync(
                 byte[]? p1,
                 Current current,
@@ -669,12 +669,12 @@ namespace IceRpc.Tests.CodeGeneration
                 double? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IEnumerable<double>? R1, IEnumerable<double>? R2)> OpDoubleListAsync(
                 List<double>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(ReadOnlyMemory<double> R1, ReadOnlyMemory<double> R2)> OpDoubleSeqAsync(
                 double[]? p1,
                 Current current,
@@ -684,7 +684,7 @@ namespace IceRpc.Tests.CodeGeneration
                 float? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IEnumerable<float>? R1, IEnumerable<float>? R2)> OpFloatListAsync(
                 List<float>? p1,
                 Current current,
@@ -699,18 +699,18 @@ namespace IceRpc.Tests.CodeGeneration
                 int? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IReadOnlyDictionary<int, int>? R1, IReadOnlyDictionary<int, int>? R2)> OpIntDictAsync(
                 Dictionary<int, int>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<IOptionalOperations.OpIntDictMarshaledResultMarshaledReturnValue> OpIntDictMarshaledResultAsync(
                 Dictionary<int, int>? p1,
                 Current current,
                 CancellationToken cancel) =>
                 new(new IOptionalOperations.OpIntDictMarshaledResultMarshaledReturnValue(p1, current));
-            
+
             public ValueTask<(IEnumerable<int>? R1, IEnumerable<int>? R2)> OpIntListAsync(
                 List<int>? p1,
                 Current current,
@@ -720,58 +720,58 @@ namespace IceRpc.Tests.CodeGeneration
                 int[]? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(long? R1, long? R2)> OpLongAsync(
                 long? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IEnumerable<long>? R1, IEnumerable<long>? R2)> OpLongListAsync(
                 List<long>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(ReadOnlyMemory<long> R1, ReadOnlyMemory<long> R2)> OpLongSeqAsync(
                 long[]? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(MyEnum? R1, MyEnum? R2)> OpMyEnumAsync(
                 MyEnum? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(MyStruct? R1, MyStruct? R2)> OpMyStructAsync(
                 MyStruct? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IEnumerable<MyStruct>? R1, IEnumerable<MyStruct>? R2)> OpMyStructListAsync(
                 List<MyStruct>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<IOptionalOperations.OpMyStructMarshaledResultMarshaledReturnValue> OpMyStructMarshaledResultAsync(
                 MyStruct? p1,
                 Current current,
                 CancellationToken cancel) =>
                 new(new IOptionalOperations.OpMyStructMarshaledResultMarshaledReturnValue(p1, current));
-            
+
             public ValueTask<(IEnumerable<MyStruct>? R1, IEnumerable<MyStruct>? R2)> OpMyStructSeqAsync(
                 MyStruct[]? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(short? R1, short? R2)> OpShortAsync(
                 short? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IEnumerable<short>? R1, IEnumerable<short>? R2)> OpShortListAsync(
                 List<short>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(ReadOnlyMemory<short> R1, ReadOnlyMemory<short> R2)> OpShortSeqAsync(
                 short[]? p1,
                 Current current,
@@ -786,17 +786,17 @@ namespace IceRpc.Tests.CodeGeneration
                 Dictionary<string, string>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IEnumerable<string>? R1, IEnumerable<string>? R2)> OpStringListAsync(
                 List<string>? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<(IEnumerable<string>? R1, IEnumerable<string>? R2)> OpStringSeqAsync(
                 string[]? p1,
                 Current current,
                 CancellationToken cancel) => new((p1, p1));
-            
+
             public ValueTask<IOptionalOperations.OpStringSeqMarshaledResultMarshaledReturnValue> OpStringSeqMarshaledResultAsync(
                 string[]? p1,
                 Current current,
