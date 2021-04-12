140dcd391617da8b85b061b42052552c7e3d2539
Jose <pepone@users.noreply.github.com>
2021-04-12 19:57:02 +0200

    Scope tests (#235)

---
+++ tests/IceRpc.Tests.CodeGeneration/ScopeTests.cs
@@ -0,0 +1,289 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IceRpc.Tests.CodeGeneration
+{
+    [Timeout(30000)]
+    [Parallelizable(ParallelScope.All)]
+    public class ScopeTests
+    {
+        private readonly Communicator _communicator;
+        private readonly Server _server;
+        private readonly Scope.IOperationsPrx _prx1;
+        private readonly Scope.Inner.IOperationsPrx _prx2;
+        private readonly Scope.Inner.Inner2.IOperationsPrx _prx3;
+        private readonly Scope.Inner.Test.Inner2.IOperationsPrx _prx4;
+
+        public ScopeTests()
+        {
+            _communicator = new Communicator();
+            _server = new Server()
+            {
+                Communicator = _communicator,
+                ColocationScope = ColocationScope.Communicator
+            };
+            _prx1 = _server.Add("/test1", new Scope.Operations(), Scope.IOperationsPrx.Factory);
+            _prx2 = _server.Add("/test2", new Scope.Inner.Operations(), Scope.Inner.IOperationsPrx.Factory);
+            _prx3 = _server.Add("/test3",
+                                new Scope.Inner.Inner2.Operations(),
+                                Scope.Inner.Inner2.IOperationsPrx.Factory);
+            _prx4 = _server.Add("/test4",
+                                new Scope.Inner.Test.Inner2.Operations(),
+                                Scope.Inner.Test.Inner2.IOperationsPrx.Factory);
+            _ = _server.ListenAndServeAsync();
+
+        }
+
+        [OneTimeTearDown]
+        public async Task TearDownAsync()
+        {
+            await _server.DisposeAsync();
+            await _communicator.DisposeAsync();
+        }
+
+        [Test]
+        public async Task Scope_Operations()
+        {
+            {
+                var s = new Scope.S(0);
+                Assert.AreEqual(s, await _prx1.OpSAsync(new Scope.S(0)));
+
+                var sseq = new Scope.S[] { s };
+                CollectionAssert.AreEqual(sseq, await _prx1.OpSSeqAsync(sseq));
+
+                var smap = new Dictionary<string, Scope.S>() { { "a", s} };
+                CollectionAssert.AreEqual(smap, await _prx1.OpSMapAsync(smap));
+
+                var c = new Scope.C(s);
+                Assert.AreEqual(s, (await _prx1.OpCAsync(c)).S);
+
+                var cseq1 = new Scope.C[] { c };
+                var cseq2 = await _prx1.OpCSeqAsync(cseq1);
+
+                Assert.AreEqual(1, cseq2.Length);
+                Assert.AreEqual(s, cseq2[0].S);
+
+                var cmap1 = new Dictionary<string, Scope.C>() { { "a", c } };
+                var cmap2 = await _prx1.OpCMapAsync(cmap1);
+
+                Assert.AreEqual(1, cmap2.Count);
+                Assert.AreEqual(s, cmap2["a"].S);
+
+                Assert.AreEqual(Scope.E1.v1, await _prx1.OpE1Async(Scope.E1.v1));
+                Assert.AreEqual(new Scope.S1("S1"), await _prx1.OpS1Async(new Scope.S1("S1")));
+                Assert.AreEqual("C1", (await _prx1.OpC1Async(new Scope.C1("C1"))).S);
+            }
+
+            {
+                var s = new Scope.Inner.Inner2.S(0);
+                Assert.AreEqual(s, await _prx2.OpSAsync(s));
+
+                var sseq = new Scope.Inner.Inner2.S[] { s };
+                CollectionAssert.AreEqual(sseq, await _prx2.OpSSeqAsync(sseq));
+
+                var smap = new Dictionary<string, Scope.Inner.Inner2.S>() { { "a", s } };
+                CollectionAssert.AreEqual(smap, await _prx2.OpSMapAsync(smap));
+
+                var c = new Scope.Inner.Inner2.C(s);
+                Assert.AreEqual(s, (await _prx2.OpCAsync(c)).S);
+
+                var cseq1 = new Scope.Inner.Inner2.C[] { c };
+                var cseq2 = await _prx2.OpCSeqAsync(cseq1);
+
+                Assert.AreEqual(1, cseq2.Length);
+                Assert.AreEqual(s, cseq2[0].S);
+
+                var cmap1 = new Dictionary<string, Scope.Inner.Inner2.C>() { { "a", c } };
+                var cmap2 = await _prx2.OpCMapAsync(cmap1);
+
+                Assert.AreEqual(1, cmap2.Count);
+                Assert.AreEqual(s, cmap2["a"].S);
+            }
+
+            {
+                var s = new Scope.Inner.Inner2.S(0);
+                Assert.AreEqual(s, await _prx3.OpSAsync(s));
+
+                var sseq = new Scope.Inner.Inner2.S[] { s };
+                CollectionAssert.AreEqual(sseq, await _prx3.OpSSeqAsync(sseq));
+
+                var smap = new Dictionary<string, Scope.Inner.Inner2.S>() { { "a", s } };
+                CollectionAssert.AreEqual(smap, await _prx3.OpSMapAsync(smap));
+
+                var c = new Scope.Inner.Inner2.C(s);
+                Assert.AreEqual(s, (await _prx3.OpCAsync(c)).S);
+
+                var cseq1 = new Scope.Inner.Inner2.C[] { c };
+                var cseq2 = await _prx3.OpCSeqAsync(cseq1);
+
+                Assert.AreEqual(1, cseq2.Length);
+                Assert.AreEqual(s, cseq2[0].S);
+
+                var cmap1 = new Dictionary<string, Scope.Inner.Inner2.C>() { { "a", c } };
+                var cmap2 = await _prx3.OpCMapAsync(cmap1);
+
+                Assert.AreEqual(1, cmap2.Count);
+                Assert.AreEqual(s, cmap2["a"].S);
+            }
+
+            {
+                var s = new Scope.S(0);
+                Assert.AreEqual(s, await _prx4.OpSAsync(s));
+
+                var sseq = new Scope.S[] { s };
+                CollectionAssert.AreEqual(sseq, await _prx4.OpSSeqAsync(sseq));
+
+                var smap = new Dictionary<string, Scope.S>() { { "a", s } };
+                CollectionAssert.AreEqual(smap, await _prx4.OpSMapAsync(smap));
+
+                var c = new Scope.C(s);
+                Assert.AreEqual(s, (await _prx4.OpCAsync(c)).S);
+
+                var cseq1 = new Scope.C[] { c };
+                var cseq2 = await _prx4.OpCSeqAsync(cseq1);
+
+                Assert.AreEqual(1, cseq2.Length);
+                Assert.AreEqual(s, cseq2[0].S);
+
+                var cmap1 = new Dictionary<string, Scope.C>() { { "a", c } };
+                var cmap2 = await _prx4.OpCMapAsync(cmap1);
+
+                Assert.AreEqual(1, cmap2.Count);
+                Assert.AreEqual(s, cmap2["a"].S);
+            }
+        }
+    }
+
+    namespace Scope
+    {
+        public class Operations : IAsyncOperations
+        {
+            public ValueTask<C1> OpC1Async(C1 c1, Current current, CancellationToken cancel) => new(c1);
+
+            public ValueTask<C> OpCAsync(C p1, Current current, CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, C>> OpCMapAsync(
+                Dictionary<string, C> p1,
+                Current current, CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable<C>> OpCSeqAsync(
+                C[] p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<E1> OpE1Async(E1 e1, Current current, CancellationToken cancel) => new(e1);
+
+            public ValueTask<S1> OpS1Async(S1 s1, Current current, CancellationToken cancel) => new(s1);
+
+            public ValueTask<S> OpSAsync(S p1, Current current, CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, S>> OpSMapAsync(
+                Dictionary<string, S> p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable<S>> OpSSeqAsync(
+                S[] p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+        }
+    }
+
+    namespace Scope.Inner
+    {
+        public class Operations : IAsyncOperations
+        {
+            public ValueTask<Inner2.C> OpCAsync(Inner2.C p1, Current current, CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, Inner2.C>> OpCMapAsync(
+                Dictionary<string, Inner2.C> p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable<Inner2.C>> OpCSeqAsync(
+                Inner2.C[] p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<Inner2.S> OpSAsync(Inner2.S p1, Current current, CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, Inner2.S>> OpSMapAsync(
+                Dictionary<string, Inner2.S> p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable< Inner2.S>> OpSSeqAsync(
+                Inner2.S[] p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+        }
+    }
+
+    namespace Scope.Inner.Inner2
+    {
+        public class Operations : IAsyncOperations
+        {
+            public ValueTask<C> OpCAsync(C p1, Current current, CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, C>> OpCMapAsync(
+                Dictionary<string, C> p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable<C>> OpCSeqAsync(
+                C[] p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<S> OpSAsync(S p1, Current current, CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, S>> OpSMapAsync(
+                Dictionary<string, S> p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable<S>> OpSSeqAsync(
+                S[] p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+        }
+    }
+
+    namespace Scope.Inner.Test.Inner2
+    {
+        public class Operations : IAsyncOperations
+        {
+            public ValueTask<Scope.C> OpCAsync(
+                Scope.C p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, Scope.C>> OpCMapAsync(
+                Dictionary<string, Scope.C> p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable<Scope.C>> OpCSeqAsync(
+                Scope.C[] c1,
+                Current current,
+                CancellationToken cancel) => new(c1);
+
+            public ValueTask<Scope.S> OpSAsync(
+                Scope.S p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IReadOnlyDictionary<string, Scope.S>> OpSMapAsync(
+                Dictionary<string, Scope.S> p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+
+            public ValueTask<IEnumerable<Scope.S>> OpSSeqAsync(
+                Scope.S[] p1,
+                Current current,
+                CancellationToken cancel) => new(p1);
+        }
+    }
+}
---
+++ tests/IceRpc.Tests.CodeGeneration/ScopeTests.ice
@@ -0,0 +1,157 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+#pragma once
+
+[[suppress-warning(reserved-identifier)]]
+
+// Ensure that using the same type names in different modules, doesn't cause any conflicts and generates correct code.
+module IceRpc::Tests::CodeGeneration::Scope
+{
+    struct S
+    {
+        int v;
+    }
+
+    dictionary<string, S> SMap;
+    sequence<S> SSeq;
+
+    class C
+    {
+        S s;
+    }
+
+    dictionary<string, C> CMap;
+    sequence<C> CSeq;
+
+    enum E1
+    {
+        v1,
+        v2,
+        v3
+    }
+
+    struct S1
+    {
+        string s;
+    }
+
+    class C1
+    {
+        string s;
+    }
+
+    // Ensure that struct data members can use a type name as a data member name.
+    struct S2
+    {
+        E1 E1;
+        S1 S1;
+        C1 C1;
+    }
+
+    // Ensure that class data members can use a type name as a data member name.
+    class C2
+    {
+        E1 E1;
+        S1 S1;
+        C1 C1;
+    }
+
+    interface Operations
+    {
+        S opS(S p1);
+        SSeq opSSeq(SSeq p1);
+        SMap opSMap(SMap p1);
+
+        C opC(C p1);
+        CSeq opCSeq(CSeq p1);
+        CMap opCMap(CMap p1);
+
+        // Ensure that a type name can be used as a parameter name
+        E1 opE1(E1 E1);
+        S1 opS1(S1 S1);
+        C1 opC1(C1 C1);
+    }
+
+    dictionary<string, Operations> OperationsMap;
+    sequence<Operations> OperationsSeq;
+
+    module Inner
+    {
+        struct S
+        {
+            int v;
+        }
+
+        module Inner2
+        {
+            struct S
+            {
+                int v;
+            }
+
+            dictionary<string, S> SMap;
+            sequence<S> SSeq;
+
+            class C
+            {
+                S s;
+            }
+
+            dictionary<string, C> CMap;
+            sequence<C> CSeq;
+
+            interface Operations
+            {
+                S opS(S p1);
+                SSeq opSSeq(SSeq p1);
+                SMap opSMap(SMap p1);
+
+                C opC(C c1);
+                CSeq opCSeq(CSeq p1);
+                CMap opCMap(CMap p1);
+            }
+
+            dictionary<string, Operations> OperationsMap;
+            sequence<Operations> OperationsSeq;
+        }
+
+        class C
+        {
+            S s;
+        }
+
+        sequence<Inner2::S> SSeq;
+        dictionary<string, Inner2::S> SMap;
+
+        dictionary<string, Inner2::C> CMap;
+        sequence<Inner2::C> CSeq;
+
+        interface Operations
+        {
+            Inner2::S opS(Inner2::S p1);
+            Inner2::SSeq opSSeq(Inner2::SSeq p1);
+            Inner2::SMap opSMap(Inner2::SMap p1);
+
+            Inner2::C opC(Inner2::C p1);
+            Inner2::CSeq opCSeq(Inner2::CSeq p1);
+            Inner2::CMap opCMap(Inner2::CMap p1);
+        }
+
+        dictionary<string, Operations> OperationsMap;
+        sequence<Operations> OperationsSeq;
+    }
+}
+
+module IceRpc::Tests::CodeGeneration::Scope::Inner::Test::Inner2
+{
+    interface Operations
+    {
+        IceRpc::Tests::CodeGeneration::Scope::S opS(IceRpc::Tests::CodeGeneration::Scope::S p1);
+        IceRpc::Tests::CodeGeneration::Scope::SSeq opSSeq(IceRpc::Tests::CodeGeneration::Scope::SSeq p1);
+        IceRpc::Tests::CodeGeneration::Scope::SMap opSMap(IceRpc::Tests::CodeGeneration::Scope::SMap p1);
+
+        IceRpc::Tests::CodeGeneration::Scope::C opC(IceRpc::Tests::CodeGeneration::Scope::C p1);
+        IceRpc::Tests::CodeGeneration::Scope::CSeq opCSeq(IceRpc::Tests::CodeGeneration::Scope::CSeq c1);
+        IceRpc::Tests::CodeGeneration::Scope::CMap opCMap(IceRpc::Tests::CodeGeneration::Scope::CMap p1);
+    }
+}
