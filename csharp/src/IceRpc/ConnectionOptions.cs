5c2d9e0e1ef43619825abc75a61b05982c67f1b2
Jose <pepone@users.noreply.github.com>
2021-04-19 16:48:35 +0200

    Update csharp/src/IceRpc/ConnectionOptions.cs
    Co-authored-by: Joe George <joe@externl.com>

--- csharp/src/IceRpc/ConnectionOptions.cs
+++ csharp/src/IceRpc/ConnectionOptions.cs
@@ -151,7 +151,7 @@ namespace IceRpc
 
         /// <summary>Configures the maximum depth for a graph of Slice class instances to unmarshal. When the limit is reached,
         /// the IceRpc run time throws <see cref="InvalidDataException"/>.</summary>
-        /// <value>The maximum depth for a graph of Slice class instances to unmarshal</value>
+        /// <value>The maximum depth for a graph of Slice class instances to unmarshal.</value>
         public int ClassGraphMaxDepth
         {
             get => _classGraphMaxDepth;
