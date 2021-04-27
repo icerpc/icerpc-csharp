8558f777529b152d97c21f0745a32f1f2008b078
Jose <jose@zeroc.com>
2021-04-27 17:43:15 +0200

    Tracing test fix: wait for server shutdown to ensure dispatch activity has stop

--- tests/IceRpc.Tests.ClientServer/TracingTests.cs
+++ tests/IceRpc.Tests.ClientServer/TracingTests.cs
@@ -118,6 +118,8 @@ namespace IceRpc.Tests.ClientServer
             server2.Listen();
             prx = server2.CreateProxy<IGreeterTestServicePrx>("/");
             await prx.IcePingAsync();
+            // Await the server shutdown to ensure the dispatch has finish
+            await server2.ShutdownAsync();
             Assert.IsNotNull(dispatchActivity);
             Assert.AreEqual("IceRpc.Dispatch", dispatchActivity.DisplayName);
             Assert.AreEqual(1, dispatchStartedActivities.Count);
@@ -187,7 +189,8 @@ namespace IceRpc.Tests.ClientServer
                         return await next(target, request, cancel);
                     });
             await prx.IcePingAsync();
-
+            // Await the server shutdown to ensure the dispatch has finish
+            await server.ShutdownAsync();
             Assert.IsNotNull(invocationActivity);
             Assert.AreEqual(testActivity, invocationActivity.Parent);
             Assert.IsNotNull(dispatchActivity);
