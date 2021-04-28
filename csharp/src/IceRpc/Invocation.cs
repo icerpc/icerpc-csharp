bb57b55fc492187ce5acc6fb2dd130b23f42c484
Bernard Normier <bernard@zeroc.com>
2021-04-28 11:35:32 -0400

    Add missing file

---
+++ csharp/src/IceRpc/Invocation.cs
@@ -0,0 +1,18 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+
+namespace IceRpc
+{
+    /// <summary>Holds properties to customize a new request and to get back information from the corresponding
+    /// response.</summary>
+    public sealed class Invocation
+    {
+        /// <summary>The context dictionary carried by the request.</summary>
+        public SortedDictionary<string, string> Context { get; set; } = new();
+
+        /// <summary>The progress provider.</summary>
+        public IProgress<bool>? Progress { get; set; }
+    }
+}
