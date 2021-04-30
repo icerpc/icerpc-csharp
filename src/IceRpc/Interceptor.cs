b4bad6f52dbc1a0603c6e6ce167a9f457333a2c7
Jose <jose@zeroc.com>
2021-04-30 20:41:46 +0200

    missing file

---
+++ src/IceRpc/Interceptor.cs
@@ -0,0 +1,47 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using System;
+
+namespace IceRpc
+{
+    public static class Interceptor
+    {
+        /// <summary>Creates a interceptor that publish invocation metrics using a <see cref="InvocationEventSource"/>.
+        /// </summary>
+        /// <param name="eventSource">The event source used to publish the metrics events.</param>
+        public static Func<IInvoker, IInvoker> CreateMetricsPublisher(InvocationEventSource eventSource) =>
+            next => new InlineInvoker(
+                async (request, cancel) =>
+                {
+                    eventSource.RequestStart(request);
+                    try
+                    {
+                        var response = await next.InvokeAsync(request, cancel).ConfigureAwait(false);
+                        if (response.ResultType == ResultType.Failure)
+                        {
+                            eventSource.RequestFailed(request, "IceRpc.RemoteException");
+                        }
+                        return response;
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        eventSource.RequestCanceled(request);
+                        throw;
+                    }
+                    catch (Exception ex)
+                    {
+                        eventSource.RequestFailed(request, ex);
+                        throw;
+                    }
+                    finally
+                    {
+                        eventSource.RequestStop(request);
+                    }
+                });
+
+        /// <summary>A interceptor that publish invocation metrics, using the default
+        /// <see cref="InvocationEventSource.Log"/> instance.</summary>
+        public static Func<IInvoker, IInvoker> MetricsPublisher { get; } =
+            CreateMetricsPublisher(InvocationEventSource.Log);
+    }
+}
