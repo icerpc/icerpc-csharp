b7e05f87bc22d6dee68c43ba4191afe848fad289
Jose <jose@zeroc.com>
2021-05-14 16:51:34 +0200

    Add test for bidir request dispatch

--- tests/IceRpc.Tests.Api/ConnectionTests.cs
+++ tests/IceRpc.Tests.Api/ConnectionTests.cs
@@ -2,6 +2,8 @@
 
 using NUnit.Framework;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace IceRpc.Tests.Api
 {
@@ -63,6 +65,51 @@ namespace IceRpc.Tests.Api
             var options = new UdpOptions();
             Assert.Throws<ArgumentException>(() => options.SendBufferSize = 512);
             Assert.Throws<ArgumentException>(() => options.ReceiveBufferSize = 512);
+        }
+
+        [Test]
+        public async Task Connection_DispatcherAsync()
+        {
+            var router = new Router();
+            router.Use(next => new InlineDispatcher(async (request, cancel) =>
+            {
+                await IGreeterServicePrx.FromConnection(request.Connection).IcePingAsync(cancel: cancel);
+                return new OutgoingResponse(request, Payload.FromVoidReturnValue(request));
+            }));
+            router.Map<IGreeterService>(new GreeterService());
+
+            await using var server = new Server
+            {
+                Endpoint = "ice+coloc://connection_dispatcher:1000",
+                Dispatcher = router
+            };
+            server.Listen();
+
+            router = new Router();
+            IncomingRequest? bidirRequest = null;
+            router.Use(next => new InlineDispatcher((request, cancel) =>
+            {
+                bidirRequest = request;
+                return new(new OutgoingResponse(request, Payload.FromVoidReturnValue(request)));
+            }));
+            router.Map<IGreeterService>(new GreeterService());
+
+            await using var connection = new Connection
+            {
+                RemoteEndpoint = server.ProxyEndpoint,
+                Dispatcher = router
+            };
+
+            var prx = IGreeterServicePrx.FromConnection(connection);
+            await prx.IcePingAsync();
+            Assert.IsNotNull(bidirRequest);
+            Assert.AreEqual("ice_ping", bidirRequest!.Operation);
+        }
+
+        public class GreeterService : IGreeterService
+        {
+            public ValueTask SayHelloAsync(Dispatch dispatch, CancellationToken cancel) =>
+                default;
         }
     }
 }
