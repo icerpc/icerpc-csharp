3d9daee8c0858bd1ded0bf7309420b56334cd592
Jose <jose@zeroc.com>
2021-06-29 15:01:19 +0200

    minor fix

--- src/IceRpc/IConnectionProvider.cs
+++ src/IceRpc/IConnectionProvider.cs
@@ -9,14 +9,14 @@ namespace IceRpc
     /// <summary>Provides connections to the <see cref="Interceptors.Binder"/> interceptor.</summary>
     public interface IConnectionProvider
     {
-        /// <summary>Retrieves or creates a connection.</summary>
+        /// <summary>Retrieves or creates an active connection.</summary>
         /// <param name="endpoint">The main endpoint.</param>
         /// <param name="altEndpoints">A list of zero or more alternative endpoints.</param>
         /// <param name="cancel">The cancellation token.</param>
         /// <returns>The connection.</returns>
         /// <exception cref="NoEndpointException">Thrown when none of the endpoint is usable.</exception>
-        // TODO: more exceptions
-        // TODO: does it need to provide an active connection?
+        /// <exception cref="ConnectFailedException">Thrown when there is a failed attempt to create a connection.
+        /// </exception>
         ValueTask<Connection> GetConnectionAsync(
             Endpoint endpoint,
             IEnumerable<Endpoint> altEndpoints,
