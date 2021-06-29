f54a69c0c8c60f79885d4695bb2181a01fab5555
Jose <jose@zeroc.com>
2021-06-29 21:50:10 +0200

    update comments

--- src/IceRpc/IConnectionProvider.cs
+++ src/IceRpc/IConnectionProvider.cs
@@ -1,5 +1,6 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,13 +10,17 @@ namespace IceRpc
     /// <summary>Provides connections to the <see cref="Interceptors.Binder"/> interceptor.</summary>
     public interface IConnectionProvider
     {
-        /// <summary>Retrieves or creates an active connection.</summary>
+        /// <summary>Retrieves or creates connection.</summary>
         /// <param name="endpoint">The main endpoint.</param>
         /// <param name="altEndpoints">A list of zero or more alternative endpoints.</param>
         /// <param name="cancel">The cancellation token.</param>
         /// <returns>The connection.</returns>
         /// <exception cref="NoEndpointException">Thrown when none of the endpoint is usable.</exception>
-        /// <exception cref="ConnectFailedException">Thrown when there is a failed attempt to create a connection.
+        /// <exception cref="ConnectFailedException">Thrown when there is a single usable endpoint and this method
+        /// failed to establish a connection to it.</exception>
+        /// <exception cref="AggregateException">Throw when there are multiple endpoints, and this method failed
+        /// to establish a connection to any of them. The <see cref="AggregateException"/> wraps the
+        /// <see cref="ConnectFailedException"/> thrown when attempting to connect each of the usable endpoints.
         /// </exception>
         ValueTask<Connection> GetConnectionAsync(
             Endpoint endpoint,
