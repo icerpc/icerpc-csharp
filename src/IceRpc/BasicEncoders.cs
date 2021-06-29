79a5bcef030b79f2f3feff1726ba716d0a82db27
Bernard Normier <bernard@zeroc.com>
2021-06-29 16:52:29 -0400

    Cleanup

--- src/IceRpc/BasicEncoders.cs
+++ src/IceRpc/BasicEncoders.cs
@@ -1,86 +1,69 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using System.ComponentModel;
-
 namespace IceRpc
 {
-    /// <summary>Writes data into a byte buffer using the Ice encoding.</summary>
+    /// <summary>Provides encoders for all the basic types.</summary>
     public static class BasicEncoders
     {
         // Cached Encoder static objects used by the generated code
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>bool</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>bool</c> values.</summary>
         public static readonly Encoder<bool> BoolEncoder =
             (writer, value) => writer.WriteBool(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>byte</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>byte</c> values.</summary>
         public static readonly Encoder<byte> ByteEncoder =
             (writer, value) => writer.WriteByte(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>double</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>double</c> values.</summary>
         public static readonly Encoder<double> DoubleEncoder =
             (writer, value) => writer.WriteDouble(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>float</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>float</c> values.</summary>
         public static readonly Encoder<float> FloatEncoder =
             (writer, value) => writer.WriteFloat(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>int</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>int</c> values.</summary>
         public static readonly Encoder<int> IntEncoder =
             (writer, value) => writer.WriteInt(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>long</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>long</c> values.</summary>
         public static readonly Encoder<long> LongEncoder =
             (writer, value) => writer.WriteLong(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>short</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>short</c> values.</summary>
         public static readonly Encoder<short> ShortEncoder =
             (writer, value) => writer.WriteShort(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>string</c> instances.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>string</c> instances.</summary>
         public static readonly Encoder<string> StringEncoder =
             (writer, value) => writer.WriteString(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>uint</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>uint</c> values.</summary>
         public static readonly Encoder<uint> UIntEncoder =
             (writer, value) => writer.WriteUInt(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>ulong</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>ulong</c> values.</summary>
         public static readonly Encoder<ulong> ULongEncoder =
             (writer, value) => writer.WriteULong(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write <c>ushort</c> values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for <c>ushort</c> values.</summary>
         public static readonly Encoder<ushort> UShortEncoder =
             (writer, value) => writer.WriteUShort(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write var int values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for var int values.</summary>
         public static readonly Encoder<int> VarIntEncoder =
             (writer, value) => writer.WriteVarInt(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write var long values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for var long values.</summary>
         public static readonly Encoder<long> VarLongEncoder =
             (writer, value) => writer.WriteVarLong(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write var uint values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for var uint values.</summary>
         public static readonly Encoder<uint> VarUIntEncoder =
             (writer, value) => writer.WriteVarUInt(value);
 
-        /// <summary>A <see cref="Encoder{T}"/> used to write var ulong values.</summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>An <see cref="Encoder{T}"/> for var ulong values.</summary>
         public static readonly Encoder<ulong> VarULongEncoder =
             (writer, value) => writer.WriteVarULong(value);
     }
