3bf2db90ec4f86a0a9e75580b1193d47b38e8875
Bernard Normier <bernard@zeroc.com>
2021-07-27 16:01:39 -0400

    More cleanup

--- src/IceRpc/Endpoint.cs
+++ src/IceRpc/Endpoint.cs
@@ -5,7 +5,6 @@ using IceRpc.Transports.Internal;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -120,103 +119,6 @@ namespace IceRpc
         }
     }
 
-    /// <summary>This class provides extension methods for <see cref="Endpoint"/>.</summary>
-    public static class EndpointExtensions
-    {
-        /// <summary>Appends the endpoint and all its options (if any) to this string builder, when using the URI
-        /// format.</summary>
-        /// <param name="sb">The string builder.</param>
-        /// <param name="endpoint">The endpoint to append.</param>
-        /// <param name="path">The path of the endpoint URI. Use this parameter to start building a proxy URI.</param>
-        /// <param name="includeScheme">When true, first appends the endpoint's scheme followed by ://.</param>
-        /// <param name="optionSeparator">The character that separates options in the query component of the URI.
-        /// </param>
-        /// <returns>The string builder parameter.</returns>
-        internal static StringBuilder AppendEndpoint(
-            this StringBuilder sb,
-            Endpoint endpoint,
-            string path = "",
-            bool includeScheme = true,
-            char optionSeparator = '&')
-        {
-            Debug.Assert(endpoint.Protocol != Protocol.Ice1); // we never generate URIs for the ice1 protocol
-
-            if (includeScheme)
-            {
-                sb.Append("ice+");
-                sb.Append(endpoint.Transport);
-                sb.Append("://");
-            }
-
-            if (endpoint.Host.Contains(':', StringComparison.Ordinal))
-            {
-                sb.Append('[');
-                sb.Append(endpoint.Host);
-                sb.Append(']');
-            }
-            else
-            {
-                sb.Append(endpoint.Host);
-            }
-
-            if (endpoint.Port != IceUriParser.DefaultPort)
-            {
-                sb.Append(':');
-                sb.Append(endpoint.Port.ToString(CultureInfo.InvariantCulture));
-            }
-
-            if (path.Length > 0)
-            {
-                sb.Append(path);
-            }
-
-            bool firstOption = true;
-
-            if (endpoint.Protocol != Protocol.Ice2)
-            {
-                AppendQueryOption();
-                sb.Append("protocol=");
-                sb.Append(endpoint.Protocol.GetName());
-            }
-            foreach ((string name, string value) in endpoint.ExternalParams)
-            {
-                AppendQueryOption();
-                sb.Append(name);
-                sb.Append('=');
-                sb.Append(value);
-            }
-            foreach ((string name, string value) in endpoint.LocalParams)
-            {
-                AppendQueryOption();
-                sb.Append(name);
-                sb.Append('=');
-                sb.Append(value);
-            }
-            return sb;
-
-            void AppendQueryOption()
-            {
-                if (firstOption)
-                {
-                    sb.Append('?');
-                    firstOption = false;
-                }
-                else
-                {
-                    sb.Append(optionSeparator);
-                }
-            }
-        }
-
-        /// <summary>Converts this endpoint into an endpoint data. This method is called when encoding an endpoint.
-        /// </summary>
-        /// <param name="endpoint">The endpoint.</param>
-        /// <returns>An endpoint data with all the properties of this endpoint except
-        /// <see cref="Endpoint.LocalParams"/>.</returns>
-        public static EndpointData ToEndpointData(this Endpoint endpoint) =>
-            new(endpoint.Protocol, endpoint.Transport, endpoint.Host, endpoint.Port, endpoint.ExternalParams);
-    }
-
     /// <summary>Equality comparer for <see cref="Endpoint"/>.</summary>
     public abstract class EndpointComparer : EqualityComparer<Endpoint>
     {
@@ -246,28 +148,12 @@ namespace IceRpc
         }
     }
 
-    /// <summary>This class provides extension methods for <see cref="EndpointData"/>.</summary>
-    public static class EndpointDataExtensions
-    {
-        /// <summary>Converts an endpoint data into an endpoint. This method is used when decoding an endpoint.
-        /// </summary>
-        /// <param name="endpointData">The endpoint data struct.</param>
-        /// <returns>The new endpoint.</returns>
-        public static Endpoint ToEndpoint(this in EndpointData endpointData) =>
-            new(endpointData.Protocol,
-                string.IsInterned(endpointData.Transport) ?? endpointData.Transport,
-                endpointData.Host,
-                endpointData.Port,
-                endpointData.Params.ToImmutableList(),
-                ImmutableList<EndpointParam>.Empty);
-    }
-
     // The main EndpointParam is generated by the Slice compiler.
     public partial struct EndpointParam
     {
         /// <summary>Deconstructs an endpoint parameter.</summary>
         /// <param name="name">The deconstructed name.</param>
-        /// <param name="name">The deconstructed value.</param>
+        /// <param name="value">The deconstructed value.</param>
         public void Deconstruct(out string name, out string value)
         {
             name = Name;
