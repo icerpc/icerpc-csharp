2542e15247d662f0f18a98373a106ef5323bd6ae
Bernard Normier <bernard@zeroc.com>
2021-07-29 10:03:08 -0400

    Add file

---
+++ src/IceRpc/EncodingNames.cs
@@ -0,0 +1,11 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+namespace IceRpc
+{
+    /// <summary>Encoding names used by the IceRpc assembly.</summary>
+    public static class EncodingNames
+    {
+        public const string V11 = "1.1";
+        public const string V20 = "2.0";
+    }
+}
