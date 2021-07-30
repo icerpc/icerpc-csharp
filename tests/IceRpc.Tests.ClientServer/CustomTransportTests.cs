4ca72db4c95ca258ed64268176bb7ab6f9c905d3
Jose <jose@zeroc.com>
2021-07-30 12:14:16 +0200

    Additional test for custom endpoint options

--- tests/IceRpc.Tests.ClientServer/CustomTransportTests.cs
+++ tests/IceRpc.Tests.ClientServer/CustomTransportTests.cs
@@ -3,7 +3,9 @@
 using IceRpc.Transports;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace IceRpc.Tests.ClientServer
 {
@@ -14,8 +16,20 @@ namespace IceRpc.Tests.ClientServer
         public MultiStreamConnection CreateConnection(
             Endpoint remoteEndpoint,
             ClientConnectionOptions connectionOptions,
-            ILoggerFactory loggerFactory) =>
-            _transport.CreateConnection(remoteEndpoint, connectionOptions, loggerFactory);
+            ILoggerFactory loggerFactory)
+        {
+            if (remoteEndpoint.Transport == "custom")
+            {
+                Endpoint newEndpoint = remoteEndpoint with {
+                    Params = remoteEndpoint.Params.RemoveAll(
+                        p => p.Name.StartsWith("custom-", StringComparison.Ordinal)) };
+                return _transport.CreateConnection(newEndpoint, connectionOptions, loggerFactory);
+            }
+            else
+            {
+                throw new UnknownTransportException($"Unknown transport {remoteEndpoint.Transport}");
+            }
+        }
     }
 
     public class CustomServerTransport : IServerTransport
@@ -25,7 +39,19 @@ namespace IceRpc.Tests.ClientServer
         public (IListener? Listener, MultiStreamConnection? Connection) Listen(
             Endpoint endpoint,
             ServerConnectionOptions connectionOptions,
-            ILoggerFactory loggerFactory) => _transport.Listen(endpoint, connectionOptions, loggerFactory);
+            ILoggerFactory loggerFactory)
+        {
+            if (endpoint.Transport == "custom")
+            {
+                Endpoint newEndpoint = endpoint with {
+                    Params = endpoint.Params.RemoveAll(p => p.Name.StartsWith("custom-", StringComparison.Ordinal)) };
+                return _transport.Listen(newEndpoint, connectionOptions, loggerFactory);
+            }
+            else
+            {
+                throw new UnknownTransportException($"Unknown transport {endpoint.Transport}");
+            }
+        }
     }
 
     public class CustomTransportTests
@@ -50,6 +76,56 @@ namespace IceRpc.Tests.ClientServer
 
             var prx = ServicePrx.FromConnection(connection);
             await prx.IcePingAsync();
+        }
+
+        [Test]
+        public async Task CustomTransport_UnknownEndpointParameterAsync()
+        {
+            // Using an unknown parameter with tcp transport results in FormatException
+            {
+                await using var server = new Server
+                {
+                    ServerTransport = new TcpServerTransport(),
+                    Endpoint = "ice+tcp://127.0.0.1:0?tls=false&custom-p=bar",
+                    Dispatcher = new MyService()
+                };
+                Assert.Throws<FormatException>(() => server.Listen());
+            }
+
+            // Custom transport handles any params that start with custom-
+            {
+                await using var server = new Server
+                {
+                    ServerTransport = new CustomServerTransport(),
+                    Endpoint = "ice+custom://127.0.0.1:0?tls=false&custom-p=bar",
+                    Dispatcher = new MyService()
+                };
+                server.Listen();
+
+                await using var connection1 = new Connection
+                {
+                    ClientTransport = new CustomClientTransport(),
+                    // We add the custom endpoint here because listen updates the endpoint and the custom transport
+                    // removes the parameter
+                    RemoteEndpoint = server.Endpoint with {
+                        Params = server.Endpoint.Params.Add(new EndpointParam("custom-p", "bar"))}
+                };
+
+                var prx = ServicePrx.FromConnection(connection1);
+                await prx.IcePingAsync();
+
+                await using var connection2 = new Connection
+                {
+                    ClientTransport = new TcpClientTransport(),
+                    // We add the custom endpoint here because listen updates the endpoint and the custom transport
+                    // removes the parameter
+                    RemoteEndpoint = server.Endpoint with {
+                        Params = server.Endpoint.Params.Add(new EndpointParam("custom-p", "bar"))}
+                };
+
+                prx = ServicePrx.FromConnection(connection2);
+                Assert.ThrowsAsync<FormatException>(async () => await prx.IcePingAsync());
+            }
         }
 
         public class MyService : Service, IService
