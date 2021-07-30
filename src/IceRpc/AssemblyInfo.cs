a2d7d40878a6c15ba400929f8f585e796588475a
Jose <jose@zeroc.com>
2021-07-30 13:02:57 +0200

    Update Runtime doc comments

---
+++ src/IceRpc/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using System.Runtime.CompilerServices;
+
+// Make internals visible to the tests assembly, to allow writing unit tests for the internal classes
+[assembly: InternalsVisibleTo("IceRpc.Tests.Internal")]
+[assembly: InternalsVisibleTo("IceRpc.Tests.Encoding")]
--- src/IceRpc/Runtime.cs
+++ src/IceRpc/Runtime.cs
@@ -1,14 +1,8 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using System.Runtime.CompilerServices;
-
-// Make internals visible to the tests assembly, to allow writing unit tests for the internal classes
-[assembly: InternalsVisibleTo("IceRpc.Tests.Internal")]
-[assembly: InternalsVisibleTo("IceRpc.Tests.Encoding")]
-
 namespace IceRpc
 {
-    /// <summary>Provides global configuration for IceRPC in the current process.</summary>
+    /// <summary>Provides information about the IceRPC runtime used in the current process.</summary>
     public static class Runtime
     {
         /// <summary>The IceRPC version in semver format.</summary>
