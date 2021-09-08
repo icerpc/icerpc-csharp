0746e2f166dbdb99e8cb8950374c0898bec66ade
Bernard Normier <bernard@zeroc.com>
2021-09-08 16:53:45 -0400

    Convert Position to record struct

--- src/IceRpc/Slice/Internal/BufferWriter.cs
+++ src/IceRpc/Slice/Internal/BufferWriter.cs
@@ -10,7 +10,7 @@ namespace IceRpc.Slice.Internal
     {
         /// <summary>Represents a position in the underlying buffer vector. This position consists of the index of the
         /// buffer in the vector and the offset into that buffer.</summary>
-        internal struct Position : IEquatable<Position>
+        internal record struct Position
         {
             /// <summary>The zero based index of the buffer.</summary>
             internal int Buffer;
@@ -21,26 +21,11 @@ namespace IceRpc.Slice.Internal
             /// <summary>Creates a new position from the buffer and offset values.</summary>
             /// <param name="buffer">The zero based index of the buffer in the buffer vector.</param>
             /// <param name="offset">The offset into the buffer.</param>
-            public Position(int buffer, int offset)
+            internal Position(int buffer, int offset)
             {
                 Buffer = buffer;
                 Offset = offset;
             }
-
-            /// <inheritdoc/>
-            public bool Equals(Position other) => Buffer == other.Buffer && Offset == other.Offset;
-
-            /// <inheritdoc/>
-            public override bool Equals(object? obj) => obj is Position value && Equals(value);
-
-            /// <inheritdoc/>
-            public override int GetHashCode() => HashCode.Combine(Buffer, Offset);
-
-            /// <summary>The equality operator == returns true if its operands are equal, false otherwise.</summary>
-            public static bool operator ==(Position lhs, Position rhs) => lhs.Equals(rhs);
-
-            /// <summary>The inequality operator != returns true if its operands are not equal, false otherwise.</summary>
-            public static bool operator !=(Position lhs, Position rhs) => !(lhs == rhs);
         }
 
         /// <summary>The number of bytes that the underlying buffer vector can hold without further allocation.
