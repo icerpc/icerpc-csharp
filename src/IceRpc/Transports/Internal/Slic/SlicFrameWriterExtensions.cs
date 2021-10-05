f35de2f0aab767435764cd15b5943191b0e8ee4a
Benoit Foucher <benoit@zeroc.com>
2021-10-05 17:48:06 +0200

    Missing files

---
+++ src/IceRpc/Transports/Internal/Slic/SlicFrameWriterExtensions.cs
@@ -0,0 +1,79 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using IceRpc.Slice;
+using IceRpc.Slice.Internal;
+using IceRpc.Transports.Slic;
+using System.Buffers;
+
+namespace IceRpc.Transports.Internal.Slic
+{
+    internal static class SlicFrameWriterExtensions
+    {
+        internal static ValueTask WriteInitializeAsync(
+            this ISlicFrameWriter writer,
+            uint version,
+            InitializeBody frame,
+            CancellationToken cancel) =>
+            WriteFrameAsync(
+                writer,
+                FrameType.Initialize,
+                stream: null,
+                encoder =>
+                {
+                    encoder.EncodeVarUInt(version);
+                    frame.Encode(encoder);
+                },
+                cancel);
+
+        internal static ValueTask WriteInitializeAckAsync(
+            this ISlicFrameWriter writer,
+            InitializeAckBody frame,
+            CancellationToken cancel) => WriteFrameAsync(writer, FrameType.InitializeAck, null, frame.Encode, cancel);
+
+        internal static ValueTask WriteVersionAsync(
+            this ISlicFrameWriter writer,
+            VersionBody frame,
+            CancellationToken cancel) => WriteFrameAsync(writer, FrameType.Version, null, frame.Encode, cancel);
+
+        internal static ValueTask WriteStreamResetAsync(
+            this ISlicFrameWriter writer,
+            SlicStream stream,
+            StreamResetBody frame,
+            CancellationToken cancel) =>
+            WriteFrameAsync(writer, FrameType.StreamReset, stream, frame.Encode, cancel);
+
+        internal static ValueTask WriteStreamConsumedAsync(
+            this ISlicFrameWriter writer,
+            SlicStream stream,
+            StreamConsumedBody frame,
+            CancellationToken cancel) =>
+            WriteFrameAsync(writer, FrameType.StreamConsumed, stream, frame.Encode, cancel);
+
+        internal static ValueTask WriteStreamStopSendingAsync(
+            this ISlicFrameWriter writer,
+            SlicStream stream,
+            StreamStopSendingBody frame,
+            CancellationToken cancel) =>
+            WriteFrameAsync(writer, FrameType.StreamStopSending, stream, frame.Encode, cancel);
+
+        private static ValueTask WriteFrameAsync(
+            ISlicFrameWriter writer,
+            FrameType type,
+            SlicStream? stream,
+            Action<IceEncoder> encode,
+            CancellationToken cancel)
+        {
+            var bufferWriter = new BufferWriter();
+            var encoder = new Ice20Encoder(bufferWriter);
+            encoder.EncodeByte((byte)type);
+            BufferWriter.Position sizePos = encoder.StartFixedLengthSize();
+            if (stream != null)
+            {
+                encoder.EncodeVarULong((ulong)stream.Id);
+            }
+            encode(encoder);
+            encoder.EndFixedLengthSize(sizePos);
+            return writer.WriteFrameAsync(stream, bufferWriter.Finish(), cancel);
+        }
+    }
+}
