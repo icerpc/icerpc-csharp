29a831e8d153c147dddd587354a584094917afc7
Bernard Normier <bernard@zeroc.com>
2021-10-21 16:21:22 -0400

    Add LogListenerDecorator

---
+++ src/IceRpc/Transports/Internal/LogListenerDecorator.cs
@@ -0,0 +1,52 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using Microsoft.Extensions.Logging;
+
+namespace IceRpc.Transports.Internal
+{
+    internal sealed class LogListenerDecorator<T> : IListener<T> where T : INetworkConnection
+    {
+        private readonly IListener<T> _decoratee;
+        private readonly LogNetworkConnectionDecoratorFactory<T> _logDecoratorFactory;
+        private readonly ILogger _logger;
+
+        Endpoint IListener.Endpoint => _decoratee.Endpoint;
+
+        async ValueTask<T> IListener<T>.AcceptAsync()
+        {
+            try
+            {
+                T connection = await _decoratee.AcceptAsync().ConfigureAwait(false);
+                return _logDecoratorFactory(connection, true, _decoratee.Endpoint, _logger);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogListenerAcceptingConnectionFailed(_decoratee.Endpoint, ex);
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _decoratee.Dispose();
+            }
+            finally
+            {
+                _logger.LogListenerShutDown(_decoratee.Endpoint);
+            }
+        }
+
+        internal LogListenerDecorator(
+            IListener<T> decoratee,
+            ILogger logger,
+            LogNetworkConnectionDecoratorFactory<T> logDecoratorFactory)
+        {
+            _decoratee = decoratee;
+            _logDecoratorFactory = logDecoratorFactory;
+            _logger = logger;
+            _logger.LogListenerListening(_decoratee.Endpoint);
+        }
+    }
+}
