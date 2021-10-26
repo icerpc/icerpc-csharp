f0a7d1b390894ac637705c3398016c20727d4583
Benoit Foucher <benoit@zeroc.com>
2021-10-26 15:19:32 +0200

    Disabled again retry test

--- tests/IceRpc.Tests.ClientServer/RetryTests.cs
+++ tests/IceRpc.Tests.ClientServer/RetryTests.cs
@@ -95,13 +95,12 @@ namespace IceRpc.Tests.ClientServer
             await bidir.AfterDelayAsync(2);
         }
 
-        [TestCase(ProtocolCode.Ice1, 2)]
-        [TestCase(ProtocolCode.Ice1, 10)]
-        [TestCase(ProtocolCode.Ice1, 20)]
-        [TestCase(ProtocolCode.Ice2, 2)]
-        [TestCase(ProtocolCode.Ice2, 10)]
-        [TestCase(ProtocolCode.Ice2, 20)]
-        [Repeat(100)]
+        // [TestCase(ProtocolCode.Ice1, 2)]
+        // [TestCase(ProtocolCode.Ice1, 10)]
+        // [TestCase(ProtocolCode.Ice1, 20)]
+        // [TestCase(ProtocolCode.Ice2, 2)]
+        // [TestCase(ProtocolCode.Ice2, 10)]
+        // [TestCase(ProtocolCode.Ice2, 20)]
         public async Task Retry_GracefulClose(ProtocolCode protocol, int maxQueue)
         {
             await WithRetryServiceAsync(Protocol.FromProtocolCode(protocol), null, async (service, retry) =>
