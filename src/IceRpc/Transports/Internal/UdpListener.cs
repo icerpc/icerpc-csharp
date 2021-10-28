af79de5dcafb458f48bbd9985521bd1887805a0c
Bernard Normier <bernard@zeroc.com>
2021-10-28 12:12:09 -0400

    Simplify UdpListener implementation

--- src/IceRpc/Transports/Internal/UdpListener.cs
+++ src/IceRpc/Transports/Internal/UdpListener.cs
@@ -1,46 +1,18 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using IceRpc.Internal;
-
 namespace IceRpc.Transports.Internal
 {
     /// <summary>The listener implementation for the UDP transport.</summary>
     internal sealed class UdpListener : IListener<ISimpleNetworkConnection>
     {
         public Endpoint Endpoint { get; }
+        private readonly ISimpleNetworkConnection _serverConnection;
 
-        private readonly TaskCompletionSource<ISimpleNetworkConnection> _acceptTask = new();
-        private ISimpleNetworkConnection? _serverConnection;
-
-        public async Task<ISimpleNetworkConnection> AcceptAsync()
-        {
-            try
-            {
-                if (Interlocked.Exchange(ref _serverConnection, null) is ISimpleNetworkConnection serverConnection)
-                {
-                    // Return the server network connection for first call
-                    return serverConnection;
-                }
-                else
-                {
-                    // Wait indefinitely until Dispose is called
-                    return await _acceptTask.Task.ConfigureAwait(false);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ExceptionUtil.Throw(ex.ToTransportException(default));
-            }
-        }
+        public Task<ISimpleNetworkConnection> AcceptAsync() => Task.FromResult(_serverConnection);
 
         public override string ToString() => Endpoint.ToString();
 
-        public void Dispose()
-        {
-           // Close the server connection if AcceptAsync didn't already consume it.
-           Interlocked.Exchange(ref _serverConnection, null)?.Close();
-           _acceptTask.SetException(new ObjectDisposedException(nameof(UdpListener)));
-        }
+        public void Dispose() => _serverConnection.Close();
 
         internal UdpListener(Endpoint endpoint, ISimpleNetworkConnection serverConnection)
         {
