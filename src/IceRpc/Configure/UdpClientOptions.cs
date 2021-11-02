ee69b1ec123dd1b7f652c3cfde50aa49d860092b
Bernard Normier <bernard@zeroc.com>
2021-11-02 16:43:16 -0400

    Remove UdpOptions base class

--- src/IceRpc/Configure/UdpClientOptions.cs
+++ src/IceRpc/Configure/UdpClientOptions.cs
@@ -4,17 +4,8 @@ using System.Net;
 
 namespace IceRpc.Transports
 {
-    /// <summary>The base options class for configuring UDP based transports.</summary>
-    public class UdpOptions
-    {
-        /// <summary>Configures an IPv6 socket to only support IPv6. The socket won't support IPv4 mapped addresses
-        /// when this property is set to true. The default value is false.</summary>
-        /// <value>The boolean value to enable or disable IPv6-only support.</value>
-        public bool IsIPv6Only { get; set; }
-    }
-
     /// <summary>The options class for configuring <see cref="UdpClientTransport"/>.</summary>
-    public sealed class UdpClientOptions : UdpOptions
+    public sealed class UdpClientOptions
     {
         /// <summary>The idle timeout. This timeout is used to monitor the network connection. If the connection
         /// is idle within this timeout period, the connection is gracefully closed. It can't be 0 and the default
@@ -26,6 +17,11 @@ namespace IceRpc.Transports
             set => _idleTimeout = value != TimeSpan.Zero ? value :
                 throw new ArgumentException($"0 is not a valid value for {nameof(IdleTimeout)}", nameof(value));
         }
+
+        /// <summary>Configures an IPv6 socket to only support IPv6. The socket won't support IPv4 mapped addresses
+        /// when this property is set to true. The default value is false.</summary>
+        /// <value>The boolean value to enable or disable IPv6-only support.</value>
+        public bool IsIPv6Only { get; set; }
 
         /// <summary>The address and port represented by a .NET IPEndPoint to use for a client socket. If specified the
         /// client socket will bind to this address and port before connection establishment.</summary>
@@ -45,22 +41,5 @@ namespace IceRpc.Transports
         private TimeSpan _idleTimeout = TimeSpan.FromSeconds(60);
 
         private int? _sendBufferSize;
-    }
-
-    /// <summary>The options class for configuring <see cref="UdpClientTransport"/>.</summary>
-    public sealed class UdpServerOptions : UdpOptions
-    {
-
-        /// <summary>The socket receive buffer size in bytes. It can't be less than 1KB. If not set, the OS default
-        /// receive buffer size is used.</summary>
-        /// <value>The receive buffer size in bytes.</value>
-        public int? ReceiveBufferSize
-        {
-            get => _receiveBufferSize;
-            set => _receiveBufferSize = value == null || value >= 1024 ? value :
-                throw new ArgumentException($"{nameof(ReceiveBufferSize)} can't be less than 1KB", nameof(value));
-        }
-
-        private int? _receiveBufferSize;
     }
 }
---
+++ src/IceRpc/Configure/UdpServerOptions.cs
@@ -0,0 +1,25 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+namespace IceRpc.Transports
+{
+    /// <summary>The options class for configuring <see cref="UdpClientTransport"/>.</summary>
+    public sealed class UdpServerOptions
+    {
+        /// <summary>Configures an IPv6 socket to only support IPv6. The socket won't support IPv4 mapped addresses
+        /// when this property is set to true. The default value is false.</summary>
+        /// <value>The boolean value to enable or disable IPv6-only support.</value>
+        public bool IsIPv6Only { get; set; }
+
+        /// <summary>The socket receive buffer size in bytes. It can't be less than 1KB. If not set, the OS default
+        /// receive buffer size is used.</summary>
+        /// <value>The receive buffer size in bytes.</value>
+        public int? ReceiveBufferSize
+        {
+            get => _receiveBufferSize;
+            set => _receiveBufferSize = value == null || value >= 1024 ? value :
+                throw new ArgumentException($"{nameof(ReceiveBufferSize)} can't be less than 1KB", nameof(value));
+        }
+
+        private int? _receiveBufferSize;
+    }
+}
