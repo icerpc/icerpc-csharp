452d4014eabc76229148a5c35dc33fabd7c2c54f
Bernard Normier <bernard@zeroc.com>
2021-11-11 10:16:20 -0500

    Added missing break

--- src/IceRpc/Transports/Internal/LogNetworkConnectionDecorator.cs
+++ src/IceRpc/Transports/Internal/LogNetworkConnectionDecorator.cs
@@ -79,6 +79,7 @@ namespace IceRpc.Transports.Internal
                 if (size > 32)
                 {
                     _ = sb.Append("...");
+                    break;
                 }
             }
             return sb.ToString().Trim();
