6cc9ab86c0dd1959529238f89df436f95aa79f2a
Bernard Normier <bernard@zeroc.com>
2021-11-15 15:49:08 -0500

    bug fix

--- src/IceRpc/Slice/Ice20Encoding.cs
+++ src/IceRpc/Slice/Ice20Encoding.cs
@@ -21,22 +21,14 @@ namespace IceRpc.Slice
             using IMemoryOwner<byte> owner = MemoryPool<byte>.Shared.Rent(8);
             Memory<byte> buffer = owner.Memory[0..1];
 
-            int read = await payloadStream.ReadAsync(buffer, cancel).ConfigureAwait(false);
-            if (read == 0)
+            await payloadStream.ReadUntilFullAsync(buffer, cancel).ConfigureAwait(false);
+            int sizeLength = Ice20Decoder.DecodeSizeLength(buffer.Span[0]);
+            if (sizeLength > 1)
             {
-                // end of stream == empty payload
-                return 0;
+                buffer = owner.Memory[0..sizeLength];
+                await payloadStream.ReadUntilFullAsync(buffer[1..], cancel).ConfigureAwait(false);
             }
-            else
-            {
-                int sizeLength = Ice20Decoder.DecodeSizeLength(buffer.Span[0]);
-                if (sizeLength > 1)
-                {
-                    buffer = owner.Memory[0..sizeLength];
-                    await payloadStream.ReadUntilFullAsync(buffer[1..], cancel).ConfigureAwait(false);
-                }
-                return Ice20Decoder.DecodeSize(buffer.Span).Size;
-            }
+            return Ice20Decoder.DecodeSize(buffer.Span).Size;
         }
 
         internal override IceEncoder CreateIceEncoder(BufferWriter bufferWriter) => new Ice20Encoder(bufferWriter);
