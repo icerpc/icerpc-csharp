acac94f8bc37bd698d5582cf440e7f6fe542f288
Jose <jose@zeroc.com>
2021-11-15 19:13:42 +0100

    Validate cs:attributes

--- tools/slicec-cs/src/cs_validator.rs
+++ tools/slicec-cs/src/cs_validator.rs
@@ -1,5 +1,6 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+use slice::grammar::*;
 use slice::visitor::Visitor;
 
 /// CsValidator visits all the elements in a slice file to check for errors and warnings specific to
@@ -8,6 +9,188 @@ use slice::visitor::Visitor;
 #[derive(Debug)]
 pub(crate) struct CsValidator;
 
+fn cs_attributes(attributes: &[Attribute]) -> Vec<Attribute> {
+    attributes
+        .iter()
+        .cloned()
+        .filter(|attribute| {
+            attribute.prefix.is_some() && attribute.prefix.as_ref().unwrap() == "cs"
+        })
+        .collect::<Vec<_>>()
+}
+
+fn report_unexpected_attribute(attribute: &Attribute) {
+    slice::report_error(
+        format!("unexpected attribute cs:{}", attribute.directive),
+        Some(attribute.location.clone()),
+    );
+}
+
+fn validate_cs_attribute(attribute: &Attribute) {
+    if attribute.arguments.len() > 1 {
+        slice::report_error(
+            r#"too many arguments expected 'cs:attribute("<value>")'"#.to_owned(),
+            Some(attribute.location.clone()),
+        );
+    }
+}
+
+fn validate_cs_marshaled_result(attribute: &Attribute) {
+    if !attribute.arguments.is_empty() {
+        slice::report_error(
+            "too many arguments expected 'cs:marshaled-result'".to_owned(),
+            Some(attribute.location.clone()),
+        );
+    }
+}
+
+fn validate_cs_generic(attribute: &Attribute) {
+    if attribute.arguments.len() > 1 {
+        slice::report_error(
+            r#"too many arguments expected 'cs:generic("<value>")'"#.to_owned(),
+            Some(attribute.location.clone()),
+        );
+    }
+}
+
+fn validate_collection_attributes<T: Attributable>(attributable: &T) {
+    for attribute in &cs_attributes(attributable.attributes()) {
+        match attribute.directive.as_ref() {
+            "generic" => validate_cs_generic(attribute),
+            _ => report_unexpected_attribute(attribute),
+        }
+    }
+}
+
+fn report_typeref_unexpected_attributes<T: Attributable>(attributable: &T) {
+    for attribute in &cs_attributes(attributable.attributes()) {
+        report_unexpected_attribute(attribute);
+    }
+}
+
 impl Visitor for CsValidator {
-    // TODO add validation logic here.
+    fn visit_module_start(&mut self, module_def: &Module) {
+        for attribute in &cs_attributes(module_def.attributes()) {
+            match attribute.directive.as_ref() {
+                "namespace" => {
+                    if attribute.arguments.len() > 1 {
+                        slice::report_error(
+                            r#"too many arguments expected 'cs:namespace("<namespace>")'"#
+                                .to_owned(),
+                            Some(attribute.location.clone()),
+                        );
+                    }
+                    if !module_def.is_top_level() {
+                        slice::report_error(
+                            "The 'cs:namespace' attribute is only valid for top-level modules"
+                                .to_owned(),
+                            Some(attribute.location.clone()),
+                        );
+                    }
+                }
+                "attribute" => validate_cs_attribute(attribute),
+                _ => report_unexpected_attribute(attribute),
+            }
+        }
+    }
+
+    fn visit_struct_start(&mut self, struct_def: &Struct) {
+        for attribute in &cs_attributes(struct_def.attributes()) {
+            match attribute.directive.as_ref() {
+                "readonly" => {
+                    if !attribute.arguments.is_empty() {
+                        slice::report_error(
+                            "too many arguments expected 'cs:readonly'".to_owned(),
+                            Some(attribute.location.clone()),
+                        );
+                    }
+                }
+                "attribute" => validate_cs_attribute(attribute),
+                _ => report_unexpected_attribute(attribute),
+            }
+        }
+    }
+
+    fn visit_class_start(&mut self, class_def: &Class) {
+        for attribute in &cs_attributes(class_def.attributes()) {
+            match attribute.directive.as_ref() {
+                "attribute" => validate_cs_attribute(attribute),
+                _ => report_unexpected_attribute(attribute),
+            }
+        }
+    }
+
+    fn visit_exception_start(&mut self, exception_def: &Exception) {
+        for attribute in &cs_attributes(exception_def.attributes()) {
+            match attribute.directive.as_ref() {
+                "attribute" => validate_cs_attribute(attribute),
+                _ => report_unexpected_attribute(attribute),
+            }
+        }
+    }
+
+    fn visit_interface_start(&mut self, interface_def: &Interface) {
+        for attribute in &cs_attributes(interface_def.attributes()) {
+            match attribute.directive.as_ref() {
+                "marshaled-result" => validate_cs_marshaled_result(attribute),
+                "attribute" => validate_cs_attribute(attribute),
+                _ => report_unexpected_attribute(attribute),
+            }
+        }
+    }
+
+    fn visit_enum_start(&mut self, enum_def: &Enum) {
+        for attribute in &cs_attributes(enum_def.attributes()) {
+            match attribute.directive.as_ref() {
+                "attribute" => validate_cs_attribute(attribute),
+                _ => report_unexpected_attribute(attribute),
+            }
+        }
+    }
+
+    fn visit_operation_start(&mut self, operation: &Operation) {
+        for attribute in &cs_attributes(operation.attributes()) {
+            match attribute.directive.as_ref() {
+                "marshaled-result" => validate_cs_marshaled_result(attribute),
+                "attribute" => validate_cs_attribute(attribute),
+                _ => report_unexpected_attribute(attribute),
+            }
+        }
+    }
+
+    fn visit_type_alias(&mut self, type_alias: &TypeAlias) {
+        match type_alias.underlying.concrete_type() {
+            Types::Sequence(_) | Types::Dictionary(_) => {
+                validate_collection_attributes(&type_alias.underlying)
+            }
+            _ => report_typeref_unexpected_attributes(&type_alias.underlying),
+        }
+    }
+
+    fn visit_data_member(&mut self, data_member: &DataMember) {
+        match data_member.data_type.concrete_type() {
+            Types::Sequence(_) | Types::Dictionary(_) => {
+                validate_collection_attributes(&data_member.data_type)
+            }
+            _ => report_typeref_unexpected_attributes(&data_member.data_type),
+        }
+    }
+
+    fn visit_parameter(&mut self, parameter: &Parameter) {
+        match parameter.data_type.concrete_type() {
+            Types::Sequence(_) | Types::Dictionary(_) => {
+                validate_collection_attributes(&parameter.data_type)
+            }
+            _ => report_typeref_unexpected_attributes(&parameter.data_type),
+        }
+    }
+
+    fn visit_return_member(&mut self, parameter: &Parameter) {
+        match parameter.data_type.concrete_type() {
+            Types::Sequence(_) | Types::Dictionary(_) => {
+                validate_collection_attributes(&parameter.data_type)
+            }
+            _ => report_typeref_unexpected_attributes(&parameter.data_type),
+        }
+    }
 }
