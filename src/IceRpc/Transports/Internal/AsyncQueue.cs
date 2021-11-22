c9e9a8289ee83a364b3c744e86987841e39eb07e
Benoit Foucher <benoit@zeroc.com>
2021-11-22 15:34:22 +0100

    Missing file

---
+++ src/IceRpc/Transports/Internal/AsyncQueue.cs
@@ -0,0 +1,34 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using IceRpc.Internal;
+using System.Diagnostics;
+using System.Threading.Tasks.Sources;
+
+namespace IceRpc.Transports.Internal
+{
+    /// <summary>The AsyncQueue provides queuing functionality with asynchronous Dequeue</summary>
+    internal class AsyncQueue<T> : IAsyncQueueValueTaskSource<T>
+    {
+        // TODO: remove pragma warning disable/restore once analyser is fixed.
+        // It is necessary to call new() explicitly to execute the parameterless ctor of AsyncQueueCore, which is
+        // synthesized from AsyncQueueCore fields defaults.
+#pragma warning disable CA1805 // member is explicitly initialized to its default value
+        private AsyncQueueCore<T> _queue = new();
+#pragma warning restore CA1805
+
+        internal void Complete(Exception exception) => _queue.Complete(exception);
+
+        internal void Enqueue(T value) => _queue.Enqueue(value);
+
+        internal ValueTask<T> DequeueAsync(CancellationToken cancel) => _queue.DequeueAsync(this, cancel);
+
+        void IAsyncQueueValueTaskSource<T>.Cancel() => _queue.Complete(new OperationCanceledException());
+
+        T IValueTaskSource<T>.GetResult(short token) => _queue.GetResult(token);
+
+        ValueTaskSourceStatus IValueTaskSource<T>.GetStatus(short token) => _queue.GetStatus(token);
+
+        void IValueTaskSource<T>.OnCompleted(Action<object?> continuation, object? state, short token, ValueTaskSourceOnCompletedFlags flags) =>
+            _queue.OnCompleted(continuation, state, token, flags);
+    }
+}
