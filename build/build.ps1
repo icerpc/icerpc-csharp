6fd25b2011f0b4218b54f445ca7561e198d215ef
Jose <jose@zeroc.com>
2021-11-23 21:30:44 +0100

    Support building examples and NuGet packages with Windows build.cmd

--- build/build.ps1
+++ build/build.ps1
@@ -3,6 +3,8 @@
 param (
     $action="build",
     $config="debug",
+    [switch]$examples,
+    [switch]$srcdist,
     [switch]$coverage,
     [switch]$help
 )
@@ -27,12 +29,20 @@ function Build-IceRpc($config) {
     $args = @('build')
     if ($config -eq 'release') {
         $args += @('--configuration', 'Release')
-    } elseif ($config -eq 'debug') {
-        $args += @('--configuration', 'Debug')
     } else {
-        Write-Error "unknown configuration"
-        exit 1
+        $args += @('--configuration', 'Debug')
     }
+    RunCommand "dotnet" $args
+}
+
+function Build-IceRpcExamples($config) {
+    $args = @('build')
+    if ($config -eq 'release') {
+        $args += @('--configuration', 'Release')
+    } else {
+        $args += @('--configuration', 'Debug')
+    }
+    $args += @("examples\Hello\Hello.sln")
     RunCommand "dotnet" $args
 }
 
@@ -40,9 +50,32 @@ function Clean-IceRpc($config) {
     RunCommand "dotnet" "clean"
 }
 
-function Build($config) {
-    Build-Compiler $config
-    Build-IceRpc $config
+function Build($config, $examples, $srcdist) {
+    if ($examples) {
+        if ($srcdist) {
+           Build-Compiler $config
+           Pack $config
+           $global_packages = dotnet nuget locals -l global-packages
+           $global_packages = $global_packages.replace("global-packages: ", "")
+           Remove-Item $global_packages"\icerpc" -Recurse -Force -ErrorAction Ignore
+           Remove-Item $global_packages"\icerpc.interop" -Recurse -Force -ErrorAction Ignore
+           RunCommand "dotnet" @('nuget', 'push', 'lib\*.nupkg', '--source', $global_packages)
+        }
+        Build-IceRpcExamples $config
+    } else {
+        Build-Compiler $config
+        Build-IceRpc $config
+    }
+}
+
+function Pack($config) {
+    $args = @('pack')
+    if ($config -eq 'release') {
+        $args += @('--configuration', 'Release')
+    } else {
+        $args += @('--configuration', 'Debug')
+    }
+    RunCommand "dotnet" $args
 }
 
 function Rebuild($config) {
@@ -92,12 +125,16 @@ function Get-Help() {
     Write-Host "Usage: build [command] [arguments]"
     Write-Host "Commands (defaults to build):"
     Write-Host "  build                     Build IceRpc sources & slice-cs compiler."
+    Write-Host "  pack                      Build the IceRpc NuGet packages."
     Write-Host "  clean                     Clean IceRpc sources & slice-cs compiler."
     Write-Host "  rebuild                   Rebuild IceRpc sources & slice-cs compiler."
     Write-Host "  test                      Runs tests."
     Write-Host "  doc                       Generate documentation"
     Write-Host "Arguments:"
     Write-Host "  -config                   Build configuration: debug or release, the default is debug."
+    Write-Host "  -examples                 Build examples solutions instead of the source solutions."
+    Write-Host "  -srcdist                  Use NuGet packages from this source distribution when building examples."
+    Write-Host "                            The NuGet packages are installed to the local global-packages source."
     Write-Host "  -coverage                 Collect code coverage from test runs."
     Write-Host "                            Requires reportgeneratool from https://github.com/danielpalme/ReportGenerator"
     Write-Host "  -help                     Print help and exit."
@@ -116,10 +153,13 @@ if ( $help ) {
 
 switch ( $action ) {
     "build" {
-        Build $config
+        Build $config $examples $srcdist
+    }
+    "pack" {
+        Pack $config
     }
     "rebuild" {
-        Rebuild $config
+        Rebuild $config $examples $srcdist
     }
     "clean" {
         Clean $config
