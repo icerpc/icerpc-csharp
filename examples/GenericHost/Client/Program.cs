f75b45deca70de65efac1a020a22eb772e79d7fa
Benoit Foucher <benoit@zeroc.com>
2021-11-30 17:07:49 +0100

    More review fixes

--- examples/GenericHost/Client/Program.cs
+++ examples/GenericHost/Client/Program.cs
@@ -12,99 +12,129 @@ using Microsoft.Extensions.Options;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-namespace ClientApp;
-
-public class Program
+namespace ClientApp
 {
-    public class HelloOptions
+    public class Program
     {
-        public ConnectionOptions ConnectionOptions { get; set; } = new();
+        /// <summary>This class is used to read the client configuration options from the appsettings.json configuration
+        /// file.</summary>
+        public class ClientOptions
+        {
+            public ConnectionOptions ConnectionOptions { get; set; } = new();
 
-        public Endpoint Endpoint { get; set; } = "ice+tcp://[::1]";
-    }
+            public Endpoint Endpoint { get; set; } = "ice+tcp://[::1]";
+        }
 
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseContentRoot(AppContext.BaseDirectory)
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddHostedService<HelloService>();
-                services.AddOptions<HelloOptions>()
-                    .Bind(hostContext.Configuration.GetSection("Hello"));
+        /// <summary>Creates the HostBuilder used to build the .NET Generic Host.</summary>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                // Set the content root path to the build directory of the server (e.g.: Client/bin/Debug/net6.0)
+                .UseContentRoot(AppContext.BaseDirectory)
 
-                services.AddSingleton<IClientTransport<IMultiplexedNetworkConnection>>(serviceProvider =>
-                    {
-                        IConfiguration configuration = hostContext.Configuration.GetSection("Transport");
-                        TcpClientOptions tcpOptions = configuration?.GetValue<TcpClientOptions>("Tcp") ?? new();
-                        tcpOptions.AuthenticationOptions = new SslClientAuthenticationOptions()
+                // Configures the .NET Generic Host services.
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Add the ClientHostedService to the hosted services of the .NET Generic Host.
+                    services.AddHostedService<ClientHostedService>();
+
+                    // Get the ClientOptions from the configuration and add it to the generic host options. The DI
+                    // container will inject it in services that require an IOptions<ClientOptions> dependency.
+                    services.AddOptions<ClientOptions>().Bind(hostContext.Configuration.GetSection("Client"));
+
+                    // Create the client transport and add it as a singleton service of the generic host services. The
+                    // DI container will inject it in services that require an
+                    // IClientTransport<IMultiplexedNetworkConnection> dependency.
+                    services.AddSingleton<IClientTransport<IMultiplexedNetworkConnection>>(serviceProvider =>
                         {
-                            RemoteCertificateValidationCallback =
-                                CertificateValidaton.GetServerCertificateValidationCallback(
-                                    certificateAuthorities: new X509Certificate2Collection
-                                    {
-                                        new X509Certificate2(
-                                            Path.Combine(
-                                                hostContext.HostingEnvironment.ContentRootPath,
-                                                configuration.GetValue<string>("CertificateAuthoritiesFile")))
-                                    })
-                        };
-                        return new SlicClientTransport(new TcpClientTransport(tcpOptions));
-                    });
+                            // Get the transport options from the configuration.
+                            IConfiguration configuration = hostContext.Configuration.GetSection("Transport");
+                            TcpClientOptions tcpOptions = configuration?.GetValue<TcpClientOptions>("Tcp") ?? new();
 
-                services.AddSingleton<IInvoker>(serviceProvider =>
-                    {
-                        ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-                        return new Pipeline()
-                            .UseLogger(loggerFactory)
-                            .UseTelemetry(new TelemetryOptions { LoggerFactory = loggerFactory });
-                    });
-            });
+                            // Create the authentication options with the certificate authorities defined in the
+                            // configured certificate authorities file.
+                            tcpOptions.AuthenticationOptions = new SslClientAuthenticationOptions()
+                            {
+                                RemoteCertificateValidationCallback =
+                                    CertificateValidaton.GetServerCertificateValidationCallback(
+                                        certificateAuthorities: new X509Certificate2Collection
+                                        {
+                                            new X509Certificate2(
+                                                Path.Combine(
+                                                    hostContext.HostingEnvironment.ContentRootPath,
+                                                    configuration.GetValue<string>("CertificateAuthoritiesFile")))
+                                        })
+                            };
 
-    private class HelloService : BackgroundService
-    {
-        private readonly IHostApplicationLifetime _applicationLifetime;
-        private readonly Connection _connection;
-        private readonly HelloPrx _proxy;
+                            return new SlicClientTransport(new TcpClientTransport(tcpOptions));
+                        });
 
-        public HelloService(
-            IClientTransport<IMultiplexedNetworkConnection> clientTransport,
-            IOptions<HelloOptions> options,
-            IInvoker invoker,
-            ILoggerFactory loggerFactory,
-            IHostApplicationLifetime applicationLifetime)
+                    // Add an IInvoker singleton service. The DI container will inject it in services that require an
+                    // IInvoker dependency.
+                    services.AddSingleton<IInvoker>(serviceProvider =>
+                        {
+                            // The invoker is a pipeline configured with the logger and telemetry interceptors. The
+                            // interceptors use the logger factory provided by the .NET Generic Host.
+                            ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                            return new Pipeline()
+                                .UseLogger(loggerFactory)
+                                .UseTelemetry(new TelemetryOptions { LoggerFactory = loggerFactory });
+                        });
+                });
+
+        /// <summary>The client hosted service is ran and managed by the .NET Generic Host</summary>
+        private class ClientHostedService : BackgroundService
         {
-            _applicationLifetime = applicationLifetime;
+            // The host application lifetime is used to stop the .NET Generic Host.
+            private readonly IHostApplicationLifetime _applicationLifetime;
+            // The IceRPC connection to communicate with the IceRPC server.
+            private readonly Connection _connection;
+            // The IceRPC proxy to perform invocations on the Hello service from the IceRPC server.
+            private readonly HelloPrx _proxy;
 
-            _connection = new Connection()
+            public ClientHostedService(
+                IClientTransport<IMultiplexedNetworkConnection> clientTransport,
+                IOptions<ClientOptions> options,
+                IInvoker invoker,
+                ILoggerFactory loggerFactory,
+                IHostApplicationLifetime applicationLifetime)
             {
-                MultiplexedClientTransport = clientTransport,
-                Options = options.Value.ConnectionOptions,
-                RemoteEndpoint = options.Value.Endpoint,
-                LoggerFactory = loggerFactory
-            };
+                _applicationLifetime = applicationLifetime;
 
-            _proxy = HelloPrx.FromConnection(_connection, invoker: invoker);
-        }
+                _connection = new Connection()
+                {
+                    MultiplexedClientTransport = clientTransport,
+                    Options = options.Value.ConnectionOptions,
+                    RemoteEndpoint = options.Value.Endpoint,
+                    LoggerFactory = loggerFactory
+                };
 
-        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
-        {
-            try
-            {
-                Console.WriteLine(await _proxy.SayHelloAsync("Hello", cancel: cancellationToken));
+                _proxy = HelloPrx.FromConnection(_connection, invoker: invoker);
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine($"couldn't connect to the hello service:\n {exception}");
-            }
-            _applicationLifetime.StopApplication();
-        }
 
-        public override async Task StopAsync(CancellationToken cancellationToken)
-        {
-            await base.StopAsync(cancellationToken);
-            await _connection.ShutdownAsync(cancellationToken);
+            protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+            {
+                try
+                {
+                    Console.WriteLine(await _proxy.SayHelloAsync("Hello", cancel: cancellationToken));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"failed to connect to the server:\n{exception}");
+                }
+
+                // Stop the generic host once the invocation is done.
+                _applicationLifetime.StopApplication();
+            }
+
+            public override async Task StopAsync(CancellationToken cancellationToken)
+            {
+                await base.StopAsync(cancellationToken);
+
+                // Shutdown the connection when the hosted service is stopped.
+                await _connection.ShutdownAsync(cancellationToken);
+            }
         }
     }
 }
--- examples/GenericHost/Server/Program.cs
+++ examples/GenericHost/Server/Program.cs
@@ -12,79 +12,105 @@ using Microsoft.Extensions.Options;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-namespace ServerApp;
-
-public class HelloOptions
+namespace ServerApp
 {
-    public ConnectionOptions ConnectionOptions { get; set; } = new();
-
-    public Endpoint Endpoint { get; set; } = "ice+tcp://[::0]";
-}
-
-public class Program
-{
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseContentRoot(AppContext.BaseDirectory)
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddHostedService<HelloService>();
-
-                services.AddOptions<HelloOptions>()
-                    .Bind(hostContext.Configuration.GetSection("Hello"));
-
-                services.AddSingleton<IServerTransport<IMultiplexedNetworkConnection>>(serviceProvider =>
-                    {
-                        IConfiguration configuration = hostContext.Configuration.GetSection("Transport");
-                        TcpServerOptions tcpOptions = configuration?.GetValue<TcpServerOptions>("Tcp") ?? new();
-                        tcpOptions.AuthenticationOptions = new SslServerAuthenticationOptions()
-                        {
-                            ServerCertificate = new X509Certificate2(
-                                Path.Combine(
-                                    hostContext.HostingEnvironment.ContentRootPath,
-                                    configuration.GetValue<string>("CertificateFile")),
-                                configuration.GetValue<string>("CertificatePassword"))
-                        };
-                        return new SlicServerTransport(new TcpServerTransport(tcpOptions));
-                    });
-
-                services.AddSingleton<IDispatcher>(serviceProvider =>
-                    {
-                        ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-                        var router = new Router();
-                        router.UseLogger(loggerFactory);
-                        router.UseTelemetry(new TelemetryOptions { LoggerFactory = loggerFactory });
-                        router.Map<IHello>(new Hello());
-                        return router;
-                    });
-            });
-
-    private class HelloService : IHostedService
+    /// <summary>This class is used to read the server configuration options from the appsettings.json configuration
+    /// file.</summary>
+    public class ServerOptions
     {
-        private readonly Server _server;
+        public ConnectionOptions ConnectionOptions { get; set; } = new();
 
-        public HelloService(
-            IServerTransport<IMultiplexedNetworkConnection> serverTransport,
-            IOptions<HelloOptions> options,
-            IDispatcher dispatcher,
-            ILoggerFactory loggerFactory) =>
-            _server = new Server
-            {
-                ConnectionOptions = options.Value.ConnectionOptions,
-                Dispatcher = dispatcher,
-                Endpoint = options.Value.Endpoint,
-                LoggerFactory = loggerFactory,
-                MultiplexedServerTransport = serverTransport
-            };
+        public Endpoint Endpoint { get; set; } = "ice+tcp://[::0]";
+    }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+    public class Program
+    {
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        /// <summary>Creates the HostBuilder used to build the .NET Generic Host.</summary>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                // Set the content root path to the build directory of the server (e.g.: Server/bin/Debug/net6.0)
+                .UseContentRoot(AppContext.BaseDirectory)
+
+                // Configure the .NET Generic Host services.
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Add the ServerHostedService to the hosted services of the .NET Generic Host.
+                    services.AddHostedService<ServerHostedService>();
+
+                    // Get the ServerOptions from the configuration and add it to the generic host options. The DI
+                    // container will inject it in services that require an IOptions<ServerOptions> dependency.
+                    services.AddOptions<ServerOptions>().Bind(hostContext.Configuration.GetSection("Server"));
+
+                    // Create the client transport and add it as a singleton service of the generic host services. The
+                    // DI container will inject it in services that require an
+                    // IServerTransport<IMultiplexedNetworkConnection> dependency.
+                    services.AddSingleton<IServerTransport<IMultiplexedNetworkConnection>>(serviceProvider =>
+                        {
+                            // Get the transport options from the configuration.
+                            IConfiguration configuration = hostContext.Configuration.GetSection("Transport");
+                            TcpServerOptions tcpOptions = configuration?.GetValue<TcpServerOptions>("Tcp") ?? new();
+
+                            // Create the authentication options with the certificate defined in the configured
+                            // certificate file.
+                            tcpOptions.AuthenticationOptions = new SslServerAuthenticationOptions()
+                            {
+                                ServerCertificate = new X509Certificate2(
+                                    Path.Combine(
+                                        hostContext.HostingEnvironment.ContentRootPath,
+                                        configuration.GetValue<string>("CertificateFile")),
+                                    configuration.GetValue<string>("CertificatePassword"))
+                            };
+
+                            return new SlicServerTransport(new TcpServerTransport(tcpOptions));
+                        });
+
+                    // Add an IDispatcher singleton service. The DI container will inject it in services that require an
+                    // IInvoker dependency.
+                    services.AddSingleton<IDispatcher>(serviceProvider =>
+                        {
+                            // The dispatcher is a router configured with the logger and telemetry middlewares and the
+                            // IHello service. The middlewares use the logger factory provided by the .NET Generic Host.
+                            ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                            var router = new Router();
+                            router.UseLogger(loggerFactory);
+                            router.UseTelemetry(new TelemetryOptions { LoggerFactory = loggerFactory });
+                            router.Map<IHello>(new Hello());
+                            return router;
+                        });
+                });
+
+        /// <summary>The server hosted service is ran and managed by the .NET Generic Host</summary>
+        private class ServerHostedService : IHostedService
         {
-            _server.Listen();
-            return Task.CompletedTask;
-        }
+            // The IceRPC server to accept connections from IceRPC clients.
+            private readonly Server _server;
 
-        public Task StopAsync(CancellationToken cancellationToken) => _server.ShutdownAsync(cancellationToken);
+            public ServerHostedService(
+                IServerTransport<IMultiplexedNetworkConnection> serverTransport,
+                IOptions<ServerOptions> options,
+                IDispatcher dispatcher,
+                ILoggerFactory loggerFactory) =>
+                _server = new Server
+                {
+                    ConnectionOptions = options.Value.ConnectionOptions,
+                    Dispatcher = dispatcher,
+                    Endpoint = options.Value.Endpoint,
+                    LoggerFactory = loggerFactory,
+                    MultiplexedServerTransport = serverTransport
+                };
+
+            public Task StartAsync(CancellationToken cancellationToken)
+            {
+                // Start listening for client connections.
+                _server.Listen();
+                return Task.CompletedTask;
+            }
+
+            public Task StopAsync(CancellationToken cancellationToken) =>
+                // Shutdown the IceRPC server when the hosted service is stopped.
+                _server.ShutdownAsync(cancellationToken);
+        }
     }
 }
