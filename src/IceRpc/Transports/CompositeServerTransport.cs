1dcb9e6b24066931b281a7c5ce1c99452ddf70eb
Benoit Foucher <benoit@zeroc.com>
2021-12-02 16:20:58 +0100

    Review fixes

--- src/IceRpc/Transports/CompositeServerTransport.cs
+++ src/IceRpc/Transports/CompositeServerTransport.cs
@@ -7,14 +7,13 @@ namespace IceRpc.Transports
     /// <summary>A composite server transport.</summary>
     public class CompositeServerTransport<T> : IServerTransport<T> where T : INetworkConnection
     {
-        private string? _firstTransport;
+        private Endpoint? _defaultEndpoint;
         private IReadOnlyDictionary<string, IServerTransport<T>>? _transports;
         private readonly Dictionary<string, IServerTransport<T>> _builder = new();
 
         /// <inheritdoc/>
-        Endpoint IServerTransport<T>.DefaultEndpoint => _firstTransport == null ?
-            throw new InvalidOperationException("no transport configured"):
-            _builder[_firstTransport].DefaultEndpoint;
+        Endpoint IServerTransport<T>.DefaultEndpoint =>
+            _defaultEndpoint ?? throw new InvalidOperationException("no transport configured");
 
         /// <summary>Adds a new server transport to this composite server transport.</summary>
         /// <param name="name">The transport name.</param>
@@ -27,7 +26,9 @@ namespace IceRpc.Transports
                 throw new InvalidOperationException(
                     $"cannot call {nameof(Add)} after calling {nameof(IClientTransport<T>.CreateConnection)}");
             }
-            _firstTransport ??= name;
+
+            // The composite default endpoint is the default endpoint of the first added server transport.
+            _defaultEndpoint ??= transport.DefaultEndpoint;
             _builder.Add(name, transport);
             return this;
         }
