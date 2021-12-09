be409adbfbc4c5c0c59d89118313c97ea16fa645
Bernard Normier <bernard@zeroc.com>
2021-12-09 15:36:47 -0500

    Cleanup

--- src/IceRpc/Internal/SimpleNetworkConnectionPipeWriter.cs
+++ src/IceRpc/Internal/SimpleNetworkConnectionPipeWriter.cs
@@ -12,18 +12,19 @@ namespace IceRpc.Internal
         public override bool CanGetUnflushedBytes => false; // doesn't support unflushed bytes at all
         private readonly ISimpleNetworkConnection _connection;
         private bool _isReaderCompleted;
+        private bool _isWriterCompleted;
 
         public override void Advance(int bytes) => throw new NotImplementedException();
 
         public override void CancelPendingFlush() => throw new NotImplementedException();
 
-        public override void Complete(Exception? _)
-        {
-            // no-op
-        }
+        public override void Complete(Exception? _ = null) => _isWriterCompleted = true;
 
-        public override ValueTask<FlushResult> FlushAsync(CancellationToken cancellationToken) =>
-            new(new FlushResult(isCanceled: false, isCompleted: _isReaderCompleted));
+        public override ValueTask<FlushResult> FlushAsync(CancellationToken cancellationToken)
+        {
+            ThrowIfCompleted();
+            return new(new FlushResult(isCanceled: false, isCompleted: _isReaderCompleted));
+        }
 
         public override Memory<byte> GetMemory(int sizeHint) => throw new NotImplementedException();
         public override Span<byte> GetSpan(int sizeHint) => throw new NotImplementedException();
@@ -32,6 +33,8 @@ namespace IceRpc.Internal
             ReadOnlyMemory<byte> source,
             CancellationToken cancellationToken)
         {
+            ThrowIfCompleted();
+
             if (!_isReaderCompleted)
             {
                 try
@@ -52,5 +55,13 @@ namespace IceRpc.Internal
 
         internal SimpleNetworkConnectionPipeWriter(ISimpleNetworkConnection connection) =>
             _connection = connection;
+
+        private void ThrowIfCompleted()
+        {
+            if (_isWriterCompleted)
+            {
+                throw new InvalidOperationException("pipe writer is completed");
+            }
+        }
     }
 }
