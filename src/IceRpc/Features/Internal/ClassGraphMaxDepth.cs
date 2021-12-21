ccb36af86e603519969679ad4b74038d622115e0
Bernard Normier <bernard@zeroc.com>
2021-12-21 11:30:37 -0500

    Cleanup

--- src/IceRpc/Features/Internal/ClassGraphMaxDepth.cs
+++ src/IceRpc/Features/Internal/ClassGraphMaxDepth.cs
@@ -5,7 +5,7 @@ namespace IceRpc.Features.Internal
     /// <summary>A feature used to override the class graph max depth used when decoding Slice.</summary>
     internal sealed class ClassGraphMaxDepth
     {
-        /// <summary>.The maximum depth for a graph of Slice class instances.</summary>
+        /// <summary>The maximum depth for a graph of Slice class instances.</summary>
         // TODO: replace by a max depth property that is not class specific.
         internal int Value { get; set; }
     }
