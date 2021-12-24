3622b8ed62d8d661e380a5eb34543b0cc67bd62c
Bernard Normier <bernard@zeroc.com>
2021-12-24 14:21:09 -0500

    Move Ice11Encoding to Slice.Internal

--- src/IceRpc/Slice/Internal/Ice11Encoding.cs
+++ src/IceRpc/Slice/Internal/Ice11Encoding.cs
@@ -1,24 +1,16 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using System.IO.Pipelines;
-
-namespace IceRpc.Slice
+namespace IceRpc.Slice.Internal
 {
     /// <summary>The Ice 1.1 encoding class.</summary>
-    public sealed class Ice11Encoding : IceEncoding
+    internal sealed class Ice11Encoding : IceEncoding
     {
         /// <summary>The Ice 1.1 encoding singleton.</summary>
         internal static IceEncoding Instance { get; } = new Ice11Encoding();
 
-        internal static int DecodeFixedLengthSize(ReadOnlySpan<byte> buffer)
-        {
-            int size = IceDecoder.DecodeInt(buffer);
-            if (size < 0)
-            {
+        internal static int DecodeFixedLengthSize(ReadOnlySpan<byte> buffer) =>
+            IceDecoder.DecodeInt(buffer) is int size && size >= 0 ? size :
                 throw new InvalidDataException("received invalid negative size");
-            }
-            return size;
-        }
 
         private Ice11Encoding()
             : base(Ice11Name)
