89a845bb2500181f0b01a021d660e8e49c99e10c
Bernard Normier <bernard@zeroc.com>
2021-12-31 09:07:29 -0500

    First cut

--- src/IceRpc/Slice/BitSequence.cs
+++ src/IceRpc/Slice/BitSequence.cs
@@ -97,4 +97,117 @@ namespace IceRpc.Slice
             return (_currentByte & (1 << _index++)) != 0;
         }
     }
+
+    /// <summary>A writer for a bit sequence.</summary>
+    public ref struct BitSequenceWriter
+    {
+        private int _index; // the bit index in _spanEnumerator.Current
+
+        private SpanEnumerator _spanEnumerator;
+
+        /// <summary>Constructs a bit sequence writer over a bit sequence represented by a <see cref="SpanEnumerator"/>.
+        /// </summary>
+        public BitSequenceWriter(SpanEnumerator spanEnumerator)
+        {
+            _index = 0;
+            _spanEnumerator = spanEnumerator;
+            if (!_spanEnumerator.MoveNext())
+            {
+                throw new ArgumentException("cannot create a bit sequence writer over an empty bit sequence");
+            }
+            _spanEnumerator.Current.Fill(255);
+        }
+
+        /// <summary>Writes the bit at the current position in the underlying bit sequence and moves to the next
+        /// position.<summary>
+        /// <param name="value"><c>true</c> to set the bit and false to unset it.</param>
+        public void Write(bool value)
+        {
+            int byteIndex = _index >> 3;
+
+            if (byteIndex > _spanEnumerator.Current.Length)
+            {
+                if (!_spanEnumerator.MoveNext())
+                {
+                    throw new InvalidOperationException("cannot write past end of bit sequence");
+                }
+                _spanEnumerator.Current.Fill(255);
+
+                _index -= byteIndex << 3;
+                byteIndex = _index >> 3;
+            }
+
+            if (!value)
+            {
+                // unset bit
+                _spanEnumerator.Current[byteIndex] =
+                    (byte)(_spanEnumerator.Current[byteIndex] & ~(1 << (_index & 0x7)));
+            }
+
+            _index++;
+        }
+    }
+
+    /// <summary>An enumerator over one or more <see cref="Span{T}"/> of bytes.</summary>
+    public ref struct SpanEnumerator
+    {
+        /// <summary>Returns the current span.</summary>
+        public Span<byte> Current =>
+            _position switch
+            {
+                -1 => throw new InvalidOperationException(),
+                0 => _firstSpan,
+                1 => _secondSpan,
+                _ => _additionalMemory![_position - 2].Span
+            };
+
+        private readonly Span<byte> _firstSpan;
+        private readonly Span<byte> _secondSpan;
+        private readonly IList<Memory<byte>>? _additionalMemory;
+        private int _position = -1;
+
+        /// <summary>Moves to the next span.</summary>
+        /// <returns><c>true</c> when the operation was successful, and <c>false</c> when the current span is the last
+        /// span.</returns>
+        public bool MoveNext()
+        {
+            switch (_position)
+            {
+                case -1:
+                    _position = 0;
+                    return true;
+                case 0:
+                    if (_secondSpan.Length > 0)
+                    {
+                        _position = 1;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                default:
+                    if (_additionalMemory != null && _additionalMemory.Count > _position - 3)
+                    {
+                        _position += 1;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+            }
+        }
+
+        /// <summary>Constructs a span enumerator.</summary>
+        public SpanEnumerator(
+            Span<byte> firstSpan,
+            Span<byte> secondSpan = default,
+            IList<Memory<byte>>? additionalMemory = null)
+        {
+            _firstSpan = firstSpan;
+            _secondSpan = secondSpan;
+            _additionalMemory = additionalMemory;
+        }
+    }
 }
