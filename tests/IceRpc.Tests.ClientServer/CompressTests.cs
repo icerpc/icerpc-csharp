17c59ffb0f992e18c2396a5ac312ca4249e4dd46
Bernard Normier <bernard@zeroc.com>
2022-01-22 17:21:12 -0500

    Test stream

--- tests/IceRpc.Tests.ClientServer/CompressTests.cs
+++ tests/IceRpc.Tests.ClientServer/CompressTests.cs
@@ -13,12 +13,16 @@ namespace IceRpc.Tests.ClientServer
     [Parallelizable(ParallelScope.All)]
     public sealed class CompressTests
     {
-        private static readonly byte[] _payload =
+        private static readonly byte[] _mainPayload =
             Enumerable.Range(0, 4096).Select(i => (byte)(i % 256)).ToArray();
 
-        [TestCase(true)]
-        [TestCase(false)]
-        public async Task Compress_RequestPayload(bool compressPayload)
+        private static readonly byte[] _streamPayload =
+            Enumerable.Range(0, 10_000).Select(i => (byte)(i % 128)).ToArray();
+
+        [Test]
+        public async Task Compress_RequestPayload(
+            [Values(true, false)] bool compressPayload,
+            [Values(true, false)] bool withStream)
         {
             bool called = false;
 
@@ -27,20 +31,23 @@ namespace IceRpc.Tests.ClientServer
                 {
                     var router = new Router();
 
+                    int uncompressedLength = withStream ?
+                        _mainPayload.Length + _streamPayload.Length : _mainPayload.Length;
+
                     router.Use(next => new InlineDispatcher(
                         async (request, cancel) =>
                         {
-                            ReadResult readResult = await request.Payload.ReadAtLeastAsync(_payload.Length, cancel);
+                            ReadResult readResult = await request.Payload.ReadAtLeastAsync(uncompressedLength, cancel);
 
                             if (compressPayload)
                             {
                                 // Verify we received a compressed payload.
                                 Assert.That(readResult.IsCompleted, Is.True);
-                                Assert.That(readResult.Buffer.Length, Is.LessThan(_payload.Length / 2));
+                                Assert.That(readResult.Buffer.Length, Is.LessThan(uncompressedLength / 2));
                             }
                             else
                             {
-                                Assert.That(readResult.Buffer.Length, Is.EqualTo(_payload.Length));
+                                Assert.That(readResult.Buffer.Length, Is.EqualTo(uncompressedLength));
                             }
                             request.Payload.AdvanceTo(readResult.Buffer.Start); // don't consume/examine anything
                             return await next.DispatchAsync(request, cancel);
@@ -51,11 +58,20 @@ namespace IceRpc.Tests.ClientServer
                     router.Map("/", new InlineDispatcher(
                         async (request, cancel) =>
                         {
-                            ReadResult readResult = await request.Payload.ReadAtLeastAsync(_payload.Length, cancel);
+                            ReadResult readResult = await request.Payload.ReadAtLeastAsync(uncompressedLength, cancel);
 
                             byte[] received = readResult.Buffer.ToArray();
                             await request.Payload.CompleteAsync();
-                            Assert.That(received, Is.EqualTo(_payload));
+
+                            if (withStream)
+                            {
+                                Assert.That(received[0.._mainPayload.Length], Is.EqualTo(_mainPayload));
+                                Assert.That(received[_mainPayload.Length..], Is.EqualTo(_streamPayload));
+                            }
+                            else
+                            {
+                                Assert.That(received, Is.EqualTo(_mainPayload));
+                            }
                             called = true;
                             return new OutgoingResponse(request);
                         }));
@@ -66,11 +82,15 @@ namespace IceRpc.Tests.ClientServer
             var proxy = Proxy.FromConnection(serviceProvider.GetRequiredService<Connection>(), "/");
             var request = new OutgoingRequest(proxy, "compressRequestPayload")
             {
-                PayloadSource = PipeReader.Create(new ReadOnlySequence<byte>(_payload))
+                PayloadSource = PipeReader.Create(new ReadOnlySequence<byte>(_mainPayload))
             };
             if (compressPayload)
             {
                 request.Features = new FeatureCollection().With(Features.CompressPayload.Yes);
+            }
+            if (withStream)
+            {
+                request.PayloadSourceStream = PipeReader.Create(new ReadOnlySequence<byte>(_streamPayload));
             }
 
             IInvoker invoker = new Pipeline().UseCompressor();
@@ -80,9 +100,10 @@ namespace IceRpc.Tests.ClientServer
             Assert.That(response.ResultType, Is.EqualTo(ResultType.Success));
         }
 
-        [TestCase(true)]
-        [TestCase(false)]
-        public async Task Compress_ResponsePayload(bool compressPayload)
+        [Test]
+        public async Task Compress_ResponsePayload(
+            [Values(true, false)] bool compressPayload,
+            [Values(true, false)] bool withStream)
         {
             await using ServiceProvider serviceProvider = new IntegrationTestServiceCollection()
                 .AddTransient<IDispatcher>(_ =>
@@ -100,7 +121,10 @@ namespace IceRpc.Tests.ClientServer
                             new OutgoingResponse(request)
                             {
                                 Features = features,
-                                PayloadSource = PipeReader.Create(new ReadOnlySequence<byte>(_payload))
+                                PayloadSource = PipeReader.Create(new ReadOnlySequence<byte>(_mainPayload)),
+                                PayloadSourceStream = PipeReader.Create(withStream ?
+                                    new ReadOnlySequence<byte>(_streamPayload) :
+                                    ReadOnlySequence<byte>.Empty)
                             })));
                     return router;
                 })
@@ -110,22 +134,25 @@ namespace IceRpc.Tests.ClientServer
             var request = new OutgoingRequest(proxy, "compressResponsePayload");
 
             bool called = false;
+
+            int uncompressedLength = withStream ? _mainPayload.Length + _streamPayload.Length : _mainPayload.Length;
+
             Pipeline pipeline = new Pipeline().UseCompressor();
             pipeline.Use(next => new InlineInvoker(
                 async (request, cancel) =>
                 {
                     IncomingResponse response = await next.InvokeAsync(request, cancel);
 
-                    ReadResult readResult = await response.Payload.ReadAtLeastAsync(_payload.Length, cancel);
+                    ReadResult readResult = await response.Payload.ReadAtLeastAsync(uncompressedLength, cancel);
                     if (compressPayload)
                     {
                         // Verify we received a compressed payload.
                         Assert.That(readResult.IsCompleted, Is.True);
-                        Assert.That(readResult.Buffer.Length, Is.LessThan(_payload.Length / 2));
+                        Assert.That(readResult.Buffer.Length, Is.LessThan(uncompressedLength / 2));
                     }
                     else
                     {
-                        Assert.That(readResult.Buffer.Length, Is.EqualTo(_payload.Length));
+                        Assert.That(readResult.Buffer.Length, Is.EqualTo(uncompressedLength));
                     }
                     response.Payload.AdvanceTo(readResult.Buffer.Start); // don't consume/examine anything
 
@@ -133,17 +160,24 @@ namespace IceRpc.Tests.ClientServer
                     return response;
                 }));
 
-            IInvoker invoker = pipeline; // TODO: let's fix Pipeline to allow a direct InvokeAsync call
-
             Assert.That(called, Is.False);
-            IncomingResponse response = await invoker.InvokeAsync(request);
+            IncomingResponse response = await pipeline.InvokeAsync(request);
             Assert.That(called, Is.True);
             Assert.That(response.ResultType, Is.EqualTo(ResultType.Success));
 
-            ReadResult readResult = await response.Payload.ReadAtLeastAsync(_payload.Length);
+            ReadResult readResult = await response.Payload.ReadAtLeastAsync(uncompressedLength);
             byte[] received = readResult.Buffer.ToArray();
             await response.Payload.CompleteAsync();
-            Assert.That(received, Is.EqualTo(_payload));
+
+            if (withStream)
+            {
+                Assert.That(received[0.._mainPayload.Length], Is.EqualTo(_mainPayload));
+                Assert.That(received[_mainPayload.Length..], Is.EqualTo(_streamPayload));
+            }
+            else
+            {
+                Assert.That(received, Is.EqualTo(_mainPayload));
+            }
         }
     }
 }
