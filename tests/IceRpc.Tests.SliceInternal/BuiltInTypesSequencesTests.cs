6653b5b931881a3e7e98a70e007202e51ee84b1b
Reece Humphreys <reece@zeroc.com>
2022-01-26 12:12:46 -0500

    Added new tests to complete coverage for encoding and decoding extension methods

--- tests/IceRpc.Tests.SliceInternal/BuiltInTypesSequencesTests.cs
+++ tests/IceRpc.Tests.SliceInternal/BuiltInTypesSequencesTests.cs
@@ -4,6 +4,7 @@ using IceRpc.Slice;
 using IceRpc.Slice.Internal;
 using NUnit.Framework;
 using System.Buffers;
+using System.Collections.Immutable;
 
 namespace IceRpc.Tests.SliceInternal
 {
@@ -24,6 +25,9 @@ namespace IceRpc.Tests.SliceInternal
             _bufferWriter = new SingleBufferWriter(_buffer);
         }
 
+        /// <summary>Tests `EncodeSpan` and `DecodeSequence` with a value type.</summary>
+        /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
+        /// <returns>void</returns>
         [TestCase(0)]
         [TestCase(256)]
         public void BuiltInTypesSequences_FixedSizeNumeric(int size)
@@ -41,47 +45,95 @@ namespace IceRpc.Tests.SliceInternal
             Assert.That(decoder.Consumed, Is.EqualTo(sizeLength + size * sizeof(int)));
         }
 
+        /// <summary>Tests `EncodeSequenceWithBitSequence` with a value type. Tests `DecodeSequenceWithBitSequence` 
+        /// with a value type. Additionally, covers the case where count is 0 and the case where count > 0 for both
+        /// `EncodeSequenceWithBitSequence` and `DecodeSequenceWithBitSequence`.</summary>
+        /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
+        /// <returns>void</returns>
         [TestCase(0)]
         [TestCase(256)]
         public void BuiltInTypesSequences_FixedSizeNumeric_Optional(int size)
         {
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
-            int[] p1 = Enumerable.Range(0, size).ToArray();
-            encoder.EncodeSequenceWithBitSequence(p1, (ref IceEncoder encoder, int v) => encoder.EncodeInt(v));
-            int[] r1 = decoder.DecodeSequenceWithBitSequence((ref IceDecoder decoder) => decoder.DecodeInt());
-
+            IList<int?> p1 = Enumerable.Range(0, size).Select(i => (int?)(i % 2 == 0 ? null : i)).ToList();
+    
+            encoder.EncodeSequenceWithBitSequence(p1, (ref IceEncoder encoder, int? v) => encoder.EncodeInt(v!.Value));
+            int?[] r1 = decoder.DecodeSequenceWithBitSequence((ref IceDecoder decoder) => decoder.DecodeInt() as int?);
+            
             Assert.That(p1, Is.EqualTo(r1));
             Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
         }
 
+        /// <summary>Tests `EncodeSequence` and `DecodeSequence` with a reference type. Also tests `DecodeString`.</summary>
+        /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
+        /// <returns>void</returns>
         [TestCase(0)]
         [TestCase(256)]
         public void BuiltInTypesSequences_String(int size)
-        {
+        {  
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
             IEnumerable<string> p1 = Enumerable.Range(0, size).Select(i => $"string-{i}");
-            encoder.EncodeSequence(
-                p1,
-                (ref IceEncoder encoder, string value) => encoder.EncodeString(value));
+            
+            encoder.EncodeSequence(p1, (ref IceEncoder encoder, string value) => encoder.EncodeString(value));
             IEnumerable<string> r1 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeString());
 
             Assert.That(p1, Is.EqualTo(r1));
             Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
         }
 
+        /// <summary>Tests `EncodeSequenceWithBitSequence` with a reference type. Tests `DecodeSequenceWithBitSequence` 
+        /// with a reference type. Additionally, covers the case where count is 0 and the case where count > 0 for both
+        /// `EncodeSequenceWithBitSequence` and `DecodeSequenceWithBitSequence`</summary>
+        /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
+        /// <returns>void</returns>
         [TestCase(0)]
         [TestCase(256)]
         public void BuiltInTypesSequences_String_Optional(int size)
         {
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
-            IEnumerable<string> p1 = Enumerable.Range(0, size).Select(i => $"string-{i}");
-            encoder.EncodeSequenceWithBitSequence(p1, (ref IceEncoder encoder, string value) => encoder.EncodeString(value));
-            IEnumerable<string> r1 = decoder.DecodeSequenceWithBitSequence((ref IceDecoder decoder) => decoder.DecodeString());
+            IEnumerable<string?> p1 = Enumerable.Range(0, size).Select(i => (string?)(i % 2 == 0 ? null : $"string-{i}"));
+
+            encoder.EncodeSequenceWithBitSequence(p1, (ref IceEncoder encoder, string? value) => encoder.EncodeString(value!));
+            string[]? r1 = decoder.DecodeSequenceWithBitSequence((ref IceDecoder decoder) => decoder.DecodeString());
 
             Assert.That(p1, Is.EqualTo(r1));
+            Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
+        }
+
+        /// <summary>Tests `EncodeSequence` and `DecodeSequence` with a value type. Also tests `EncodeVarLong`
+        /// and `DecodeVarLong` which consequently tests `GetVarLongEncodedSizeExponent` and `DecodeVarLong`.
+        /// Finally tests the `T[]`, `ImmutableArray<T>`, and `ArraySegment<T>` cases for `EncodeSequence`</summary>
+        /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
+        /// <returns>void</returns>
+        [TestCase(0)]
+        [TestCase(256)]
+        public void BuiltInTypesSequences_Long(int size){
+            var encoder = new IceEncoder(_bufferWriter, _encoding);
+            var decoder = new IceDecoder(_buffer, _encoding);
+
+            IEnumerable<long> p1 = Enumerable.Range(0, size).Select(i => (long)i);
+            ImmutableArray<long> p2 = ImmutableArray.CreateRange(p1);
+            ArraySegment<long> p3 = new ArraySegment<long>(p1.ToArray());
+            long[] p4 = p1.ToArray();
+
+            encoder.EncodeSequence(p1, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
+            encoder.EncodeSequence(p2, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
+            encoder.EncodeSequence(p3, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
+            encoder.EncodeSequence(p4, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
+
+            IEnumerable<long> r1 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
+            IEnumerable<long> r2 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
+            IEnumerable<long> r3 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
+            IEnumerable<long> r4 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
+
+            Assert.That(p1, Is.EqualTo(r1));
+            Assert.That(p2, Is.EqualTo(r2));
+            Assert.That(p3, Is.EqualTo(r3));
+            Assert.That(p4, Is.EqualTo(r4));
+
             Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
         }
     }
