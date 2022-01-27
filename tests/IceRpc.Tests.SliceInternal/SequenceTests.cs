b88c2bf1313510ca268222764ae656412c657e12
Reece Humphreys <reece@zeroc.com>
2022-01-27 14:36:14 -0500

    Renamed Sequence tests, fixed c sharp style issues, added missing tests for full code coverage

--- tests/IceRpc.Tests.SliceInternal/SequenceTests.cs
+++ tests/IceRpc.Tests.SliceInternal/SequenceTests.cs
@@ -12,13 +12,13 @@ namespace IceRpc.Tests.SliceInternal
     [TestFixture("1.1")]
     [TestFixture("2.0")]
     [Parallelizable(scope: ParallelScope.All)]
-    public class SequencesTests
+    public class SequenceTests
     {
         private readonly Memory<byte> _buffer;
         private readonly IceEncoding _encoding;
         private readonly SingleBufferWriter _bufferWriter;
 
-        public SequencesTests(string encoding)
+        public SequenceTests(string encoding)
         {
             _encoding = IceEncoding.FromString(encoding);
             _buffer = new byte[1024 * 1024];
@@ -30,11 +30,12 @@ namespace IceRpc.Tests.SliceInternal
         /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
         [TestCase(0)]
         [TestCase(256)]
-        public void Sequences_FixedSizeNumeric(int size)
+        public void Sequence_FixedSizeNumeric(int size)
         {
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
             int[] p1 = Enumerable.Range(0, size).ToArray();
+            
             encoder.EncodeSpan(new ReadOnlySpan<int>(p1));
             int[] r1 = decoder.DecodeSequence<int>();
 
@@ -51,7 +52,7 @@ namespace IceRpc.Tests.SliceInternal
         /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
         [TestCase(0)]
         [TestCase(256)]
-        public void Sequences_FixedSizeNumeric_Optional(int size)
+        public void Sequence_FixedSizeNumeric_Optional(int size)
         {
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
@@ -64,20 +65,23 @@ namespace IceRpc.Tests.SliceInternal
             Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
         }
 
+
         /// <summary>Tests <see cref="IceEncoderExtensions.EncodeSequence"/> and 
         /// <see cref="IceDecoderExtensions.DecodeSequence"/> with a reference type. 
         /// Also tests <see cref="IceDecoder.DecodeString"/>.</summary>
         /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
         [TestCase(0)]
         [TestCase(256)]
-        public void Sequences_String(int size)
+        public void Sequence_String(int size)
         {  
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
             IEnumerable<string> p1 = Enumerable.Range(0, size).Select(i => $"string-{i}");
             
             encoder.EncodeSequence(p1, (ref IceEncoder encoder, string value) => encoder.EncodeString(value));
-            IEnumerable<string> r1 = decoder.DecodeSequence(minElementSize:1, (ref IceDecoder decoder) => decoder.DecodeString());
+            IEnumerable<string> r1 = decoder.DecodeSequence(
+                minElementSize:1,
+                (ref IceDecoder decoder) => decoder.DecodeString());
 
             Assert.That(p1, Is.EqualTo(r1));
             Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
@@ -91,14 +95,46 @@ namespace IceRpc.Tests.SliceInternal
         /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
         [TestCase(0)]
         [TestCase(256)]
-        public void Sequences_String_Optional(int size)
+        public void Sequence_String_Optional(int size)
         {
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
-            IEnumerable<string?> p1 = Enumerable.Range(0, size).Select(i => (string?)(i % 2 == 0 ? null : $"string-{i}"));
-
-            encoder.EncodeSequenceWithBitSequence(p1, (ref IceEncoder encoder, string? value) => encoder.EncodeString(value!));
+            IEnumerable<string?> p1 = Enumerable
+                .Range(0, size)
+                .Select(i => (string?)(i % 2 == 0 ? null : $"string-{i}"));
+            
+            encoder.EncodeSequenceWithBitSequence(
+                p1,
+                (ref IceEncoder encoder, string? value) => encoder.EncodeString(value!));
             string[] r1 = decoder.DecodeSequenceWithBitSequence((ref IceDecoder decoder) => decoder.DecodeString());
+
+            Assert.That(p1, Is.EqualTo(r1));
+            Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
+        }
+
+        /// <summary>Tests <see cref="IceEncoderExtensions.EncodeSequenceWithBitSequence"/> with a reference type. 
+        /// Tests <see cref="IceDecoderExtensions.DecodeSequenceWithBitSequence"/> with a reference type. Additionally, 
+        /// covers the case where count is 0 and the case where count > 0 for both
+        /// <see cref="IceEncoderExtensions.EncodeSequenceWithBitSequence"/> and 
+        /// <see cref="IceDecoderExtensions.DecodeSequenceWithBitSequence"/>. Finally, covers the Sequence factory
+        // constructor for <see cref="IceDecoderExtensions.DecodeSequenceWithBitSequence"/>.</summary>
+        /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
+        [TestCase(0)]
+        [TestCase(256)]
+        public void Sequence_String_Optional_Factory(int size)
+        {
+            var encoder = new IceEncoder(_bufferWriter, _encoding);
+            var decoder = new IceDecoder(_buffer, _encoding);
+            IEnumerable<string?> p1 = Enumerable
+                .Range(0, size)
+                .Select(i => (string?)(i % 2 == 0 ? null : $"string-{i}"));
+
+            encoder.EncodeSequenceWithBitSequence(
+                p1,
+                (ref IceEncoder encoder, string? value) => encoder.EncodeString(value!));
+            List<string> r1 = decoder.DecodeSequenceWithBitSequence(
+                (int i) => new List<string>(i),
+                (ref IceDecoder decoder) => decoder.DecodeString());
 
             Assert.That(p1, Is.EqualTo(r1));
             Assert.That(decoder.Consumed, Is.EqualTo(_bufferWriter.WrittenBuffer.Length));
@@ -114,7 +150,8 @@ namespace IceRpc.Tests.SliceInternal
         /// <param name="size">An int used to specify how many elements to generate in the sequence.</param>
         [TestCase(0)]
         [TestCase(256)]
-        public void Sequences_VarLong(int size){
+        public void Sequence_Long(int size)
+        {
             var encoder = new IceEncoder(_bufferWriter, _encoding);
             var decoder = new IceDecoder(_buffer, _encoding);
 
@@ -123,15 +160,18 @@ namespace IceRpc.Tests.SliceInternal
             ArraySegment<long> p3 = new ArraySegment<long>(p1.ToArray());
             long[] p4 = p1.ToArray();
 
-            encoder.EncodeSequence(p1, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
-            encoder.EncodeSequence(p2, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
-            encoder.EncodeSequence(p3, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
-            encoder.EncodeSequence(p4, (ref IceEncoder encoder, long value) => encoder.EncodeVarLong(value));
+            encoder.EncodeSequence(p1);
+            encoder.EncodeSequence(p2);
+            encoder.EncodeSequence(p3);
+            encoder.EncodeSequence(p4);
 
-            long[] r1 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
-            long[] r2 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
-            long[] r3 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
-            long[] r4 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeVarLong());
+            long[] r1 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeLong());
+            long[] r2 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeLong());
+            long[] r3 = decoder.DecodeSequence(1, (ref IceDecoder decoder) => decoder.DecodeLong());
+            List<long> r4 = decoder.DecodeSequence(
+                1,
+                (int i) => new List<long>(i),
+                (ref IceDecoder decoder) => decoder.DecodeLong());
 
             Assert.That(p1, Is.EqualTo(r1));
             Assert.That(p2, Is.EqualTo(r2));
