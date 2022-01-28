9f873e6e525d464ac549115c7fd63837781fd9a5
Bernard Normier <bernard@zeroc.com>
2022-01-28 15:40:43 -0500

    Move DecodeNullablePrx extension method

--- src/IceRpc/Slice/IPrx.cs
+++ src/IceRpc/Slice/IPrx.cs
@@ -2,21 +2,11 @@
 
 namespace IceRpc.Slice
 {
-    /// <summary>The common interface of all concrete typed proxies. It gives access to an untyped proxy object that can
-    /// send requests to a remote IceRPC service.</summary>
+    /// <summary>The common interface of all Prx structs. It gives access to an untyped proxy object that can send
+    /// requests to a remote IceRPC service.</summary>
     public interface IPrx
     {
         /// <summary>Gets the proxy object, or sets this proxy object during initialization.</summary>
         Proxy Proxy { get; init; }
-    }
-
-    /// <summary>Provides extension methods for SliceDecoder.</summary>
-    public static class SliceDecoderPrxExtensions
-    {
-        /// <summary>Decodes a nullable typed proxy.</summary>
-        /// <param name="decoder">The decoder.</param>
-        /// <returns>The decoded proxy, or null.</returns>
-        public static T? DecodeNullablePrx<T>(ref this SliceDecoder decoder) where T : struct, IPrx =>
-            decoder.DecodeNullableProxy() is Proxy proxy ? new T { Proxy = proxy } : null;
     }
 }
--- src/IceRpc/Slice/SliceDecoderExtensions.cs
+++ src/IceRpc/Slice/SliceDecoderExtensions.cs
@@ -97,6 +97,12 @@ namespace IceRpc.Slice
             }
         }
 
+        /// <summary>Decodes a nullable Prx struct.</summary>
+        /// <param name="decoder">The Slice decoder.</param>
+        /// <returns>The decoded Prx struct, or null.</returns>
+        public static T? DecodeNullablePrx<T>(ref this SliceDecoder decoder) where T : struct, IPrx =>
+            decoder.DecodeNullableProxy() is Proxy proxy ? new T { Proxy = proxy } : null;
+
         /// <summary>Decodes a sequence of fixed-size numeric values.</summary>
         /// <param name="decoder">The Slice decoder.</param>
         /// <param name="checkElement">A delegate used to check each element of the array (optional).</param>
