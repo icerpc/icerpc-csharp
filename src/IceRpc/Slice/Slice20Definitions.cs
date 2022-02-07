ea5e5a112aead8ea8d250437e7c365d835e9740c
Austin Henriksen <austin@zeroc.com>
2022-02-07 15:46:05 -0500

    Added 'TagEndMarker' for 2.0 encoding.

---
+++ src/IceRpc/Slice/Slice20Definitions.cs
@@ -0,0 +1,11 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+namespace IceRpc.Slice
+{
+    /// <summary>Enumerations and constants used by the Slice 2.0 encoding.</summary>
+    public static class Slice20Definitions
+    {
+        /// <summary>A marker that indicates the end of a (possibly empty) sequence of tagged members.</summary>
+        public const int TagEndMarker = -1;
+    }
+}
