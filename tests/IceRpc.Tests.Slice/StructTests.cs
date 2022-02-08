cc1d1b797c9d1517909b1e5ef39612ed21f635f5
Austin Henriksen <austin@zeroc.com>
2022-02-08 16:02:02 -0500

    Split struct test into Struct and CompactStruct tests.

--- tests/IceRpc.Tests.Slice/StructTests.cs
+++ tests/IceRpc.Tests.Slice/StructTests.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
-using IceRpc.Slice;
 
 namespace IceRpc.Tests.Slice
 {
@@ -41,44 +40,6 @@ namespace IceRpc.Tests.Slice
                                               new MyStruct(3, 4)));
         }
 
-        [Test]
-        public async Task CompactStruct_OperationsAsync()
-        {
-            // TODO Parse below should not use a connection with a different endpoint
-            await TestAsync((p1, p2) => _prx.OpMyCompactStructAsync(p1, p2),
-                            new MyCompactStruct(1, 2),
-                            new MyCompactStruct(3, 4));
-            await TestAsync((p1, p2) => _prx.OpAnotherCompactStructAsync(p1, p2),
-                            new AnotherCompactStruct("hello",
-                                              OperationsPrx.Parse("icerpc://foo/bar"),
-                                              MyEnum.enum1,
-                                              new MyCompactStruct(1, 2)),
-                            new AnotherCompactStruct("world",
-                                              OperationsPrx.Parse("icerpc://foo/bar"),
-                                              MyEnum.enum2,
-                                              new MyCompactStruct(3, 4)));
-        }
-
-        [Test]
-        public async Task CompactStruct_CustomTypeAsync()
-        {
-            TimeSpan t1 = await _prx.OpMyTimeSpan1Async(TimeSpan.FromMilliseconds(100));
-            Assert.AreEqual(t1, TimeSpan.FromMilliseconds(100));
-
-            (TimeSpan t2, TimeSpan t3) = await _prx.OpMyTimeSpan2Async(
-                TimeSpan.FromMilliseconds(100),
-                TimeSpan.FromMilliseconds(200));
-            Assert.AreEqual(t2, TimeSpan.FromMilliseconds(100));
-            Assert.AreEqual(t3, TimeSpan.FromMilliseconds(200));
-
-            MyStructWithCustomType s1 = await _prx.OpMyStructWithCustomTypeAsync(new MyStructWithCustomType(t1));
-            Assert.AreEqual(s1.Timespan, TimeSpan.FromMilliseconds(100));
-
-            LinkedList<int> l1 = await _prx.OpMyList1Async(new LinkedList<int>(new int[] { 1, 2, 3 }));
-            Assert.AreEqual(l1, new LinkedList<int>(new int[] { 1, 2, 3 }));
-
-        }
-
         static async Task TestAsync<T>(Func<T, T, Task<(T, T)>> invoker, T p1, T p2)
         {
             (T r1, T r2) = await invoker(p1, p2);
@@ -99,45 +60,6 @@ namespace IceRpc.Tests.Slice
                 AnotherStruct p2,
                 Dispatch dispatch,
                 CancellationToken cancel) => new((p1, p2));
-
-            public ValueTask<(MyCompactStruct R1, MyCompactStruct R2)> OpMyCompactStructAsync(
-                MyCompactStruct p1,
-                MyCompactStruct p2,
-                Dispatch dispatch,
-                CancellationToken cancel) => new((p1, p2));
-
-            public ValueTask<(AnotherCompactStruct R1, AnotherCompactStruct R2)> OpAnotherCompactStructAsync(
-                AnotherCompactStruct p1,
-                AnotherCompactStruct p2,
-                Dispatch dispatch,
-                CancellationToken cancel) => new((p1, p2));
-
-            public ValueTask<TimeSpan> OpMyTimeSpan1Async(
-                TimeSpan p1,
-                Dispatch dispatch,
-                CancellationToken cancel = default) => new(p1);
-
-            public ValueTask<(TimeSpan R1, TimeSpan R2)> OpMyTimeSpan2Async(
-                TimeSpan p1,
-                TimeSpan p2,
-                Dispatch dispatch,
-                CancellationToken cancel = default) => new((p1, p2));
-
-            public ValueTask<MyStructWithCustomType> OpMyStructWithCustomTypeAsync(
-                MyStructWithCustomType p1,
-                Dispatch dispatch,
-                CancellationToken cancel = default) => new(p1);
-
-            public ValueTask<LinkedList<int>> OpMyList1Async(
-                LinkedList<int> p1,
-                Dispatch dispatch,
-                CancellationToken cancel = default) => new(p1);
-
-            public ValueTask<(LinkedList<int> R1, LinkedList<int> R2)> OpMyList2Async(
-                LinkedList<int> p1,
-                LinkedList<int> p2,
-                Dispatch dispatch,
-                CancellationToken cancel = default) => new((p1, p2));
         }
     }
 
@@ -150,34 +72,5 @@ namespace IceRpc.Tests.Slice
         public override readonly int GetHashCode() => I.GetHashCode();
 
         public override readonly string ToString() => $"{I + J}";
-    }
-
-    public partial record struct MyCompactStruct
-    {
-        // Test overrides
-
-        public readonly bool Equals(MyCompactStruct other) => I == other.I;
-
-        public override readonly int GetHashCode() => I.GetHashCode();
-
-        public override readonly string ToString() => $"{I + J}";
-    }
-
-    public static class MyTimeSpanExtensions
-    {
-        public static void EncodeMyTimeSpan(ref SliceEncoder encoder, TimeSpan value) =>
-            encoder.EncodeVarLong(checked((long)value.TotalMilliseconds));
-
-        public static TimeSpan DecodeMyTimeSpan(ref SliceDecoder decoder) =>
-            TimeSpan.FromMilliseconds(decoder.DecodeVarLong());
-    }
-
-    public static class MyListExtensions
-    {
-        public static void EncodeMyList(ref SliceEncoder encoder, LinkedList<int> value) =>
-            encoder.EncodeSequence(value.ToArray());
-
-        public static LinkedList<int> DecodeMyList(ref SliceDecoder decoder) =>
-            new(decoder.DecodeSequence<int>(null).Reverse());
     }
 }
