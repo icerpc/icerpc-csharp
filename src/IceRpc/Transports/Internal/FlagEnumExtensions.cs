4eb31948ec2a01066be62b4d91192c2d614e108d
Benoit Foucher <benoit@zeroc.com>
2022-02-11 17:53:51 +0100

    Missing file

---
+++ src/IceRpc/Transports/Internal/FlagEnumExtensions.cs
@@ -0,0 +1,48 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using System.Runtime.CompilerServices;
+
+namespace IceRpc.Transports.Internal
+{
+    /// <summary>The flag enum extensions help with atomically set/get enumeration flags on a integer value. The
+    /// enumeration underlying type must be an integer as well.</summary>
+    internal static class FlagEnumExtensions
+    {
+        internal static bool TrySetFlag<T>(this ref int source, T value) where T : Enum
+        {
+            int state = Unsafe.As<T, int>(ref value);
+            int previousState = Interlocked.Or(ref source, state);
+            return previousState != (previousState | state);
+        }
+
+        internal static bool TrySetFlag<T>(this ref int source, T value, out T newValue) where T : Enum
+        {
+            int state = Unsafe.As<T, int>(ref value);
+            int previousState = Interlocked.Or(ref source, state);
+            int newState = previousState | state;
+            newValue = Unsafe.As<int, T>(ref newState);
+            return previousState != newState;
+        }
+
+        internal static bool HasFlag<T>(this ref int source, T value) where T : Enum =>
+            (Thread.VolatileRead(ref source) & (int)(IConvertible)value) > 0;
+
+        internal static void ClearFlag<T>(this ref int source, T value) where T : Enum
+        {
+            int state = Unsafe.As<T, int>(ref value);
+            int previousState = Interlocked.And(ref source, ~state);
+            if (previousState == (previousState & ~state))
+            {
+                throw new InvalidOperationException("state was already cleared");
+            }
+        }
+
+        internal static void SetState<T>(this ref int source, T value) where T : Enum
+        {
+            if (!TrySetFlag(ref source, value))
+            {
+                throw new InvalidOperationException("state was already set");
+            }
+        }
+    }
+}
