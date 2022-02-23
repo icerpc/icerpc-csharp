9daa2464357b5d1bcd09272332362a19b3c17c80
Austin Henriksen <austin@zeroc.com>
2022-02-23 14:23:48 -0500

    Updated tests to use the new 'EncodeTrait' method.

--- tests/IceRpc.Tests/Slice/SlicingTests.cs
+++ tests/IceRpc.Tests/Slice/SlicingTests.cs
@@ -194,7 +194,7 @@ public class SlicingTests
         var encoder = new SliceEncoder(bufferWriter, Encoding.Slice11, classFormat: FormatType.Sliced);
 
         var p1 = new MyMostDerivedException("most-derived", "derived", "base");
-        encoder.EncodeException(p1);
+        p1.EncodeTrait(ref encoder);
         buffer = bufferWriter.WrittenBuffer;
 
         // Create an activator that knows about all the types using in this test
@@ -262,7 +262,7 @@ public class SlicingTests
             buffer = new byte[1024];
             bufferWriter = new SingleBufferWriter(buffer);
             var encoder = new SliceEncoder(bufferWriter, Encoding.Slice11, classFormat: FormatType.Sliced);
-            encoder.EncodeException(r);
+            r.EncodeTrait(ref encoder);
         }, Throws.TypeOf<InvalidOperationException>());
     }
 
