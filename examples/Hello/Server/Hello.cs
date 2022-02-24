bf38ebb940d6a30ff7563fb1bedc41fc3bac9a09
Reece Humphreys <reece@zeroc.com>
2022-02-24 15:45:11 -0500

    Removed optional string + updated Hello.slice syntax

--- examples/Hello/Server/Hello.cs
+++ examples/Hello/Server/Hello.cs
@@ -6,7 +6,7 @@ namespace Demo;
 
 public class Hello : Service, IHello
 {
-    public ValueTask<string?> SayHelloAsync(string? greeting, Dispatch dispatch, CancellationToken cancel)
+    public ValueTask<string> SayHelloAsync(string greeting, Dispatch dispatch, CancellationToken cancel)
     {
         Console.Out.WriteLine("Hello World!");
         return new(greeting + ", server!");
