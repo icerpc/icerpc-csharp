d06583fa5b653eba58ca00aa0ace28cfb945f47d
Reece Humphreys <reece@zeroc.com>
2022-02-24 10:49:48 -0500

    Added dotnet template for creating simple hello client and server

---
+++ src/ProjectTemplates/server/Server/Hello.cs
@@ -0,0 +1,12 @@
+using IceRpc.Slice;
+
+namespace Demo;
+
+public class Hello : Service, IHello
+{
+    public ValueTask<string?> SayHelloAsync(string? greeting, Dispatch dispatch, CancellationToken cancel)
+    {
+        Console.Out.WriteLine("Hello World!");
+        return new(greeting + ", server!");
+    }
+}
---
+++ src/ProjectTemplates/server/Server/Program.cs
@@ -0,0 +1,28 @@
+using Demo;
+using IceRpc;
+
+try
+{
+    await using var server = new Server
+    {
+        Endpoint = "icerpc://127.0.0.1:10000?tls=false",
+        Dispatcher = new Hello()
+    };
+
+    // Destroy the server on Ctrl+C or Ctrl+Break
+    Console.CancelKeyPress += (sender, eventArgs) =>
+    {
+        eventArgs.Cancel = true;
+        _ = server.ShutdownAsync();
+    };
+
+    server.Listen();
+    await server.ShutdownComplete;
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine(ex);
+    return 1;
+}
+
+return 0;
