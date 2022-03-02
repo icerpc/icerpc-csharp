58a8fddc7cc1cf6f995f4843c3108e9f7bd1be4c
Reece Humphreys <reece@zeroc.com>
2022-03-02 13:15:52 -0500

    Update examples/Bidir/Server/AlertSystem.cs
    Co-authored-by: Jose  <pepone@users.noreply.github.com>

--- examples/Bidir/Server/AlertSystem.cs
+++ examples/Bidir/Server/AlertSystem.cs
@@ -6,7 +6,7 @@ public class AlertSystem: Service, IAlertSystem
 {
     public async ValueTask AddObserverAsync(AlertRecipientPrx alertRecipient, Dispatch dispatch, CancellationToken cancel)
     {
-        System.Threading.Thread.Sleep(3000);
+        await Task.Delay(3000, cancel);
         bool response = await alertRecipient.AlertAsync();
         string didHandle = (response ? "did" : "did not");
         Console.WriteLine($"Alert Recipient {didHandle} accept the alert");
