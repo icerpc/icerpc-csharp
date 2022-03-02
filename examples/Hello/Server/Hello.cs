ace2a40a9246a3c5c66d306adc6fb758af1db9d3
Reece Humphreys <reece@zeroc.com>
2022-03-02 13:26:20 -0500

    Added server response

--- examples/Hello/Server/Hello.cs
+++ examples/Hello/Server/Hello.cs
@@ -6,9 +6,9 @@ namespace Demo;
 
 public class Hello : Service, IHello
 {
-    public ValueTask SayHelloAsync(string name, Dispatch dispatch, CancellationToken cancel)
+    public ValueTask<string> SayHelloAsync(string name, Dispatch dispatch, CancellationToken cancel)
     {
-        Console.WriteLine($"Hello, {name}!");
-        return default;
+        Console.WriteLine($"{name} says Hello!");
+        return new($"Cheers {name}!");
     }
 }
