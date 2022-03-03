a132a0f9b35531560318f430fa19f212a9fcc809
Joe George <joe@zeroc.com>
2022-03-03 15:47:26 -0500

    Simplify DispatchAsync

--- src/IceRpc/Slice/Service.cs
+++ src/IceRpc/Slice/Service.cs
@@ -88,16 +88,9 @@ namespace IceRpc.Slice
         public ValueTask IcePingAsync(Dispatch dispatch, CancellationToken cancel) => default;
 
         /// <inheritdoc/>
-        public ValueTask<OutgoingResponse> DispatchAsync(IncomingRequest request, CancellationToken cancel)
-        {
-            if (_dispatchMethods.TryGetValue(request.Operation, out DispatchMethod? dispatchMethod))
-            {
-                return dispatchMethod(this, request, cancel);
-            }
-            else
-            {
+        public ValueTask<OutgoingResponse> DispatchAsync(IncomingRequest request, CancellationToken cancel) =>
+            _dispatchMethods.TryGetValue(request.Operation, out DispatchMethod? dispatchMethod) ?
+                dispatchMethod(this, request, cancel) :
                 throw new DispatchException(DispatchErrorCode.OperationNotFound);
-            }
-        }
     }
 }
