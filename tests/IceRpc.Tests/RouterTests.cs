71bd7a85a42862efd15a87261298f7bea81c49d1
Jose <jose@zeroc.com>
2022-03-05 01:07:33 +0100

    Review comment fixes

--- tests/IceRpc.Tests/RouterTests.cs
+++ tests/IceRpc.Tests/RouterTests.cs
@@ -9,32 +9,26 @@ namespace IceRpc.Tests;
 [Parallelizable(scope: ParallelScope.All)]
 public class RouterTests
 {
-    private static readonly IDispatcher _defaultDispatcher = new InlineDispatcher(
-        (request, cancel) => throw new DispatchException(
-            DispatchErrorCode.ServiceNotFound,
-            RetryPolicy.OtherReplica));
-
-
-    /// <summary>Ensure that <see cref="Router.Mount(string, IDispatcher)"/> fails when using an invalid path.</summary>
+    /// <summary>Verifies that <see cref="Router.Mount(string, IDispatcher)"/> fails when using an invalid path.</summary>
     /// <param name="path"></param>
     [Test]
-    public void Mount_an_invalid_path_fails()
+    public void Mounting_an_invalid_path_fails()
     {
         Router router = new();
 
-        Assert.Throws<FormatException>(() => router.Mount("foo", _defaultDispatcher));
+        Assert.Throws<FormatException>(() => router.Mount("foo", ConnectionOptions.DefaultDispatcher));
     }
 
-    /// <summary>Ensure that <see cref="Router.Map(string, IDispatcher)"/> fails when using an invalid path.</summary>
+    /// <summary>Verifies that <see cref="Router.Map(string, IDispatcher)"/> fails when using an invalid path.</summary>
     [Test]
-    public void Map_an_invalid_path_fails()
+    public void Maping_an_invalid_path_fails()
     {
         Router router = new();
 
-        Assert.Throws<FormatException>(() => router.Mount("foo", _defaultDispatcher));
+        Assert.Throws<FormatException>(() => router.Mount("foo", ConnectionOptions.DefaultDispatcher));
     }
 
-    /// <summary>Ensure that creating a <see cref="Router"/> with an invalid prefix fails.</summary>
+    /// <summary>Verifies that creating a <see cref="Router"/> with an invalid prefix fails.</summary>
     [Test]
     public void Creating_a_router_with_invalid_prefix_fails() =>
         Assert.Throws<FormatException>(() => new Router("foo"));
