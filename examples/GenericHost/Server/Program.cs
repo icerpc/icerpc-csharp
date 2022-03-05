b2d5ca1a315323baa06b29e1d21c4d69f80b7cc5
Jose <jose@zeroc.com>
2022-03-05 18:07:56 +0100

    Fix review comments

--- examples/GenericHost/Server/Program.cs
+++ examples/GenericHost/Server/Program.cs
@@ -1,6 +1,5 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using Demo;
 using IceRpc;
 using IceRpc.Configure;
 using IceRpc.Transports;
@@ -12,72 +11,78 @@ using Microsoft.Extensions.Options;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-namespace ServerApp
+namespace Demo;
+
+public class Program
 {
-    public class Program
-    {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-        /// <summary>Creates the HostBuilder used to build the .NET Generic Host.</summary>
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                // Set the content root path to the build directory of the server (e.g.: Server/bin/Debug/net6.0)
-                .UseContentRoot(AppContext.BaseDirectory)
+    /// <summary>Creates the HostBuilder used to build the .NET Generic Host.</summary>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            // Set the content root path to the build directory of the server (e.g.: Server/bin/Debug/net6.0)
+            .UseContentRoot(AppContext.BaseDirectory)
 
-                // Configure the .NET Generic Host services.
-                .ConfigureServices((hostContext, services) =>
-                {
-                    // Add the ServerHostedService to the hosted services of the .NET Generic Host.
-                    services.AddHostedService<ServerHostedService>();
-
-                    // Get the ServerOptions from the configuration and add it to the generic host options. The DI
-                    // container will inject it in services that require an IOptions<ServerOptions> dependency.
-                    services
-                        .AddOptions()
-                        .AddOptions<ServerOptions>()
-                        .Bind(hostContext.Configuration.GetSection("Server"))
-                        .Configure(serverOptions =>
-                            {
-                                // Configure the authentication options
-                                serverOptions.AuthenticationOptions = new SslServerAuthenticationOptions()
-                                {
-                                    ServerCertificate = new X509Certificate2(
-                                        hostContext.Configuration.GetValue<string>(
-                                            "Server:AuthenticationOptions:CertificateFile"),
-                                        hostContext.Configuration.GetValue<string>(
-                                            "Server:AuthenticationOptions:CertificatePassword"))
-                                };
-                            })
-                        .Configure((ServerOptions serverOptions, ILoggerFactory loggerFactory) =>
-                            {
-                                // Configure the dispatcher
-                                var router = new Router();
-                                router.UseLogger(loggerFactory);
-                                router.UseTelemetry(new TelemetryOptions { LoggerFactory = loggerFactory });
-                                router.Map<IHello>(new Hello());
-                                serverOptions.Dispatcher = router;
-                            });
-                });
-
-        /// <summary>The server hosted service is ran and managed by the .NET Generic Host</summary>
-        private class ServerHostedService : IHostedService
-        {
-            // The IceRPC server to accept connections from IceRPC clients.
-            private readonly Server _server;
-
-            public ServerHostedService(IOptions<ServerOptions> options) =>
-                _server = new Server(options.Value);
-
-            public Task StartAsync(CancellationToken cancellationToken)
+            // Configure the .NET Generic Host services.
+            .ConfigureServices((hostContext, services) =>
             {
-                // Start listening for client connections.
-                _server.Listen();
-                return Task.CompletedTask;
-            }
+                // Add the ServerHostedService to the hosted services of the .NET Generic Host.
+                services.AddHostedService<ServerHostedService>();
 
-            public Task StopAsync(CancellationToken cancellationToken) =>
-                // Shutdown the IceRPC server when the hosted service is stopped.
-                _server.ShutdownAsync(cancellationToken);
+                // Add an IDispatcher singleton service. The DI container will inject it in services that require an
+                // IInvoker dependency.
+                services.AddSingleton<IDispatcher>(serviceProvider =>
+                    {
+                        // The dispatcher is a router configured with the logger and telemetry middlewares and the
+                        // IHello service. The middlewares use the logger factory provided by the .NET Generic Host.
+                        ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                        var router = new Router();
+                        router.UseLogger(loggerFactory);
+                        router.UseTelemetry(new TelemetryOptions { LoggerFactory = loggerFactory });
+                        router.Map<IHello>(new Hello());
+                        return router;
+                    });
+
+                // Get the ServerOptions from the configuration and add it to the generic host options. The DI
+                // container will inject it in services that require an IOptions<ServerOptions> dependency.
+                services
+                    .AddOptions<ServerOptions>()
+                    .Bind(hostContext.Configuration.GetSection("Server"))
+                    .Configure(serverOptions =>
+                        {
+                            // Configure the authentication options
+                            serverOptions.AuthenticationOptions = new SslServerAuthenticationOptions()
+                            {
+                                ServerCertificate = new X509Certificate2(
+                                    hostContext.Configuration.GetValue<string>("Certificate:File"),
+                                    hostContext.Configuration.GetValue<string>("Certificate:Password"))
+                            };
+                        })
+                    .Configure((ServerOptions serverOptions, IDispatcher dispatcher) =>
+                        {
+                            // Configure the dispatcher
+                            serverOptions.Dispatcher = dispatcher;
+                        });
+            });
+
+    /// <summary>The server hosted service is ran and managed by the .NET Generic Host</summary>
+    private class ServerHostedService : IHostedService
+    {
+        // The IceRPC server to accept connections from IceRPC clients.
+        private readonly Server _server;
+
+        public ServerHostedService(IOptions<ServerOptions> options) =>
+            _server = new Server(options.Value);
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            // Start listening for client connections.
+            _server.Listen();
+            return Task.CompletedTask;
         }
+
+        public Task StopAsync(CancellationToken cancellationToken) =>
+            // Shutdown the IceRPC server when the hosted service is stopped.
+            _server.ShutdownAsync(cancellationToken);
     }
 }
