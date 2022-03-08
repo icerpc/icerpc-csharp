f19dda9fc9684e589c59eaf54a78f7538d0ec5ab
Reece Humphreys <reece@zeroc.com>
2022-03-08 11:27:08 -0500

    Updated Options to use get/set (#883)
    * Updated options to use get inplace of init
    
    * Updated XML doc comments
    
    * Updated comments
    
    * More comment fixes
    
    * Update src/IceRpc/Configure/SlicTransportOptions.cs
    
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>
    
    * Update src/IceRpc/Configure/SlicTransportOptions.cs
    
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>
    
    * Update src/IceRpc/Configure/SlicTransportOptions.cs
    
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>
    
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>

--- src/IceRpc/Configure/CompressOptions.cs
+++ src/IceRpc/Configure/CompressOptions.cs
@@ -8,17 +8,18 @@ namespace IceRpc.Configure
     /// <see cref="CompressorMiddleware"/>.</summary>
     public sealed record class CompressOptions
     {
-        /// <summary>The compression level for the compress operation, the default value is
-        /// <see cref="CompressionLevel.Fastest"/>.</summary>
-        public CompressionLevel CompressionLevel { get; init; } = CompressionLevel.Fastest;
+        /// <summary>Gets or sets the compression level for the compress operation.</summary>
+        /// <value> The default value is <see cref="CompressionLevel.Fastest"/>. </value>
+        public CompressionLevel CompressionLevel { get; set; } = CompressionLevel.Fastest;
 
-        /// <summary>Whether or not to apply compression to the 2.0 encoded payload of a request or response when
-        /// <see cref="Features.CompressPayload.Yes"/> is present in the request features or response features
-        /// respectively. The default value is <c>true</c>.</summary>
-        public bool CompressPayload { get; init; } = true;
+        /// <summary>Gets or sets whether or not to compress the request or response payload.</summary>
+        /// <value>When <c>true</c>, compress the payload if it is not compressed yet. When <c>false</c>, do not
+        /// compress the payload. The default value is <c>true</c>.</value>
+        public bool CompressPayload { get; set; } = true;
 
-        /// <summary>Whether or not to decompress the compressed request or response payload. The default value is
-        /// <c>true</c>.</summary>
-        public bool DecompressPayload { get; init; } = true;
+        /// <summary>Gets or sets whether or not to decompress the compressed request or response payload. </summary>
+        /// <value>When <c>true</c>, decompress the payload if it is compressed. When <c>false</c>, do not
+        /// decompress the payload. The default value is <c>true</c>.</value>
+        public bool DecompressPayload { get; set; } = true;
     }
 }
--- src/IceRpc/Configure/RetryOptions.cs
+++ src/IceRpc/Configure/RetryOptions.cs
@@ -8,13 +8,14 @@ namespace IceRpc.Configure
     /// <summary>Options class to configure <see cref="RetryInterceptor"/>.</summary>
     public sealed record class RetryOptions
     {
-        /// <summary>The maximum amount of memory in bytes used to hold all retryable requests. Once this limit is
-        /// reached new requests are not retried and their memory is released after being sent. The default value is
-        /// 100 MB</summary>
+        /// <summary>Gets or sets the maximum amount of memory in bytes used to hold all retryable requests.
+        /// Once this limit is reached new requests are not retried and their memory is released after being sent.
+        /// </summary>
+        /// <value>The buffer maximum size in bytes. The default value is 100 MB.</value>
         public int BufferMaxSize
         {
             get => _bufferMaxSize;
-            init
+            set
             {
                 if (value < 1)
                 {
@@ -26,13 +27,14 @@ namespace IceRpc.Configure
         }
 
         /// <summary>A logger factory used to create the retry interceptor logger.</summary>
-        public ILoggerFactory LoggerFactory { get; init; } = NullLoggerFactory.Instance;
+        public ILoggerFactory LoggerFactory { get; set; } = NullLoggerFactory.Instance;
 
-        /// <summary>The maximum number of attempts for retrying a request.</summary>
+        /// <summary>Gets or sets the maximum number of attempts for retrying a request.</summary>
+        /// <value>The maximum number of attempts for retrying a request. The default value is 1 attempt.</value>
         public int MaxAttempts
         {
             get => _maxAttempts;
-            init
+            set
             {
                 if (value < 1)
                 {
@@ -43,12 +45,13 @@ namespace IceRpc.Configure
             }
         }
 
-        /// <summary>The maximum payload size in bytes for a request to be retryable, requests with a bigger payload
-        /// size are released after sent and cannot be retried. The default value is 1 MB.</summary>
+        /// <summary>Gets or sets the maximum payload size in bytes for a request to be retryable. Requests with a
+        /// bigger payload size are released after sent and cannot be retried.</summary>
+        /// <value>The maximum payload size in bytes. The default value is 1 MB.</value>
         public int RequestMaxSize
         {
             get => _requestMaxSize;
-            init
+            set
             {
                 if (value < 1)
                 {
