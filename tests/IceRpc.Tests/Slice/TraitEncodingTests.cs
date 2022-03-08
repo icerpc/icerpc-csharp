2a51c27effd24a8c72b361a8495dfe7ba830c8da
Jose <jose@zeroc.com>
2022-03-08 20:06:49 +0100

    cleanup

--- tests/IceRpc.Tests/Slice/TraitEncodingTests.cs
+++ tests/IceRpc.Tests/Slice/TraitEncodingTests.cs
@@ -13,7 +13,6 @@ public sealed class TraitEncodingTests
     private Memory<byte> _buffer;
     private MemoryBufferWriter _bufferWriter;
 
-    /// <summary>Common setup for the trait decoding tests.</summary>
     public TraitEncodingTests()
     {
         _buffer = new byte[1024];
@@ -21,18 +20,19 @@ public sealed class TraitEncodingTests
         _activator = ActivatorFactory.Instance.Get(typeof(TraitEncodingTests).Assembly);
     }
 
-    /// <summary>Verifies the struct's EncodeTrait generated method correctly encodes a struct as a trait.</summary>
+    /// <summary>Verifies that decoding a trait with a mismatched type fails with <see cref="InvalidDataException"/>.
+    /// </summary>
     [Test]
-    public void Encoding_a_struct_as_a_trait()
+    public void Decoding_a_mismatched_type_fails()
     {
+        // Arrange
         var encoder = new SliceEncoder(_bufferWriter, Encoding.Slice20);
-        var decoder = new SliceDecoder(_buffer, Encoding.Slice20, activator: _activator);
-        var traitStructA = new TraitStructA("Foo");
+        var traitStructB = new TraitStructB(97);
+        traitStructB.EncodeTrait(ref encoder);
 
-        traitStructA.EncodeTrait(ref encoder);
-
-        Assert.That(decoder.DecodeString(), Is.EqualTo("::IceRpc::Slice::Tests::TraitStructA"));
-        Assert.That(new TraitStructA(ref decoder), Is.EqualTo(traitStructA));
+        // Act/Assert
+        Assert.Throws<InvalidDataException>(() =>
+            new SliceDecoder(_buffer, Encoding.Slice20, activator: _activator).DecodeTrait<IMyTraitA>());
     }
 
     /// <summary>Verify that <see cref="SliceDecoder.DecodeTrait{T}"/> method correctly decodes a trait into a concrete
@@ -46,7 +46,7 @@ public sealed class TraitEncodingTests
         traitStructB1.Encode(ref encoder);
         var decoder = new SliceDecoder(_buffer, Encoding.Slice20, activator: _activator);
 
-        var traitStructB2 = decoder.DecodeTrait<TraitStructB>();
+        TraitStructB traitStructB2 = decoder.DecodeTrait<TraitStructB>();
 
         Assert.That(traitStructB2, Is.EqualTo(traitStructB1));
     }
@@ -67,34 +67,34 @@ public sealed class TraitEncodingTests
         Assert.That(decodedTrait.GetString(), Is.EqualTo("Bar"));
     }
 
-    /// <summary>Verifies that decoding a trait with a mismatched type fails with <see cref="InvalidDataException"/>.
-    /// </summary>
-    [Test]
-    public void Decoding_a_mismatched_type_fails()
-    {
-        // Arrange
-        var encoder = new SliceEncoder(_bufferWriter, Encoding.Slice20);
-        var traitStructB = new TraitStructB(97);
-        traitStructB.EncodeTrait(ref encoder);
-
-        // Act/Assert
-        Assert.Throws<InvalidDataException>(() =>
-            new SliceDecoder(_buffer, Encoding.Slice20, activator: _activator).DecodeTrait<IMyTraitA>());
-    }
-
     /// <summary>Verifies that decoding a trait with an unknown type ID fails with <see cref="InvalidDataException"/>.
     /// </summary>
     [Test]
     public void Decoding_an_unknown_type_id_fails()
     {
+        // Arrange
         var encoder = new SliceEncoder(_bufferWriter, Encoding.Slice20);
-
         var traitStructB = new TraitStructB(42);
         encoder.EncodeString("::IceRpc::Slice::Tests::FakeTrait");
         traitStructB.Encode(ref encoder);
 
+        // Act/Assert
         Assert.Throws<InvalidDataException>(() =>
             new SliceDecoder(_buffer, Encoding.Slice20, activator: _activator).DecodeTrait<IMyTraitB>());
+    }
+
+    /// <summary>Verifies the struct's EncodeTrait generated method correctly encodes a struct as a trait.</summary>
+    [Test]
+    public void Encoding_a_struct_as_a_trait()
+    {
+        var encoder = new SliceEncoder(_bufferWriter, Encoding.Slice20);
+        var decoder = new SliceDecoder(_buffer, Encoding.Slice20, activator: _activator);
+        var traitStructA = new TraitStructA("Foo");
+
+        traitStructA.EncodeTrait(ref encoder);
+
+        Assert.That(decoder.DecodeString(), Is.EqualTo("::IceRpc::Slice::Tests::TraitStructA"));
+        Assert.That(new TraitStructA(ref decoder), Is.EqualTo(traitStructA));
     }
 }
 
