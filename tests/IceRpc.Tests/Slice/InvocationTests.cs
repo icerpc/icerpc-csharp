7ce3eb4b76f2f88f8a52006d70793ad46cfce340
Jose <jose@zeroc.com>
2022-03-09 12:50:01 +0100

    Add invocation timeout test

--- tests/IceRpc.Tests/Slice/InvocationTests.cs
+++ tests/IceRpc.Tests/Slice/InvocationTests.cs
@@ -44,4 +44,42 @@ public class InvocationTests
         // Assert
         Assert.That(context, Is.EqualTo(invocation.Features.GetContext()));
     }
+
+    // <summary>Verifies that the cancellation token passed to the invoker is canceled after the invocation timeout
+    // expires.</summary>
+    [Test]
+    public void Invocation_timeout()
+    {
+        // Arrange
+        var invocation = new Invocation
+        {
+            Timeout = TimeSpan.FromMilliseconds(500),
+        };
+
+        CancellationToken? cancelationToken = null;
+        var proxy = new Proxy(Protocol.IceRpc)
+        {
+            Path = "/",
+            Invoker = new InlineInvoker(async (request, cancel) =>
+            {
+                cancelationToken = cancel;
+                await Task.Delay(10000, cancel);
+                return new IncomingResponse(request, ResultType.Success, EmptyPipeReader.Instance);
+            }),
+        };
+
+        // Act/Assert
+        Assert.ThrowsAsync<TaskCanceledException>(
+            () => proxy.InvokeAsync(
+                "",
+                Encoding.Slice20,
+                EmptyPipeReader.Instance,
+                null,
+                SliceDecoder.GetActivator(typeof(InvocationTests).Assembly),
+                invocation));
+
+        Assert.That(cancelationToken, Is.Not.Null);
+        Assert.That(cancelationToken.Value.CanBeCanceled, Is.True);
+        Assert.That(cancelationToken.Value.IsCancellationRequested, Is.True);
+    }
 }
