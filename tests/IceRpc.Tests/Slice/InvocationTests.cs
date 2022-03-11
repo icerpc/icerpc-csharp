4213f5733ff06fc7910e50229995811f1f955c71
Jose <jose@zeroc.com>
2022-03-11 22:35:58 +0100

    More review fixes

--- tests/IceRpc.Tests/Slice/InvocationTests.cs
+++ tests/IceRpc.Tests/Slice/InvocationTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
 using IceRpc.Internal;
+using IceRpc.Slice.Internal;
 using NUnit.Framework;
 
 namespace IceRpc.Slice.Tests;
@@ -9,9 +10,7 @@ namespace IceRpc.Slice.Tests;
 [Timeout(30000)]
 public class InvocationTests
 {
-    /// <summary>Verifies that the request context feature is set to the invocation context.
-    /// </summary>
-    /// <returns></returns>
+    /// <summary>Verifies that the request context feature is set to the invocation context.</summary>
     [Test]
     public async Task Invocation_context()
     {
@@ -42,5 +41,176 @@ public class InvocationTests
 
         // Assert
         Assert.That(context, Is.EqualTo(invocation.Features.GetContext()));
+    }
+
+    /// <summary>Verifies that the encoded deadline has the expected value when <see cref="Invocation.Deadline"/> is
+    /// set.</summary>
+    [Test]
+    public async Task Invocation_deadline_sets_the_deadline_field()
+    {
+        // Arrange
+        var timeout = TimeSpan.FromMilliseconds(500);
+        DateTime deadline = DateTime.MaxValue;
+        var invoker = new InlineInvoker((request, cancel) =>
+        {
+            deadline = DecodeDeadlineField(request.Fields);
+            return Task.FromResult(new IncomingResponse(request));
+        });
+
+        using var cancellationSource = new CancellationTokenSource();
+        var sut = new Proxy(Protocol.IceRpc) { Invoker = invoker };
+        DateTime expectedDeadline = DateTime.UtcNow + timeout;
+        var invocation = new Invocation() { Deadline = expectedDeadline };
+
+        // Act
+        await sut.InvokeAsync(
+            "",
+            Encoding.Slice20,
+            EmptyPipeReader.Instance,
+            null,
+            SliceDecoder.GetActivator(typeof(InvocationTests).Assembly),
+            invocation,
+            cancel: cancellationSource.Token);
+
+        // Assert
+        Assert.That(Math.Abs((deadline - expectedDeadline).TotalMilliseconds), Is.LessThanOrEqualTo(1));
+    }
+
+    /// <summary>Verifies that the invocation is canceled after the <see cref="Invocation.Timeout"/> expires.</summary>
+    [Test]
+    public void Invocation_is_canceled_after_the_timeout_expires()
+    {
+        // Arrange
+        CancellationToken? cancellationToken = null;
+        bool hasDeadline = false;
+        var sut = new Proxy(Protocol.IceRpc)
+        {
+            Invoker = new InlineInvoker(async (request, cancel) =>
+            {
+                hasDeadline = request.Fields.ContainsKey(RequestFieldKey.Deadline);
+                cancellationToken = cancel;
+                await Task.Delay(10000, cancel);
+                return new IncomingResponse(request);
+            }),
+        };
+
+        // Act
+        Assert.ThrowsAsync<TaskCanceledException>(
+            () => sut.InvokeAsync(
+                "",
+                Encoding.Slice20,
+                EmptyPipeReader.Instance,
+                null,
+                SliceDecoder.GetActivator(typeof(InvocationTests).Assembly),
+                new Invocation { Timeout = TimeSpan.FromMilliseconds(500) }));
+
+        // Assert
+        Assert.That(cancellationToken, Is.Not.Null);
+        Assert.That(cancellationToken.Value.CanBeCanceled, Is.True);
+        Assert.That(cancellationToken.Value.IsCancellationRequested, Is.True);
+        Assert.That(hasDeadline, Is.True);
+    }
+
+    /// <summary>Verifies that the invocation timeout value set in the <see cref="Invocation.Timeout"/> prevails over
+    /// the invocation timeout value previously set with the <see cref="TimeoutInterceptor"/>.</summary>
+    [Test]
+    public async Task Invocation_timeout_value_prevails_over_invoker_timeout_interceptor()
+    {
+        // Arrange
+        var invocationTimeout = TimeSpan.FromSeconds(30);
+        DateTime deadline = DateTime.MaxValue;
+        DateTime expectedDeadline = DateTime.UtcNow + invocationTimeout;
+        var invoker = new InlineInvoker((request, cancel) =>
+        {
+            deadline = DecodeDeadlineField(request.Fields);
+            return Task.FromResult(new IncomingResponse(request));
+        });
+        var timeoutInterceptor = new TimeoutInterceptor(invoker, TimeSpan.FromSeconds(120));
+        var sut = new Proxy(Protocol.IceRpc) { Invoker = timeoutInterceptor };
+
+        // Act
+        await sut.InvokeAsync(
+            "",
+            Encoding.Slice20,
+            EmptyPipeReader.Instance,
+            null,
+            SliceDecoder.GetActivator(typeof(InvocationTests).Assembly),
+            new Invocation() { Timeout = invocationTimeout });
+
+        // Assert
+        Assert.That(Math.Abs((deadline - expectedDeadline).TotalMilliseconds), Is.LessThanOrEqualTo(50));
+    }
+
+
+    /// <summary>Verifies that when using an infinite invocation timeout the cancellation token passed to the invoker
+    /// is not cancelable.</summary>
+    [Test]
+    public async Task Invocation_with_an_infinite_timeout_uses_the_default_cancellation_token()
+    {
+        // Arrange
+        CancellationToken? cancellationToken = null;
+        bool hasDeadline = false;
+        var sut = new Proxy(Protocol.IceRpc)
+        {
+            Invoker = new InlineInvoker((request, cancel) =>
+            {
+                cancellationToken = cancel;
+                hasDeadline = request.Fields.ContainsKey(RequestFieldKey.Deadline);
+                return Task.FromResult(new IncomingResponse(request));
+            }),
+        };
+
+        // Act
+        await sut.InvokeAsync(
+            "",
+            Encoding.Slice20,
+            EmptyPipeReader.Instance,
+            null,
+            SliceDecoder.GetActivator(typeof(InvocationTests).Assembly),
+            new Invocation { Timeout = Timeout.InfiniteTimeSpan });
+
+        // Assert
+        Assert.That(cancellationToken, Is.Not.Null);
+        Assert.That(cancellationToken.Value.CanBeCanceled, Is.False);
+        Assert.That(hasDeadline, Is.False);
+    }
+
+    /// <summary>Verifies that setting an invocation deadline requires providing a cancelable cancellation token.
+    /// </summary>
+    [Test]
+    public void Setting_the_invocation_deadline_requires_a_cancelable_cancellation_token()
+    {
+        // Arrange
+        var sut = new Proxy(Protocol.IceRpc);
+
+        // Act/Assert
+        Assert.ThrowsAsync<ArgumentException>(
+            () => sut.InvokeAsync(
+                "",
+                Encoding.Slice20,
+                EmptyPipeReader.Instance,
+                null,
+                SliceDecoder.GetActivator(typeof(InvocationTests).Assembly),
+                new Invocation { Deadline = DateTime.Now + TimeSpan.FromSeconds(60) },
+                cancel: CancellationToken.None));
+    }
+
+    private static DateTime DecodeDeadlineField(IDictionary<RequestFieldKey, OutgoingFieldValue> fields)
+    {
+        if (fields.TryGetValue(RequestFieldKey.Deadline, out var deadlineField))
+        {
+            var buffer = new byte[256];
+            var bufferWriter = new MemoryBufferWriter(buffer);
+            var encoder = new SliceEncoder(bufferWriter, Encoding.Slice20);
+            deadlineField.Encode(ref encoder);
+            var decoder = new SliceDecoder(buffer, Encoding.Slice20);
+            decoder.SkipSize();
+            long value = decoder.DecodeVarLong();
+            return DateTime.UnixEpoch + TimeSpan.FromMilliseconds(value);
+        }
+        else
+        {
+            return DateTime.MaxValue;
+        }
     }
 }
---
+++ tests/IceRpc.Tests/TimeoutInterceptorTests.cs
@@ -0,0 +1,81 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using IceRpc.Slice;
+using IceRpc.Slice.Internal;
+using NUnit.Framework;
+
+namespace IceRpc.Tests;
+
+public sealed class TimeoutInterceptorTests
+{
+    /// <summary>Verifies that the invocation is canceled when the invocation time expires.</summary>
+    [Test]
+    public void Invocation_is_canceled_after_the_invocation_timeout_expires()
+    {
+        // Arrange
+        CancellationToken? cancellationToken = null;
+        bool hasDeadline = false;
+
+        var invoker = new InlineInvoker(async (request, cancel) =>
+        {
+            hasDeadline = request.Fields.ContainsKey(RequestFieldKey.Deadline);
+            cancellationToken = cancel;
+            await Task.Delay(TimeSpan.FromSeconds(1), cancel);
+            return new IncomingResponse(request);
+        });
+
+        var sut = new TimeoutInterceptor(invoker, TimeSpan.FromMilliseconds(500));
+        var request = new OutgoingRequest(new Proxy(Protocol.IceRpc));
+
+        // Act
+        Assert.ThrowsAsync<TaskCanceledException>(() => sut.InvokeAsync(request, default));
+
+        // Assert
+        Assert.That(hasDeadline, Is.True);
+        Assert.That(cancellationToken, Is.Not.Null);
+        Assert.That(cancellationToken.Value.CanBeCanceled, Is.True);
+        Assert.That(cancellationToken.Value.IsCancellationRequested, Is.True);
+    }
+
+    /// <summary>Verifies that the timeout interceptor encodes the expected deadline value.</summary>
+    [Test]
+    public async Task Invocation_interceptor_sets_the_deadline_field()
+    {
+        // Arrange
+        var timeout = TimeSpan.FromMilliseconds(500);
+        DateTime deadline = DateTime.MaxValue;
+        var invoker = new InlineInvoker((request, cancel) =>
+        {
+            if (request.Fields.TryGetValue(RequestFieldKey.Deadline, out var deadlineField))
+            {
+                byte[] buffer = new byte[256];
+                var bufferWriter = new MemoryBufferWriter(buffer);
+                var encoder = new SliceEncoder(bufferWriter, Encoding.Slice20);
+                deadlineField.Encode(ref encoder);
+                var decoder = new SliceDecoder(buffer, Encoding.Slice20);
+                decoder.SkipSize();
+                deadline = DateTime.UnixEpoch + TimeSpan.FromMilliseconds(decoder.DecodeVarLong());
+            }
+            return Task.FromResult(new IncomingResponse(request));
+        });
+
+        var sut = new TimeoutInterceptor(invoker, timeout);
+        var request = new OutgoingRequest(new Proxy(Protocol.IceRpc));
+        DateTime expectedDeadline = DateTime.UtcNow + timeout;
+
+        // Act
+        await sut.InvokeAsync(request, default);
+
+        // Assert
+        Assert.That(Math.Abs((deadline - expectedDeadline).TotalMilliseconds), Is.LessThan(10));
+    }
+
+    /// <summary>Verifies that <see cref="TimeoutInterceptor"/> doesn't allow using an invalid timeout value.</summary>
+    /// <param name="timeout">The invalid timeout value for the test.</param>
+    [TestCase(-2)] // Cannot use a negative timeout
+    [TestCase(-1)] // Cannot use infinite timeout
+    public void Timeout_interceptor_does_not_allow_invalid_timeout_values(int timeout) =>
+        // Act & Assert
+        Assert.Throws<ArgumentException>(
+            () => _ = new TimeoutInterceptor(Proxy.DefaultInvoker, TimeSpan.FromSeconds(timeout)));
+}
