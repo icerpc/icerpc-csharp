b15e3b40d48a6df93413978071ee4f69e8ff79a1
Jose <jose@zeroc.com>
2022-03-14 16:30:21 +0100

    Reworked connection pool test to use coloc transport

--- tests/IntegrationTests/ConnectionPoolTests.cs
+++ tests/IntegrationTests/ConnectionPoolTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
 using IceRpc.Configure;
+using IceRpc.Transports;
 using NUnit.Framework;
 
 namespace IceRpc.Tests;
@@ -13,28 +14,45 @@ public sealed class ConnectionPoolTests
     public async Task Do_not_prefer_existing_connection()
     {
         // Arrange
-        await using var server1 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
+        var colocTransport = new ColocTransport();
+        await using var server1 = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://foo",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
         server1.Listen();
 
-        await using var server2 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
+        await using var server2 = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://bar",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
         server2.Listen();
 
-        await using var pool = new ConnectionPool(new ConnectionOptions(), preferExistingConnection: false);
+        await using var pool = new ConnectionPool(
+            new ConnectionOptions()
+            {
+                MultiplexedClientTransport = new SlicClientTransport(colocTransport.ClientTransport)
+            },
+            preferExistingConnection: false);
 
-        Connection connection = await pool.GetConnectionAsync(
+        Connection connection2 = await pool.GetConnectionAsync(
             server2.Endpoint,
             Array.Empty<Endpoint>(),
             default);
 
         // Act
-        connection = await pool.GetConnectionAsync(
+        Connection connection1 = await pool.GetConnectionAsync(
             server1.Endpoint,
             new Endpoint[] { server2.Endpoint },
             default);
 
         // Assert
-        Assert.That(connection.State, Is.EqualTo(ConnectionState.Active));
-        Assert.That(connection.RemoteEndpoint, Is.EqualTo(server1.Endpoint));
+        Assert.That(connection1.State, Is.EqualTo(ConnectionState.Active));
+        Assert.That(connection1.RemoteEndpoint, Is.EqualTo(server1.Endpoint));
+        Assert.That(connection1, Is.Not.EqualTo(connection2));
         Assert.That(server1.Endpoint, Is.Not.EqualTo(server2.Endpoint));
     }
 
@@ -44,14 +62,25 @@ public sealed class ConnectionPoolTests
     public async Task Get_connection_for_alt_endpoint()
     {
         // Arrange
-        await using var server = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
+        var colocTransport = new ColocTransport();
+        await using var server = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://foo",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
         server.Listen();
 
-        await using var pool = new ConnectionPool(new ConnectionOptions(), preferExistingConnection: true);
+        await using var pool = new ConnectionPool(
+            new ConnectionOptions()
+            {
+                MultiplexedClientTransport = new SlicClientTransport(colocTransport.ClientTransport)
+            },
+            preferExistingConnection: true);
 
         // Act
         Connection connection = await pool.GetConnectionAsync(
-            Endpoint.FromString("icerpc://127.0.0.1:10000"),
+            Endpoint.FromString("icerpc://bar?transport=coloc"),
             new Endpoint[] { server.Endpoint },
             default);
 
@@ -65,13 +94,28 @@ public sealed class ConnectionPoolTests
     public async Task Get_connection_for_main_endpoint()
     {
         // Arrange
-        await using var server1 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
+        var colocTransport = new ColocTransport();
+        await using var server1 = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://foo",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
         server1.Listen();
 
-        await using var server2 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
+        await using var server2 = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://bar",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
         server2.Listen();
 
-        await using var pool = new ConnectionPool(new ConnectionOptions(), preferExistingConnection: true);
+        await using var pool = new ConnectionPool(
+            new ConnectionOptions()
+            {
+                MultiplexedClientTransport = new SlicClientTransport(colocTransport.ClientTransport)
+            });
 
         // Act
         Connection connection = await pool.GetConnectionAsync(
@@ -89,23 +133,31 @@ public sealed class ConnectionPoolTests
     public async Task Get_connection_reuse_existing_connection()
     {
         // Arrange
-        await using var server1 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
-        server1.Listen();
+        var colocTransport = new ColocTransport();
+        await using var server = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://foo",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
+        server.Listen();
 
-        await using var server2 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
-        server2.Listen();
-
-        await using var pool = new ConnectionPool(new ConnectionOptions(), preferExistingConnection: false);
+        await using var pool = new ConnectionPool(
+            new ConnectionOptions()
+            {
+                MultiplexedClientTransport = new SlicClientTransport(colocTransport.ClientTransport)
+            },
+            preferExistingConnection: true);
 
         Connection connection1 = await pool.GetConnectionAsync(
-            server1.Endpoint,
-            new Endpoint[] { server2.Endpoint },
+            server.Endpoint,
+            Array.Empty<Endpoint>(),
             default);
 
         // Act
         Connection connection2 = await pool.GetConnectionAsync(
-            server1.Endpoint,
-            new Endpoint[] { server2.Endpoint },
+            server.Endpoint,
+            Array.Empty<Endpoint>(),
             default);
 
         // Assert
@@ -114,34 +166,51 @@ public sealed class ConnectionPoolTests
     }
 
     /// <summary>Verifies that the connection pool prefers reusing an existing connection when
-    /// <c>preferExistingConnection</c> is used.</summary>
+    /// <c>preferExistingConnection</c> is true.</summary>
     [Test]
     public async Task Prefer_existing_connection()
     {
         // Arrange
-        await using var server1 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
+        var colocTransport = new ColocTransport();
+        await using var server1 = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://foo",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
         server1.Listen();
 
-        await using var server2 = new Server(ConnectionOptions.DefaultDispatcher, "icerpc://127.0.0.1:0");
+        await using var server2 = new Server(
+            new ServerOptions()
+            {
+                Endpoint = "icerpc://bar",
+                MultiplexedServerTransport = new SlicServerTransport(colocTransport.ServerTransport)
+            });
         server2.Listen();
 
-        await using var pool = new ConnectionPool(new ConnectionOptions(), preferExistingConnection: true);
+        await using var pool = new ConnectionPool(
+           new ConnectionOptions()
+           {
+               MultiplexedClientTransport = new SlicClientTransport(colocTransport.ClientTransport)
+           },
+           preferExistingConnection: true);
 
-        Connection connection = await pool.GetConnectionAsync(
+        Connection connection1 = await pool.GetConnectionAsync(
             server2.Endpoint,
             Array.Empty<Endpoint>(),
             default);
 
         // Act
-        connection = await pool.GetConnectionAsync(
+        Connection connection2 = await pool.GetConnectionAsync(
             server1.Endpoint,
             new Endpoint[] { server2.Endpoint },
             default);
 
 
         // Assert
-        Assert.That(connection.State, Is.EqualTo(ConnectionState.Active));
-        Assert.That(connection.RemoteEndpoint, Is.EqualTo(server2.Endpoint));
+        Assert.That(connection2.State, Is.EqualTo(ConnectionState.Active));
+        Assert.That(connection2.RemoteEndpoint, Is.EqualTo(server2.Endpoint));
+        Assert.That(connection2, Is.EqualTo(connection1));
         Assert.That(server1.Endpoint, Is.Not.EqualTo(server2.Endpoint));
     }
 }
