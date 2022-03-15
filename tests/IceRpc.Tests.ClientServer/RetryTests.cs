fa7e25f5af95fdc0108dfd9b8827dbc35b6bd19f
Benoit Foucher <benoit@zeroc.com>
2022-03-15 15:49:44 +0100

    Retry_GracefulCloseCanceled test failure fix

--- tests/IceRpc.Tests.ClientServer/RetryTests.cs
+++ tests/IceRpc.Tests.ClientServer/RetryTests.cs
@@ -171,24 +171,41 @@ namespace IceRpc.Tests.ClientServer
                 results.Add(retry.OpWithDataAsync(-1, 0, seq));
             }
 
-            if (protocol == "icerpc")
+            var whenAllTask = Task.WhenAll(results);
+            try
             {
-                // With icerpc cancelation, the peer shutdown might cancel invocations which are being dispatched.
-                try
-                {
-                    await Task.WhenAll(results);
-                }
-                catch (OperationCanceledException)
-                {
-                }
-                catch (AggregateException ex)
-                {
-                    Assert.That(ex.InnerExceptions.All(exception => exception is OperationCanceledException), Is.True);
-                }
+                await whenAllTask;
             }
-            else
+            catch (Exception ex)
             {
-                await Task.WhenAll(results);
+                // await whenAllTask unwraps the AggregateException and throws the first exception. So we obtain the
+                // exception from the completed WhenAll task instead.
+                IEnumerable<Exception> exceptions;
+                if (whenAllTask.Exception is AggregateException aggregateException)
+                {
+                    exceptions = aggregateException.InnerExceptions;
+                }
+                else
+                {
+                    exceptions = new [] { ex };
+                }
+
+                // The peer shutdown might cancel dispatches. For now, the cancellation is reported as a
+                // DispatchException with ice and as an OperationCanceledException for icerpc.
+                // TODO: fix the different exceptions once we have a plan and consider if the cancellation should be
+                // retryable or only retryable in some specific circumstances or never.
+                if (protocol == "ice")
+                {
+                    Assert.That(exceptions.All(exception => exception is DispatchException), Is.True);
+                }
+                else
+                {
+                    // With icerpc cancelation, the peer shutdown might cancel invocations which are being dispatched.
+                    Assert.That(exceptions.All(exception => exception is OperationCanceledException), Is.True);
+                }
+
+                // There should at least be few invocations to succeed (the ones sent after the connection shutdown).
+                Assert.That(exceptions.Count, Is.LessThan(results.Count));
             }
         }
 
