51ccfa2821764e3e3c2a0c1a709a3370e5daaac5
Reece Humphreys <reece@zeroc.com>
2022-03-16 16:18:43 -0400

    Implemented suggestions

--- tests/IceRpc.Tests/Slice/SpanEnumeratorTests.cs
+++ tests/IceRpc.Tests/Slice/SpanEnumeratorTests.cs
@@ -10,29 +10,27 @@ namespace IceRpc.Slice.Tests;
 public class SpanEnumeratorTests
 {
     /// <summary>Provides test case data for
-    /// <see cref="Span_enumerator_successfully_enumerates_to_additional_spans_and_memory(...)"/> test.
+    /// <see cref="Move_to_next_span(...)"/> test.
     /// </summary>
     private static IEnumerable<TestCaseData> EnumeratorCurrentUpdatesSuccessfullySource
     {
         get
         {
-            (byte[], byte[], IList<Memory<byte>>?, int, int, byte)[] testData =
+            (byte[], byte[], IList<Memory<byte>>?, int, byte[])[] testData =
             {
                 (
                     new byte[] { 1, 2, 3 },
                     Array.Empty<byte>(),
                     null,
                     1,
-                    3,
-                    1
+                    new byte[] { 1, 2, 3 }
                 ),
                 (
                     new byte[] { 1, 2, 3 },
                     new byte[] { 4, 5, 6 },
                     null,
                     2,
-                    3,
-                    4
+                    new byte[] { 4, 5, 6 }
                 ),
                 (
                     new byte[] { 1, 2, 3 },
@@ -42,8 +40,7 @@ public class SpanEnumeratorTests
                         new byte[] { 7, 8, 9 }
                     },
                     3,
-                    3,
-                    7
+                    new byte[] { 7, 8, 9 }
                 ),
                 (
                     new byte[] { 1, 2, 3 },
@@ -54,8 +51,7 @@ public class SpanEnumeratorTests
                         new byte[] { 10, 11, 12 }
                     },
                     4,
-                    3,
-                    10
+                    new byte[] { 10, 11, 12 }
                 ),
             };
             foreach ((
@@ -63,16 +59,15 @@ public class SpanEnumeratorTests
                 byte[] secondBytes,
                 IList<Memory<byte>>? additionalMemory,
                 int moves,
-                int length,
-                byte expected) in testData)
+                byte[] expected) in testData)
             {
-                yield return new TestCaseData(firstBytes, secondBytes, additionalMemory, moves, length, expected);
+                yield return new TestCaseData(firstBytes, secondBytes, additionalMemory, moves, expected);
             }
         }
     }
 
     /// <summary>Provides test case data for
-    /// <see cref="Span_enumerator_fails_to_move_to_additional_spans_and_memory_if_none_provided(...)"/> test.
+    /// <see cref="Move_pass_the_end_fails(...)"/> test.
     /// </summary>
     private static IEnumerable<TestCaseData> EnumeratorNextFailsSource
     {
@@ -130,28 +125,22 @@ public class SpanEnumeratorTests
     /// <param name="secondBytes">The bytes that will be used to create the second span. (Can be empty)</param>
     /// <param name="additionalMemory">The list of memory used for additional memory. (Optional)</param>
     /// <param name="moves">The number of times to call `MoveNext`.</param>
-    /// <param name="expectedLength">The expected number of bytes in `Current`</param>
-    /// <param name="expectedValue">The expected value of the first byte in `Current`.</param>
-    [Test, TestCaseSource(nameof(enumeratorCurrentUpdatesSuccessfullySource))]
-    public void Span_enumerator_successfully_enumerates_to_additional_spans_and_memory(
+    /// <param name="expected">The expected byte array in `Current`.</param>
+    [Test, TestCaseSource(nameof(EnumeratorCurrentUpdatesSuccessfullySource))]
+    public void Move_to_next_span(
         byte[] firstBytes,
         byte[] secondBytes,
         IList<Memory<byte>>? additionalMemory,
         int moves,
-        int expectedLength,
-        byte expectedValue)
+        byte[] expected)
     {
-        // Arrange
         var enumerator = new SpanEnumerator(firstBytes.AsSpan(), secondBytes.AsSpan(), additionalMemory);
         for (int i = 0; i < moves - 1; i++) enumerator.MoveNext();
 
-        // Act
         bool opperationSuccessful = enumerator.MoveNext();
 
-        // Assert
         Assert.That(opperationSuccessful, Is.True);
-        Assert.That(enumerator.Current.Length, Is.EqualTo(expectedLength));
-        Assert.That(enumerator.Current[0], Is.EqualTo(expectedValue));
+        Assert.That(enumerator.Current.ToArray(), Is.EqualTo(expected.ToArray()));
     }
 
     /// <summary>Tests that the SpanEnumerator will not enumerate past the last provided span or additional memory if
@@ -161,21 +150,18 @@ public class SpanEnumeratorTests
     /// <param name="secondBytes">The bytes that will be used to create the second span. (Can be empty)</param>
     /// <param name="additionalMemory">The list of memory used for additional memory. (Optional)</param>
     /// <param name="moves">The number of times to call `MoveNext`.</param>
-    [Test, TestCaseSource(nameof(enumeratorNextFailsSource))]
-    public void Span_enumerator_fails_to_move_to_additional_spans_and_memory_if_none_provided(
+    [Test, TestCaseSource(nameof(EnumeratorNextFailsSource))]
+    public void Move_pass_the_end_fails(
         byte[] firstBytes,
         byte[] secondBytes,
         IList<Memory<byte>>? additionalMemory,
         int moves)
     {
-        // Arrange
         var enumerator = new SpanEnumerator(firstBytes.AsSpan(), secondBytes.AsSpan(), additionalMemory);
         for (int i = 0; i < moves; i++) enumerator.MoveNext();
 
-        // Act
         bool opperationSuccessful = enumerator.MoveNext();
 
-        // Assert
         Assert.That(opperationSuccessful, Is.False);
     }
 }
