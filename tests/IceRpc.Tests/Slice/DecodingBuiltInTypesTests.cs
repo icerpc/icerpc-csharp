fc65369f74b80a06f02e64f4db0177c2bef7d094
Reece Humphreys <reece@zeroc.com>
2022-03-23 11:50:11 -0400

    Added decoding tests

--- tests/IceRpc.Tests/Slice/DecodingBuiltInTypesTests.cs
+++ tests/IceRpc.Tests/Slice/DecodingBuiltInTypesTests.cs
@@ -14,87 +14,110 @@ namespace IceRpc.Slice.Tests;
 [Parallelizable(scope: ParallelScope.All)]
 public class DecodingBuiltInTypesTests
 {
-    private readonly Memory<byte> _buffer;
     private readonly SliceEncoding _encoding;
-    private readonly MemoryBufferWriter _bufferWriter;
+
+    private static IEnumerable<TestCaseData> StringDecodingDataSource
+    {
+        get
+        {
+            (byte[], byte[], String)[] testData =
+            {
+                (
+                    new byte[] {0x00},
+                    new byte[] {0x00},
+                    ""
+                ),
+                (
+                    new byte[] {0xFF, 0x57, 0x00 ,0x00 , 0x00},
+                    new byte[] {0x5D, 0x01},
+                    "Lorem ipsum dolor sit amet, no explicari repudiare vis, an dicant legimus ponderum sit."
+                ),
+                (
+                    new byte[] { 0x9A },
+                    new byte[] { 0x69, 0x02 },
+                    "국민경제의 발전을 위한 중요정책의 수립에 관하여 대통령의 자문에 응하기 위하여 국민경제자문회의를 둘 수 있다" // Korean
+                ),
+                (
+                    new byte[] { 0x9C },
+                    new byte[] { 0x71, 0x02 },
+                    "旅ロ京青利セムレ弱改フヨス波府かばぼ意送でぼ調掲察たス日西重ケアナ住橋ユムミク順待ふかんぼ人奨貯鏡すびそ" // Japanese
+                ),
+                (
+                    new byte[] { 0x40 },
+                    new byte[] { 0x01, 0x01 },
+                    "😁😂😃😄😅😆😉😊😋😌😍😏😒😓😔😖"
+                )
+            };
+            foreach ((byte[] sizeBytes11, byte[] sizeBytes20, String expected) in testData)
+            {
+                yield return new TestCaseData(sizeBytes11, sizeBytes20, expected);
+            }
+        }
+    }
+
+    // Initializes a local field with the encoding from the fixture. Done for tests that need to make some distinction
+    // between the 1.1 and 2.0 encodings.
     public DecodingBuiltInTypesTests(string encoding)
     {
         _encoding = SliceEncoding.FromString(encoding);
-        _buffer = new byte[256];
-        _bufferWriter = new MemoryBufferWriter(_buffer);
     }
 
     /// <summary>Test the encoding of a fixed size numeric type.</summary>
-    /// <param name="p1">The encoded value.</param>
-    [TestCase(long.MinValue)]
-    [TestCase(long.MaxValue)]
-    public void Decoding_long(long p1)
+    /// <param name="p1">The value to be encoded.</param>
+    /// <param name="expected">The expected hexadecimal representation of the encoded value.</param>
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80 }, long.MinValue)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F }, long.MaxValue)]
+    public void Decoding_long(byte[] encodedBytes, long expected)
     {
-        var encoder = new SliceEncoder(_bufferWriter, _encoding);
-        var decoder = new SliceDecoder(_buffer, _encoding);
-        encoder.EncodeLong(p1);
+        var decoder = new SliceDecoder(encodedBytes, _encoding);
 
         long r1 = decoder.DecodeLong();
 
-        Assert.That(p1, Is.EqualTo(r1));
-        Assert.That(_bufferWriter.WrittenMemory.Length, Is.EqualTo(sizeof(long)));
+        Assert.That(r1, Is.EqualTo(expected));
         Assert.That(decoder.Consumed, Is.EqualTo(sizeof(long)));
     }
 
     /// <summary>Test the encoding of a variable size numeric type.</summary>
-    /// <param name="p1">The encoded value.</param>
-    [TestCase(SliceEncoder.VarLongMinValue)]
-    [TestCase(SliceEncoder.VarLongMaxValue)]
-    public void Decoding_varLong(long p1)
+    /// <param name="p1">The value to be encoded.</param>
+    /// <param name="expected">The expected hexadecimal representation of the encoded value.</param>
+    [TestCase(new byte[] { 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80 }, SliceEncoder.VarLongMinValue)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F }, SliceEncoder.VarLongMaxValue)]
+    public void Decoding_varLong(byte[] encodedBytes, long expected)
     {
-        var encoder = new SliceEncoder(_bufferWriter, _encoding);
-        var decoder = new SliceDecoder(_buffer, _encoding);
-        encoder.EncodeVarLong(p1);
+        var decoder = new SliceDecoder(encodedBytes, _encoding);
+
         long r1 = decoder.DecodeVarLong();
 
-        Assert.That(p1, Is.EqualTo(r1));
+        Assert.That(r1, Is.EqualTo(expected));
+        Assert.That(decoder.Consumed, Is.EqualTo(sizeof(long)));
     }
 
-    [TestCase("")]
-    [TestCase("Lorem ipsum dolor sit amet, no explicari repudiare vis, an dicant legimus ponderum sit.")]
-    [TestCase("국민경제의 발전을 위한 중요정책의 수립에 관하여 대통령의 자문에 응하기 위하여 국민경제자문회의를 둘 수 있다")] // Korean
-    [TestCase("旅ロ京青利セムレ弱改フヨス波府かばぼ意送でぼ調掲察たス日西重ケアナ住橋ユムミク順待ふかんぼ人奨貯鏡すびそ")] // Japanese
-    [TestCase("😁😂😃😄😅😆😉😊😋😌😍😏😒😓😔😖")] // each character encoded with surrogates
-    public void Decoding_string(string p1)
+    /// <summary>Test the encoding of a variable size numeric type.</summary>
+    /// <param name="p1">The value to be encoded.</param>
+    /// <param name="expected">The expected hexadecimal representation of the encoded value.</param>
+    [TestCase(new byte[] { 0x00 }, SliceEncoder.VarULongMinValue)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }, SliceEncoder.VarULongMaxValue)]
+    public void Decoding_var_u_long(byte[] encodedBytes, ulong expected)
     {
-        var encoder = new SliceEncoder(_bufferWriter, _encoding);
-        var decoder = new SliceDecoder(_buffer, _encoding);
+        var decoder = new SliceDecoder(encodedBytes, _encoding);
 
-        // simple test
-        encoder.EncodeString(p1);
+        ulong r1 = decoder.DecodeVarULong();
+
+        Assert.That(r1, Is.EqualTo(expected));
+        Assert.That(decoder.Consumed, Is.EqualTo(encodedBytes.Length));
+    }
+
+    [Test, TestCaseSource(nameof(StringDecodingDataSource))]
+    public void Decoding_string(byte[] sizeBytes11, byte[] sizeBytes20, String expected)
+    {
+        var sizeBytes = _encoding.ToString() == "1.1" ? sizeBytes11 : sizeBytes20;
+        var utf8Bytes = System.Text.Encoding.UTF8.GetBytes(expected);
+        var encodedBytes = sizeBytes.Concat(utf8Bytes).ToArray();
+        var decoder = new SliceDecoder(encodedBytes, _encoding);
+
         string r1 = decoder.DecodeString();
-        Assert.That(p1, Is.EqualTo(r1));
 
-        if (p1.Length > 0)
-        {
-            // now with a custom memory pool with a tiny max buffer size
-            using var customPool = new TestMemoryPool(7);
+        Assert.That(r1, Is.EqualTo(expected));
 
-            // minimumSegmentSize is not the same as the sizeHint given to GetMemory/GetSpan; it refers to the
-            // minBufferSize given to Rent
-            var pipe = new Pipe(new PipeOptions(pool: customPool, minimumSegmentSize: 5));
-
-            encoder = new SliceEncoder(pipe.Writer, _encoding);
-            for (int i = 0; i < 20; ++i)
-            {
-                encoder.EncodeString(p1);
-            }
-            pipe.Writer.Complete();
-
-            pipe.Reader.TryRead(out ReadResult readResult);
-            decoder = new SliceDecoder(readResult.Buffer, _encoding);
-
-            for (int i = 0; i < 20; ++i)
-            {
-                r1 = decoder.DecodeString();
-                Assert.That(p1, Is.EqualTo(r1));
-            }
-            pipe.Reader.AdvanceTo(readResult.Buffer.End);
-        }
     }
 }
