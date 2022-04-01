691815d96f69ed9fbdfd2bacb6fa4f25f43df117
Reece Humphreys <reece@zeroc.com>
2022-04-01 10:36:44 -0400

    Update tests/IceRpc.Tests/Slice/DecodeSequenceTests.cs
    Co-authored-by: Jose  <pepone@users.noreply.github.com>

--- tests/IceRpc.Tests/Slice/DecodeSequenceTests.cs
+++ tests/IceRpc.Tests/Slice/DecodeSequenceTests.cs
@@ -17,7 +17,7 @@ public class DecodeSequenceTests
     {
         get
         {
-            foreach (SliceEncoding encoding in new SliceEncoding[] { SliceEncoding.Slice11, SliceEncoding.Slice20 })
+            foreach (SliceEncoding encoding in Enum.GetValues(typeof(SliceEncoding)))
             {
                 foreach (int size in new int[] { 0, 256 })
                 {
