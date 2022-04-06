719fae701096fee945ab9dfd7c612def3370c5ae
Joe George <joe@zeroc.com>
2022-04-06 11:36:20 -0400

    Review fixes

--- tests/IceRpc.Tests/Slice/ProxyTests.cs
+++ tests/IceRpc.Tests/Slice/ProxyTests.cs
@@ -15,52 +15,45 @@ public class ProxyTests
     {
         get
         {
-            (string, string, IProxyFormat format, SliceEncoding)[] testData =
+            (string, string, SliceEncoding)[] testData =
             {
                 (
                     "icerpc://host:1000/identity?foo=bar",
                     "icerpc://host:1000/identity?foo=bar",
-                    UriProxyFormat.Instance,
-                    SliceEncoding.Slice20
+                    SliceEncoding.Slice2
                 ),
                 (
-                    "identity:tcp -h host -p 10000",
-                    "identity:tcp -h host -p 10000",
-                    IceProxyFormat.Default,
-                    SliceEncoding.Slice11
+                    "icerpc://host:1000/identity?foo=bar",
+                    "icerpc://host:1000/identity?foo=bar",
+                    SliceEncoding.Slice1
                 ),
                 (
-                    "identity:opaque -t 1 -e 1.1 -v CTEyNy4wLjAuMeouAAAQJwAAAA==",
-                    "identity:tcp -h 127.0.0.1 -p 12010 -t 10000",
-                    IceProxyFormat.Default,
-                    SliceEncoding.Slice11
+                    "ice://host:10000/identity?transport=tcp",
+                    "ice://host:10000/identity?transport=tcp",
+                    SliceEncoding.Slice2
                 ),
                 (
-                    "identity:opaque -t 1 -e 1.1 -v CTEyNy4wLjAuMeouAAAQJwAAAA==",
-                    "identity:opaque -t 1 -e 1.1 -v CTEyNy4wLjAuMeouAAAQJwAAAA==",
-                    IceProxyFormat.Default,
-                    SliceEncoding.Slice20
+                    "ice://host:10000/identity?transport=tcp",
+                    "ice://host:10000/identity?transport=tcp",
+                    SliceEncoding.Slice1
                 ),
                 (
-                    "identity:opaque -t 99 -e 1.1 -v abcd", // 99 = unknown and -t -e -v in this order
-                    "identity:opaque -t 99 -v abcd",
-                    IceProxyFormat.Default,
-                    SliceEncoding.Slice11
+                    "ice://opaque/identity?e=1.1&t=1&transport=opaque&v=CTEyNy4wLjAuMeouAAAQJwAAAA==",
+                    "ice://opaque/identity?e=1.1&t=1&transport=opaque&v=CTEyNy4wLjAuMeouAAAQJwAAAA==",
+                    SliceEncoding.Slice2
                 ),
                 (
-                    "identity:opaque -t 99 -e 1.1 -v abcd", // 99 = unknown and -t -e -v in this order
-                    "identity:opaque -t 99 -e 1.1 -v abcd",
-                    IceProxyFormat.Default,
-                    SliceEncoding.Slice20
+                    "ice://opaque/identity?e=1.1&t=1&transport=opaque&v=CTEyNy4wLjAuMeouAAAQJwAAAA==",
+                    "ice://127.0.0.1:12010/identity?transport=tcp&t=10000",
+                    SliceEncoding.Slice1
                 )
             };
             foreach ((
                 string value,
                 string expected,
-                IProxyFormat format,
                 SliceEncoding encoding) in testData)
             {
-                yield return new TestCaseData(value, expected, format, encoding);
+                yield return new TestCaseData(Proxy.Parse(value), Proxy.Parse(expected), encoding);
             }
         }
     }
@@ -68,22 +61,17 @@ public class ProxyTests
     /// <summary>Verifies that calling <see cref="SliceDecoder.DecodeProxy"/> correctly decodes a proxy.</summary>
     /// <param name="value">The proxy to encode.</param>
     /// <param name="expected">The expected proxy string.</param>
-    /// <param name="format">The proxy string format.</param>
     /// <param name="encoding">The encoding used to decode the proxy.</param>
     [Test, TestCaseSource(nameof(DecodeProxyDataSource))]
-    public void Decode_proxy(string value, string expected, IProxyFormat format, SliceEncoding encoding)
+    public void Decode_proxy(Proxy value, Proxy expected, SliceEncoding encoding)
     {
-        byte[] buffer = new byte[256];
-        var bufferWriter = new MemoryBufferWriter(buffer);
+        var bufferWriter = new MemoryBufferWriter(new byte[256]);
         var encoder = new SliceEncoder(bufferWriter, encoding);
-        Proxy proxy = format.Parse(value, Proxy.DefaultInvoker);
-        encoder.EncodeProxy(proxy);
-
-        var sut = new SliceDecoder(buffer, encoding: encoding);
+        encoder.EncodeProxy(value);
+        var sut = new SliceDecoder(bufferWriter.WrittenMemory, encoding: encoding);
 
         Proxy decoded = sut.DecodeProxy();
 
-        Proxy expectedProxy = format.Parse(expected, Proxy.DefaultInvoker);
-        Assert.That(decoded, Is.EqualTo(expectedProxy));
+        Assert.That(decoded, Is.EqualTo(expected));
     }
 }
