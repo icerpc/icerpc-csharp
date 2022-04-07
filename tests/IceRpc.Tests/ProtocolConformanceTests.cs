3bbc5b908f3600d38f74d3640a3b3a39a48908c0
Benoit Foucher <benoit@zeroc.com>
2022-04-07 13:19:01 +0200

    Add protocol conformance tests

---
+++ tests/IceRpc.Tests/ProtocolConformanceTests.cs
@@ -0,0 +1,220 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using IceRpc.Configure;
+using IceRpc.Transports;
+using IceRpc.Internal;
+using IceRpc.Slice;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Buffers;
+using System.Collections.Immutable;
+
+namespace IceRpc.Tests;
+
+[Timeout(5000)]
+[Parallelizable(ParallelScope.All)]
+public sealed class ProtocolConformanceTests
+{
+    private static IEnumerable<Protocol> Protocols
+    {
+        get
+        {
+            yield return Protocol.Ice;
+            yield return Protocol.IceRpc;
+        }
+    }
+
+    private static IEnumerable<TestCaseData> ProtocolsAndClientServer
+    {
+        get
+        {
+            yield return new TestCaseData(Protocol.Ice, ConnectionType.Client);
+            yield return new TestCaseData(Protocol.IceRpc, ConnectionType.Client);
+            yield return new TestCaseData(Protocol.Ice, ConnectionType.Server);
+            yield return new TestCaseData(Protocol.IceRpc, ConnectionType.Server);
+        }
+    }
+
+    public enum ConnectionType
+    {
+        Client,
+        Server
+    }
+
+    [Test, TestCaseSource(nameof(Protocols))]
+    public async Task Connection_has_invocation_and_dispatch_in_progress(Protocol protocol)
+    {
+        // Arrange
+        var result = new TaskCompletionSource<bool>();
+        (IProtocolConnection Client, IProtocolConnection Server)? sut = null;
+        await using var serviceProvider = new ProtocolServiceCollection()
+            .UseProtocol(protocol)
+            .UseServerConnectionOptions(new ConnectionOptions()
+                {
+                    Dispatcher = new InlineDispatcher((request, cancel) =>
+                        {
+                            result.SetResult(
+                                sut!.Value.Client.HasInvocationsInProgress &&
+                                sut!.Value.Server.HasDispatchesInProgress);
+                            return new(new OutgoingResponse(request));
+                        })
+                })
+            .BuildServiceProvider();
+
+        sut = await serviceProvider.GetClientServerProtocolConnectionAsync();
+        _ = sut.Value.Server.AcceptRequestsAsync();
+
+        // Act
+        await sut.Value.Client.SendRequestAsync(new OutgoingRequest(new Proxy(protocol)), default);
+
+        // Assert
+        Assert.That(await result.Task, Is.True);
+    }
+
+    [Test, TestCaseSource(nameof(Protocols))]
+    public async Task Dispose_the_protocol_connections(Protocol protocol)
+    {
+        // Arrange
+        await using var serviceProvider = new ProtocolServiceCollection().UseProtocol(protocol).BuildServiceProvider();
+        var sut = await serviceProvider.GetClientServerProtocolConnectionAsync();
+
+        // Act
+        sut.Client.Dispose();
+        sut.Server.Dispose();
+    }
+
+    [Test]
+    public async Task Initialize_peer_fields()
+    {
+        // Arrange
+        await using var serviceProvider = new ProtocolServiceCollection()
+            .UseProtocol(Protocol.IceRpc)
+            .UseServerConnectionOptions(new()
+            {
+                Fields = new Dictionary<ConnectionFieldKey, OutgoingFieldValue>()
+                        .With(ConnectionFieldKey.MaxHeaderSize, (ref SliceEncoder encoder) => encoder.EncodeInt(56))
+            })
+            .UseClientConnectionOptions(new()
+            {
+                Fields = new Dictionary<ConnectionFieldKey, OutgoingFieldValue>()
+                        .With(ConnectionFieldKey.MaxHeaderSize, (ref SliceEncoder encoder) => encoder.EncodeInt(34))
+                        .With((ConnectionFieldKey)10, (ref SliceEncoder encoder) => encoder.EncodeInt(38))
+            })
+            .BuildServiceProvider();
+
+        // Act
+        var sut = await serviceProvider.GetClientServerProtocolConnectionAsync(); // Initializes the connections
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(sut.Server.PeerFields, Has.Count.EqualTo(2));
+            Assert.That(sut.Client.PeerFields, Has.Count.EqualTo(1));
+            Assert.That(DecodeField(sut.Server.PeerFields, ConnectionFieldKey.MaxHeaderSize), Is.EqualTo(34));
+            Assert.That(DecodeField(sut.Server.PeerFields, (ConnectionFieldKey)10), Is.EqualTo(38));
+            Assert.That(DecodeField(sut.Client.PeerFields, ConnectionFieldKey.MaxHeaderSize), Is.EqualTo(56));
+        });
+
+        static int DecodeField(
+            ImmutableDictionary<ConnectionFieldKey, ReadOnlySequence<byte>> fields,
+            ConnectionFieldKey key) =>
+            fields.DecodeValue(key, (ref SliceDecoder decoder) => decoder.DecodeInt());
+    }
+
+    [Test, TestCaseSource(nameof(ProtocolsAndClientServer))]
+    public async Task PeerShutdownInitiated_callback_is_called(Protocol protocol, ConnectionType connectionType)
+    {
+        // Arrange
+        await using var serviceProvider = new ProtocolServiceCollection().UseProtocol(protocol).BuildServiceProvider();
+        var sut = await serviceProvider.GetClientServerProtocolConnectionAsync();
+
+        IProtocolConnection connection1 = connectionType == ConnectionType.Client ? sut.Server : sut.Client;
+        IProtocolConnection connection2 = connectionType == ConnectionType.Client ? sut.Client : sut.Server;
+
+        var shutdownInitiatedCalled = new TaskCompletionSource<string>();
+        connection2.PeerShutdownInitiated = message =>
+            {
+                shutdownInitiatedCalled.SetResult(message);
+                _ = connection2.ShutdownAsync("", default);
+            };
+
+        // Act
+        await connection1.ShutdownAsync("hello world", default);
+
+        // Assert
+        string message = protocol == Protocol.Ice ? "connection shutdown by peer" : "hello world";
+        Assert.That(await shutdownInitiatedCalled.Task, Is.EqualTo(message));
+    }
+}
+
+internal class ProtocolServiceCollection : TransportServiceCollection
+{
+    public ProtocolServiceCollection()
+    {
+        this.AddSingleton(IceProtocol.Instance.ProtocolConnectionFactory);
+        this.AddSingleton(IceRpcProtocol.Instance.ProtocolConnectionFactory);
+    }
+}
+
+internal static class ProtocolServiceCollectionExtensions
+{
+    internal static IServiceCollection UseProtocol(this IServiceCollection collection, Protocol protocol) =>
+        collection.AddSingleton(protocol);
+
+    internal static IServiceCollection UseServerConnectionOptions(
+        this IServiceCollection collection,
+        ConnectionOptions options) =>
+        collection.AddSingleton(new ServerConnectionOptions(options));
+
+    internal static IServiceCollection UseClientConnectionOptions(
+        this IServiceCollection collection,
+        ConnectionOptions options) =>
+        collection.AddSingleton(new ClientConnectionOptions(options));
+
+    internal static Task<IProtocolConnection> GetClientProtocolConnectionAsync(
+        this IServiceProvider serviceProvider) => serviceProvider.GetRequiredService<Protocol>() == Protocol.Ice ?
+         GetProtocolConnectionAsync(serviceProvider, false, serviceProvider.GetSimpleClientConnectionAsync) :
+         GetProtocolConnectionAsync(serviceProvider, false, serviceProvider.GetMultiplexedClientConnectionAsync);
+
+    internal static Task<IProtocolConnection> GetServerProtocolConnectionAsync(
+        this IServiceProvider serviceProvider) => serviceProvider.GetRequiredService<Protocol>() == Protocol.Ice ?
+         GetProtocolConnectionAsync(serviceProvider, true, serviceProvider.GetSimpleServerConnectionAsync) :
+         GetProtocolConnectionAsync(serviceProvider, true, serviceProvider.GetMultiplexedServerConnectionAsync);
+
+    internal static async Task<(IProtocolConnection Client, IProtocolConnection Server)> GetClientServerProtocolConnectionAsync(
+        this IServiceProvider serviceProvider)
+    {
+        Task<IProtocolConnection> serverTask = serviceProvider.GetServerProtocolConnectionAsync();
+        IProtocolConnection clientConnection = await serviceProvider.GetClientProtocolConnectionAsync();
+        IProtocolConnection serverConnection = await serverTask;
+        return (clientConnection, serverConnection);
+    }
+
+    private static async Task<IProtocolConnection> GetProtocolConnectionAsync<T>(
+        IServiceProvider serviceProvider,
+        bool isServer,
+        Func<Task<T>> networkConnectionFactory) where T : INetworkConnection =>
+        await serviceProvider.GetRequiredService<IProtocolConnectionFactory<T>>().CreateProtocolConnectionAsync(
+            await networkConnectionFactory(),
+            connectionInformation: new(),
+            connection: null!,
+            connectionOptions: isServer ?
+                serviceProvider.GetService<ServerConnectionOptions>()?.Value ?? new() :
+                serviceProvider.GetService<ClientConnectionOptions>()?.Value ?? new(),
+            isServer,
+            CancellationToken.None);
+
+    private sealed class ClientConnectionOptions
+    {
+        internal ConnectionOptions Value { get; }
+
+        internal ClientConnectionOptions(ConnectionOptions options) => Value = options;
+    }
+
+    private sealed class ServerConnectionOptions
+    {
+        internal ConnectionOptions Value { get; }
+
+        internal ServerConnectionOptions(ConnectionOptions options) => Value = options;
+    }
+}
