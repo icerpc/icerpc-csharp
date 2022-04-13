e498cb65f54c27a3d56ad3ce7a73be4ae389630d
Jose <jose@zeroc.com>
2022-04-13 14:40:20 +0200

    More tests

--- tests/IceRpc.Tests/Slice/StructTests.cs
+++ tests/IceRpc.Tests/Slice/StructTests.cs
@@ -36,9 +36,43 @@ public sealed class StructTests
         Assert.That(decoded, Is.EqualTo(expected));
     }
 
-    [Test, TestCaseSource(nameof(EncodeDecodeStructWithTaggedMembersSource))]
-    public void Encode_decode_struct_with_tagged_members(MyStructWithTaggedMembers expected)
+    [Test]
+    public void Encode_decode_compact_struct(
+        [Values(SliceEncoding.Slice1, SliceEncoding.Slice2)]SliceEncoding encoding)
     {
+        var buffer = new MemoryBufferWriter(new byte[256]);
+        var encoder = new SliceEncoder(buffer, encoding);
+        var expected = new MyCompactStruct(10, 20);
+        expected.Encode(ref encoder);
+        var decoder = new SliceDecoder(buffer.WrittenMemory, encoding);
+
+        var decoded = new MyCompactStruct(ref decoder);
+
+        Assert.That(decoded, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Encode_decode_struct_with_optional_members(
+        [Values(10, null)] int? k,
+        [Values(20, null)] int? l)
+    {
+        var expected = new MyStructWithOptionalMembers(10, 20, k, l);
+        var buffer = new MemoryBufferWriter(new byte[256]);
+        var encoder = new SliceEncoder(buffer, SliceEncoding.Slice2);
+        expected.Encode(ref encoder);
+        var decoder = new SliceDecoder(buffer.WrittenMemory, SliceEncoding.Slice2);
+
+        var decoded = new MyStructWithOptionalMembers(ref decoder);
+
+        Assert.That(decoded, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Encode_decode_struct_with_tagged_members(
+        [Values(10, null)] int? k,
+        [Values(20, null)] int? l)
+    {
+        var expected = new MyStructWithTaggedMembers(10, 20, k, l);
         var buffer = new MemoryBufferWriter(new byte[256]);
         var encoder = new SliceEncoder(buffer, SliceEncoding.Slice2);
         expected.Encode(ref encoder);
