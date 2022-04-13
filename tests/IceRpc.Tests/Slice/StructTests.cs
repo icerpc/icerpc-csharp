92a92fd00a956036827a3786b812c26fee21a23f
Jose <jose@zeroc.com>
2022-04-13 14:40:43 +0200

    cleanup

--- tests/IceRpc.Tests/Slice/StructTests.cs
+++ tests/IceRpc.Tests/Slice/StructTests.cs
@@ -12,22 +12,12 @@ public interface INotImplementedTrait : ITrait { }
 [Parallelizable(ParallelScope.All)]
 public sealed class StructTests
 {
-    public static IEnumerable<MyStruct> EncodeDecodeSimpleStructSource =>
-        Enumerable.Range(0, 12).Select(x => new MyStruct(x, x * 33));
-
-    public static IEnumerable<MyStructWithTaggedMembers> EncodeDecodeStructWithTaggedMembersSource =>
-        Enumerable.Range(0, 12).Select(
-            i => new MyStructWithTaggedMembers(
-                i,
-                i * 33,
-                i % 2 == 0 ? i : null,
-                i % 3 == 0 ? i : null));
-
-    [Test, TestCaseSource(nameof(EncodeDecodeSimpleStructSource))]
-    public void Encode_decode_simple_struct(MyStruct expected)
+    [Test]
+    public void Encode_decode_struct()
     {
         var buffer = new MemoryBufferWriter(new byte[256]);
         var encoder = new SliceEncoder(buffer, SliceEncoding.Slice2);
+        var expected = new MyStruct(10, 20);
         expected.Encode(ref encoder);
         var decoder = new SliceDecoder(buffer.WrittenMemory, SliceEncoding.Slice2);
 
@@ -83,11 +73,12 @@ public sealed class StructTests
         Assert.That(decoded, Is.EqualTo(expected));
     }
 
-    [Test, TestCaseSource(nameof(EncodeDecodeSimpleStructSource))]
-    public void Encode_decode_simple_struct_as_trait(MyStruct expected)
+    [Test]
+    public void Encode_decode_struct_as_trait()
     {
         var buffer = new MemoryBufferWriter(new byte[256]);
         var encoder = new SliceEncoder(buffer, SliceEncoding.Slice2);
+        var expected = new MyStruct(10, 20);
         expected.EncodeTrait(ref encoder);
         var decoder = new SliceDecoder(
             buffer.WrittenMemory,
