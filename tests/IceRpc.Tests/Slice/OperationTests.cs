36be421410f4dd6330b5eb6b3d2b14ebff1b9c1d
Jose <jose@zeroc.com>
2022-04-13 13:21:03 +0200

    Add test for Response generated classes

--- tests/IceRpc.Tests/Slice/OperationTests.cs
+++ tests/IceRpc.Tests/Slice/OperationTests.cs
@@ -1,58 +1,46 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using NUnit.Framework;
 using IceRpc.Configure;
-using System.IO.Pipelines;
+using NUnit.Framework;
 
 namespace IceRpc.Slice.Tests;
-
-public partial record struct MyStruct : IMyTrait {}
 
 [Parallelizable(scope: ParallelScope.All)]
 public class OperationTests
 {
-    public static IEnumerable<TestCaseData> OperationEncodeDecodeSource
-    {
-        get
-        {
-            yield return new TestCaseData(
-                (object)MyOperationsPrx.Request.OpInt,
-                (object)IMyOperations.Request.OpIntAsync,
-                10).SetName("Operation_encode_decode_with_single_parameter(OpInt)");
-
-            yield return new TestCaseData(
-                (object)MyOperationsPrx.Request.OpString,
-                (object)IMyOperations.Request.OpStringAsync,
-                "hello world!").SetName("Operation_encode_decode_with_single_parameter(OpString)");
-
-            yield return new TestCaseData(
-                (object)MyOperationsPrx.Request.OpMyStruct,
-                (object)IMyOperations.Request.OpMyStructAsync,
-                new MyStruct(1, 2)).SetName("Operation_encode_decode_with_single_parameter(OpMyStruct)");
-
-            yield return new TestCaseData(
-                (object)MyOperationsPrx.Request.OpMyTrait,
-                (object)IMyOperations.Request.OpMyTraitAsync,
-                new MyStruct(1, 2)).SetName("Operation_encode_decode_with_single_parameter(OpMyTrait)");
-        }
-    }
-
-    [Test, TestCaseSource(nameof(OperationEncodeDecodeSource))]
-    public async Task Operation_encode_decode_with_single_parameter<T>(
-        Func<T, PipeReader> encodeFunc,
-        Func<IncomingRequest, CancellationToken, ValueTask<T>> decodeFunc,
-        T value)
+    [Test]
+    public async Task Operation_encode_decode_with_single_parameter()
     {
         await using var connection = new Connection(new ConnectionOptions());
         var request = new IncomingRequest(Protocol.IceRpc)
         {
             Connection = connection,
-            Payload = encodeFunc(value)
+            Payload = MyOperationsPrx.Request.OpInt(10)
         };
 
-        T decodedValue = await decodeFunc(request, default);
+        int value = await IMyOperations.Request.OpIntAsync(request, default);
 
-        Assert.That(value, Is.EqualTo(decodedValue));
+        Assert.That(value, Is.EqualTo(10));
+    }
+
+    [Test]
+    public async Task Operation_encode_decode_with_single_return()
+    {
+        await using var connection = new Connection(new ConnectionOptions());
+        var request = new OutgoingResponse(new IncomingRequest(Protocol.IceRpc))
+        {
+            Payload = IMyOperations.Response.OpInt(10)
+        };
+
+        var response = new IncomingResponse(new OutgoingRequest(new Proxy(Protocol.IceRpc)))
+        {
+            Connection = connection,
+            Payload = request.Payload
+        };
+
+        int value = await MyOperationsPrx.Response.OpIntAsync(response, default);
+
+        Assert.That(value, Is.EqualTo(10));
     }
 
     [Test]
@@ -65,10 +53,31 @@ public class OperationTests
             Payload = MyOperationsPrx.Request.OpIntAndString(10, "hello world!")
         };
 
-        var decodedValue = await IMyOperations.Request.OpIntAndStringAsync(request, default);
+        var value = await IMyOperations.Request.OpIntAndStringAsync(request, default);
 
-        Assert.That(decodedValue.P1, Is.EqualTo(10));
-        Assert.That(decodedValue.P2, Is.EqualTo("hello world!"));
+        Assert.That(value.P1, Is.EqualTo(10));
+        Assert.That(value.P2, Is.EqualTo("hello world!"));
+    }
+
+    [Test]
+    public async Task Operation_encode_decode_with_multiple_return()
+    {
+        await using var connection = new Connection(new ConnectionOptions());
+        var request = new OutgoingResponse(new IncomingRequest(Protocol.IceRpc))
+        {
+            Payload = IMyOperations.Response.OpIntAndString(10, "hello world!")
+        };
+
+        var response = new IncomingResponse(new OutgoingRequest(new Proxy(Protocol.IceRpc)))
+        {
+            Connection = connection,
+            Payload = request.Payload
+        };
+
+        var value = await MyOperationsPrx.Response.OpIntAndStringAsync(response, default);
+
+        Assert.That(value.R1, Is.EqualTo(10));
+        Assert.That(value.R2, Is.EqualTo("hello world!"));
     }
 
     [Test]
@@ -77,7 +86,7 @@ public class OperationTests
         [Values("hello world!", null)] string? p4)
     {
         const int p1 = 10;
-        const string p2 = "hellow world!";
+        const string p2 = "hello world!";
         await using var connection = new Connection(new ConnectionOptions());
         var request = new IncomingRequest(Protocol.IceRpc)
         {
@@ -85,12 +94,39 @@ public class OperationTests
             Payload = MyOperationsPrx.Request.OpOptional(p1, p2, p3, p4)
         };
 
-        var decodedValue = await IMyOperations.Request.OpOptionalAsync(request, default);
+        var value = await IMyOperations.Request.OpOptionalAsync(request, default);
 
-        Assert.That(decodedValue.P1, Is.EqualTo(p1));
-        Assert.That(decodedValue.P2, Is.EqualTo(p2));
-        Assert.That(decodedValue.P3, Is.EqualTo(p3));
-        Assert.That(decodedValue.P4, Is.EqualTo(p4));
+        Assert.That(value.P1, Is.EqualTo(p1));
+        Assert.That(value.P2, Is.EqualTo(p2));
+        Assert.That(value.P3, Is.EqualTo(p3));
+        Assert.That(value.P4, Is.EqualTo(p4));
+    }
+
+    [Test]
+    public async Task Operation_encode_decode_with_optional_return(
+        [Values(10, null)] int? p3,
+        [Values("hello world!", null)] string? p4)
+    {
+        const int p1 = 10;
+        const string p2 = "hello world!";
+        await using var connection = new Connection(new ConnectionOptions());
+        var request = new OutgoingResponse(new IncomingRequest(Protocol.IceRpc))
+        {
+            Payload = IMyOperations.Response.OpOptional(p1, p2, p3, p4)
+        };
+
+        var response = new IncomingResponse(new OutgoingRequest(new Proxy(Protocol.IceRpc)))
+        {
+            Connection = connection,
+            Payload = request.Payload
+        };
+
+        var value = await MyOperationsPrx.Response.OpOptionalAsync(response, default);
+
+        Assert.That(value.R1, Is.EqualTo(p1));
+        Assert.That(value.R2, Is.EqualTo(p2));
+        Assert.That(value.R3, Is.EqualTo(p3));
+        Assert.That(value.R4, Is.EqualTo(p4));
     }
 
     [Test]
@@ -99,7 +135,7 @@ public class OperationTests
         [Values("hello world!", null)] string? p4)
     {
         const int p1 = 10;
-        const string p2 = "hellow world!";
+        const string p2 = "hello world!";
         await using var connection = new Connection(new ConnectionOptions());
         var request = new IncomingRequest(Protocol.IceRpc)
         {
@@ -107,11 +143,38 @@ public class OperationTests
             Payload = MyOperationsPrx.Request.OpTagged(p1, p2, p3, p4)
         };
 
-        var decodedValue = await IMyOperations.Request.OpTaggedAsync(request, default);
+        var value = await IMyOperations.Request.OpTaggedAsync(request, default);
 
-        Assert.That(decodedValue.P1, Is.EqualTo(p1));
-        Assert.That(decodedValue.P2, Is.EqualTo(p2));
-        Assert.That(decodedValue.P3, Is.EqualTo(p3));
-        Assert.That(decodedValue.P4, Is.EqualTo(p4));
+        Assert.That(value.P1, Is.EqualTo(p1));
+        Assert.That(value.P2, Is.EqualTo(p2));
+        Assert.That(value.P3, Is.EqualTo(p3));
+        Assert.That(value.P4, Is.EqualTo(p4));
+    }
+
+    [Test]
+    public async Task Operation_encode_decode_with_tagged_return(
+        [Values(10, null)] int? p3,
+        [Values("hello world!", null)] string? p4)
+    {
+        const int p1 = 10;
+        const string p2 = "hello world!";
+        await using var connection = new Connection(new ConnectionOptions());
+        var request = new OutgoingResponse(new IncomingRequest(Protocol.IceRpc))
+        {
+            Payload = IMyOperations.Response.OpTagged(p1, p2, p3, p4)
+        };
+
+        var response = new IncomingResponse(new OutgoingRequest(new Proxy(Protocol.IceRpc)))
+        {
+            Connection = connection,
+            Payload = request.Payload
+        };
+
+        var value = await MyOperationsPrx.Response.OpTaggedAsync(response, default);
+
+        Assert.That(value.R1, Is.EqualTo(p1));
+        Assert.That(value.R2, Is.EqualTo(p2));
+        Assert.That(value.R3, Is.EqualTo(p3));
+        Assert.That(value.R4, Is.EqualTo(p4));
     }
 }
