f3dd452eb287b8493929a1272c5a3e20b01c176a
Bernard Normier <bernard@zeroc.com>
2022-04-18 16:39:17 -0400

    Update test

--- tests/IceRpc.Tests/IceProtocolConnectionTests.cs
+++ tests/IceRpc.Tests/IceProtocolConnectionTests.cs
@@ -2,11 +2,8 @@
 
 using IceRpc.Configure;
 using IceRpc.Internal;
-using IceRpc.Slice;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
-using System.Buffers;
-using System.Collections.Immutable;
 
 namespace IceRpc.Tests;
 
@@ -14,6 +11,92 @@ namespace IceRpc.Tests;
 [Parallelizable(ParallelScope.All)]
 public sealed class IceProtocolConnectionTests
 {
+    /// <summary>Verifies that concurrent dispatches on a given ice connection are limited to MaxConcurrentDispatches.
+    /// </summary>
+    [Test]
+    public async Task Connection_dispatches_requests_concurrently_up_to_max_concurrent_dispatches(
+        [Values(1, 200)] int maxConcurrentDispatches)
+    {
+        // Arrange
+        using var startSemaphore = new SemaphoreSlim(0);
+        using var workSemaphore = new SemaphoreSlim(0);
+        int count = 0;
+        int maxCount = 0;
+        var mutex = new object();
+
+        var serverConnectionOptions = new ConnectionOptions
+        {
+            Dispatcher = new InlineDispatcher(async (request, cancel) =>
+            {
+                await request.Payload.CompleteAsync(); // done with payload
+
+                // We want to make sure that no more than maxConcurrentDispatches are executing this dispatcher. So
+                // we are tracking the maximum count here (before work) and decrement this count immediately in the
+                // "work". Without the decrement, the count (and max count) could be greater than
+                // maxConcurrentDispatches.
+                IncrementCount();
+                startSemaphore.Release();
+                await workSemaphore.WaitAsync(cancel);
+                DecrementCount();
+                return new OutgoingResponse(request);
+
+                void DecrementCount()
+                {
+                    lock (mutex)
+                    {
+                        count--;
+                    }
+                }
+
+                void IncrementCount()
+                {
+                    lock (mutex)
+                    {
+                        count++;
+                        maxCount = Math.Max(maxCount, count);
+                    }
+                }
+            }),
+
+            IceProtocolOptions = new IceProtocolOptions { MaxConcurrentDispatches = maxConcurrentDispatches }
+        };
+
+        await using var serviceProvider = new ProtocolServiceCollection()
+            .UseProtocol(Protocol.Ice)
+            .UseServerConnectionOptions(serverConnectionOptions)
+            .BuildServiceProvider();
+
+        await using var sut = await serviceProvider.GetClientServerProtocolConnectionAsync();
+
+        var request = new OutgoingRequest(new Proxy(Protocol.Ice));
+        var responseTasks = new List<Task<IncomingResponse>>();
+
+        // Act
+        for (int i = 0; i < maxConcurrentDispatches + 1; ++i)
+        {
+            responseTasks.Add(sut.Client.InvokeAsync(request, default));
+        }
+        // wait for maxDispatchesPerConnection dispatches to start
+        for (int i = 0; i < maxConcurrentDispatches; ++i)
+        {
+            await startSemaphore.WaitAsync();
+        }
+
+        // Assert
+        for (int i = 0; i < maxConcurrentDispatches + 1; ++i)
+        {
+            Assert.That(responseTasks[i].IsCompleted, Is.False);
+        }
+
+        workSemaphore.Release(maxConcurrentDispatches + 1);
+
+        Assert.Multiple(async () =>
+        {
+            await Task.WhenAll(responseTasks);
+            Assert.That(maxCount, Is.EqualTo(maxConcurrentDispatches));
+        });
+    }
+
     /// <summary>Ensures that the request payload stream is completed even if the Ice protocol doesn't support
     /// it.</summary>
     [Test]
