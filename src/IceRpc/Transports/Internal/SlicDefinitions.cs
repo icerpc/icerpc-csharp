8d504bf9d70f1d053adb3764853459a86cd2f073
Austin Henriksen <austin@zeroc.com>
2022-04-20 18:20:19 -0400

    Removed 'FrameHeader'.

--- src/IceRpc/Transports/Internal/SlicDefinitions.cs
+++ src/IceRpc/Transports/Internal/SlicDefinitions.cs
@@ -4,11 +4,6 @@ namespace IceRpc.Transports.Internal
 {
     internal static class SlicDefinitions
     {
-        // The header below is used to reserve space in the protocol frame to avoid allocating a separate byte
-        // buffer. The Slic header is composed of a FrameType byte enum value, a FrameSize varuint32 value (4
-        // bytes) and a stream ID varuint62 value (8 bytes).
-        internal static readonly ReadOnlyMemory<byte> FrameHeader = new byte[13];
-
         internal const uint V1 = 1;
     }
 }
