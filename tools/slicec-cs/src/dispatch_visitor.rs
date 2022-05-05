23df564e5f78b7dd86247188d37c08810b659a5f
Jose <pepone@users.noreply.github.com>
2022-05-05 15:55:05 +0200

    Update tools/slicec-cs/src/dispatch_visitor.rs
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>

--- tools/slicec-cs/src/dispatch_visitor.rs
+++ tools/slicec-cs/src/dispatch_visitor.rs
@@ -86,7 +86,7 @@ fn request_class(interface_def: &Interface) -> CodeBlock {
     }
 
     let mut class_builder = ContainerBuilder::new(
-        &if bases.is_empty() {
+        if bases.is_empty() {
             "public static class"
         } else {
             "public static new class"
