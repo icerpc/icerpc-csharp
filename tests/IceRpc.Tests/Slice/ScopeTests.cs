2a04448764831bdc8c9589bbd8be27cb84d1432f
Jose <jose@zeroc.com>
2022-05-05 18:40:39 +0200

    cleanup

--- tests/IceRpc.Tests/Slice/ScopeTests.cs
+++ tests/IceRpc.Tests/Slice/ScopeTests.cs
@@ -1,11 +1,8 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using IceRpc.Configure;
 using IceRpc.Tests;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
-using System.Buffers;
-using System.IO.Pipelines;
 
 namespace IceRpc.Slice.Tests;
 
@@ -13,42 +10,41 @@ namespace IceRpc.Slice.Tests;
 public class ScopeTests
 {
     [Test]
-    public async Task Operation_with_parameter_defined_in_different_scopes1()
+    public async Task Operation_with_parameter_defined_in_different_scopes()
     {
         await using var provider = new SliceTestServiceCollection()
             .UseDispatcher(new ScopeOperations1())
             .BuildServiceProvider();
         var prx = ScopeOperationsPrx.FromConnection(provider.GetRequiredService<Connection>());
 
-        var r = await prx.OpAsync(new S(10));
+        var r = await prx.OpWithParameterDefinedInDifferentScopeAsync(new Inner.S(10));
 
         Assert.That(r.V, Is.EqualTo(10));
     }
 
     [Test]
-    public async Task Operation_with_parameter_defined_in_different_scopes2()
+    public async Task Operation_with_return_defined_in_different_scopes()
     {
         await using var provider = new SliceTestServiceCollection()
-            .UseDispatcher(new ScopeOperations2())
+            .UseDispatcher(new ScopeOperations1())
             .BuildServiceProvider();
-        var prx = Inner.ScopeOperationsPrx.FromConnection(provider.GetRequiredService<Connection>());
+        var prx = ScopeOperationsPrx.FromConnection(provider.GetRequiredService<Connection>());
 
-        var r = await prx.OpAsync(new Inner.S(10));
+        var r = await prx.OpWithReturnDefinedInDifferentScopeAsync(new S(10));
 
         Assert.That(r.V, Is.EqualTo(10));
     }
 
     public class ScopeOperations1 : Service, IScopeOperations
     {
-        public ValueTask<Inner.S> OpAsync(S s, Dispatch dispatch, CancellationToken cancel = default) =>
-            new(new Inner.S(s.V));
-    }
-
-    public class ScopeOperations2 : Service, Inner.IScopeOperations
-    {
-        public ValueTask<S> OpAsync(
+        public ValueTask<S> OpWithParameterDefinedInDifferentScopeAsync(
             Inner.S s,
             Dispatch dispatch,
             CancellationToken cancel) => new(new S(s.V));
+
+        public ValueTask<Inner.S> OpWithReturnDefinedInDifferentScopeAsync(
+            S s,
+            Dispatch dispatch,
+            CancellationToken cancel) => new(new Inner.S(s.V));
     }
 }
