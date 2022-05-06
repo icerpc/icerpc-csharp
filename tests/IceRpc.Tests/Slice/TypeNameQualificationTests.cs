ccdf28e8084709b1ef2663b70458bfc9cac8ab1a
Jose <jose@zeroc.com>
2022-05-06 11:08:22 +0200

    Fix review comments

--- tests/IceRpc.Tests/Slice/TypeNameQualificationTests.cs
+++ tests/IceRpc.Tests/Slice/TypeNameQualificationTests.cs
@@ -9,24 +9,26 @@ namespace IceRpc.Slice.Tests;
 [Parallelizable(scope: ParallelScope.All)]
 public class TypeNameQualificationTests
 {
+    /// <summary>Verifies that when a type is defined in multiple modules, the generated code doesn't mix up the
+    /// type names, and use the correct qualified type names.</summary>
     [Test]
-    public async Task Operation_with_parameter_defined_in_different_scopes()
+    public async Task Operation_with_parameter_type_name_defined_in_multiple_modules()
     {
         await using var provider = new SliceTestServiceCollection()
-            .UseDispatcher(new ScopeOperations1())
+            .UseDispatcher(new TypeNameQualification())
             .BuildServiceProvider();
         var prx = TypeNameQualificationOperationsPrx.FromConnection(provider.GetRequiredService<Connection>());
 
         var r = await prx.OpWithTypeNamesDefinedInMultipleModulesAsync(new Inner.S(10));
 
-        Assert.That(r.V, Is.EqualTo(10));
+        Assert.That(r.V, Is.EqualTo("10"));
     }
 
-    public class ScopeOperations1 : Service, ITypeNameQualificationOperations
+    public class TypeNameQualification : Service, ITypeNameQualificationOperations
     {
         public ValueTask<S> OpWithTypeNamesDefinedInMultipleModulesAsync(
             Inner.S s,
             Dispatch dispatch,
-            CancellationToken cancel) => new(new S(s.V));
+            CancellationToken cancel) => new(new S($"{s.V}"));
     }
 }
