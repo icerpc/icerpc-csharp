db690e1466c969a19e4742adfca16190ae50927a
Jose <jose@zeroc.com>
2022-05-11 11:51:17 +0200

    Fix #1190

---
+++ tests/IceRpc.Tests/Slice/Slice1NullableTests.cs
@@ -0,0 +1,42 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using IceRpc.Slice.Internal;
+using NUnit.Framework;
+
+namespace IceRpc.Slice.Tests;
+
+[Parallelizable(scope: ParallelScope.All)]
+public class Slice1NullableTests
+{
+    [Test]
+    public void Using_null_for_non_nullable_proxy_fails_during_decoding()
+    {
+        var buffer = new MemoryBufferWriter(new byte[256]);
+        var encoder = new SliceEncoder(buffer, SliceEncoding.Slice1);
+        encoder.EncodeNullableProxy(null);
+
+        Assert.That(
+            () =>
+            {
+                var decoder = new SliceDecoder(buffer.WrittenMemory, SliceEncoding.Slice1);
+                var decoded = new MyCompactSturctWithNonNullableProxy(ref decoder);
+            },
+            Throws.TypeOf<InvalidDataException>());
+    }
+
+    [Test]
+    public void Using_null_for_non_nullable_class_fails_during_decoding()
+    {
+        var buffer = new MemoryBufferWriter(new byte[256]);
+        var encoder = new SliceEncoder(buffer, SliceEncoding.Slice1);
+        encoder.EncodeNullableClass(null);
+
+        Assert.That(
+            () =>
+            {
+                var decoder = new SliceDecoder(buffer.WrittenMemory, SliceEncoding.Slice1);
+                var decoded = new MyCompactSturctWithNonNullableClass(ref decoder);
+            },
+            Throws.TypeOf<InvalidDataException>());
+    }
+}
