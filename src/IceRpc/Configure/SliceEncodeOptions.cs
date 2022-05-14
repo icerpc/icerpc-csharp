94d2d631e12a3ed7378bcae376ccdb1802b5b543
Bernard Normier <bernard@zeroc.com>
2022-05-13 21:06:40 -0400

    Change defaults and comments

--- src/IceRpc/Configure/SliceEncodeOptions.cs
+++ src/IceRpc/Configure/SliceEncodeOptions.cs
@@ -8,18 +8,19 @@ namespace IceRpc.Configure;
 public sealed record class SliceEncodeOptions
 {
     /// <summary>Returns the default value for <see cref="PipeOptions"/>.</summary>
-    public static PipeOptions DefaultPipeOptions { get; } =
-        new(pauseWriterThreshold: 0, useSynchronizationContext: false);
+    public static PipeOptions DefaultPipeOptions { get; } = new(useSynchronizationContext: false);
 
     /// <summary>Gets or sets the pipe options that the Slice engine uses when creating pipes. The Slice engine creates
     /// a pipe when encoding a request or response payload, and when encoding an async enumerable into
     /// <see cref="PipeReader"/>. We suggest you set these pipe options as follows:
-    /// - pool: the memory pool of your choice (or keep the default)
+    /// - pool: the memory pool of your choice, or keep the default
     /// - readerScheduler: keep the default (PipeScheduler.ThreadPool)
-    /// - writerScheduler: keep the default (not used when pauseWriterThreshold is 0)
-    /// - pauseWriterThreshold: set to 0
-    /// - resumeWriterThreshold: keep the default (-1)
-    /// - minimumSegmentSize: the size of your choice (or keep the default)
+    /// - writerScheduler: keep the default (PipeScheduler.ThreadPool)
+    /// - pauseWriterThreshold: customize for your needs or keep the default (has no effect on pipes used for payload
+    ///   encoding)
+    /// - resumeWriterThreshold: customize for your needs or keep the default (has no effect on pipes used for payload
+    ///   encoding)
+    /// - minimumSegmentSize: keep the default or customize together with the memory pool
     /// - useSynchronizationContext: false
     /// </summary>
     public PipeOptions PipeOptions { get; set; } = DefaultPipeOptions;
@@ -28,8 +29,8 @@ public sealed record class SliceEncodeOptions
     /// source async enumerable into a pipe writer and only flushes when no new value is available synchronously or it
     /// has written some number of bytes to this pipe writer.</summary>
     /// <value>The maximum number of bytes encoded synchronously in a stream without flushing the pipe writer. The
-    /// default value is 32KB.</value>
-    public int StreamFlushThreshold { get; set; } = 32 * 1024;
+    /// default value is 16 KB.</value>
+    public int StreamFlushThreshold { get; set; } = 16 * 1024;
 
     /// <summary>A default instance of <see cref="SliceEncodeOptions"/>.</summary>
     internal static SliceEncodeOptions Default { get; } = new();
