c99228bbcfa1398266ee3f7dfd5c53ed665d9777
Bernard Normier <bernard@zeroc.com>
2022-05-24 11:54:13 -0400

    Fix demos

--- examples/Bidir/Server/AlertSystem.cs
+++ examples/Bidir/Server/AlertSystem.cs
@@ -1,12 +1,16 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+using IceRpc.Features;
 using IceRpc.Slice;
 
 namespace Demo;
 
 public class AlertSystem : Service, IAlertSystem
 {
-    public async ValueTask AddObserverAsync(AlertObserverPrx observer, IFeatureCollection features, CancellationToken cancel)
+    public async ValueTask AddObserverAsync(
+        AlertObserverPrx observer,
+        IFeatureCollection features,
+        CancellationToken cancel)
     {
         await Task.Delay(TimeSpan.FromSeconds(5), cancel);
         string didHandle = await observer.AlertAsync(cancel: cancel) ? "did" : "did not";
