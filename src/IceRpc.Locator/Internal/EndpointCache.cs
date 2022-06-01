7093f68731783ec1143e403dee67015c6add67d4
Jose <jose@zeroc.com>
2022-06-01 22:18:46 +0200

    Move EndpointCacheLoggerExtensions

--- src/IceRpc.Locator/Internal/EndpointCache.cs
+++ src/IceRpc.Locator/Internal/EndpointCache.cs
@@ -6,42 +6,6 @@ using System.Diagnostics;
 
 namespace IceRpc.Locator.Internal;
 
-/// <summary>This class contains ILogger extension methods used by LogEndpointCacheDecorator.</summary>
-internal static partial class EndpointCacheLoggerExtensions
-{
-    [LoggerMessage(
-        EventId = (int)LocationEventIds.FoundEntry,
-        EventName = nameof(LocationEventIds.FoundEntry),
-        Level = LogLevel.Trace,
-        Message = "found {LocationKind} '{Location}' = '{Proxy}' in cache")]
-    internal static partial void LogFoundEntry(
-        this ILogger logger,
-        string locationKind,
-        Location location,
-        Proxy proxy);
-
-    [LoggerMessage(
-        EventId = (int)LocationEventIds.SetEntry,
-        EventName = nameof(LocationEventIds.SetEntry),
-        Level = LogLevel.Trace,
-        Message = "set {LocationKind} '{Location}' = '{Proxy}' in cache")]
-    internal static partial void LogSetEntry(
-        this ILogger logger,
-        string locationKind,
-        Location location,
-        Proxy proxy);
-
-    [LoggerMessage(
-        EventId = (int)LocationEventIds.RemovedEntry,
-        EventName = nameof(LocationEventIds.RemovedEntry),
-        Level = LogLevel.Trace,
-        Message = "removed {LocationKind} '{Location}' from cache")]
-    internal static partial void LogRemovedEntry(
-        this ILogger logger,
-        string locationKind,
-        Location location);
-}
-
 /// <summary>An endpoint cache maintains a dictionary of location to endpoint(s), where the endpoints are held by a
 /// dummy proxy. It also keeps track of the insertion time of each entry. It's consumed by
 /// <see cref="LocationResolver"/>.</summary>
