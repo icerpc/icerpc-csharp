c5076748db3a246861782b5d7023dbd0c0d56508
Jose <jose@zeroc.com>
2022-06-09 15:36:51 +0200

    Router extensions

--- src/IceRpc/RouterExtensions.cs
+++ src/IceRpc/RouterExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
 using IceRpc.Features;
+using IceRpc.Slice;
 
 namespace IceRpc
 {
@@ -8,6 +9,34 @@ namespace IceRpc
     /// </summary>
     public static class RouterExtensions
     {
+        /// <summary>Registers a route to a service that uses the service default path as the route path. If there is
+        /// an existing route at the same path, it is replaced.</summary>
+        /// <typeparam name="TService">The service type used to get the default path.</typeparam>
+        /// <param name="router">The router being configured.</param>
+        /// <param name="service">The target service of this route.</param>
+        /// <exception cref="InvalidOperationException">Thrown if <see cref="IDispatcher.DispatchAsync"/> was already
+        /// called on this router.</exception>
+        /// <seealso cref="Router.Mount(string, IDispatcher)"/>
+        public static void Map<TService>(this Router router, IDispatcher service)
+            where TService : class =>
+            router.Map(typeof(TService).GetDefaultPath(), service);
+
+        /// <summary>Creates a sub-router, configures this sub-router and mounts it (with
+        /// <see cref="Router.Mount(string, IDispatcher)"/>) at the given <c>prefix</c>.</summary>
+        /// <param name="router">The router being configured.</param>
+        /// <param name="prefix">The prefix of the route to the sub-router.</param>
+        /// <param name="configure">A delegate that configures the new sub-router.</param>
+        /// <returns>The new sub-router.</returns>
+        /// <exception cref="FormatException">Thrown if <paramref name="prefix"/> is not a valid path.</exception>
+        public static Router Route(this Router router, string prefix, Action<Router> configure)
+        {
+            Proxy.CheckPath(prefix);
+            var subRouter = new Router($"{router.AbsolutePrefix}{prefix}");
+            configure(subRouter);
+            router.Mount(prefix, subRouter);
+            return subRouter;
+        }
+
         /// <summary>Adds a middleware that creates and inserts the <see cref="IDispatchInformationFeature"/> feature
         /// in all requests.</summary>
         /// <param name="router">The router being configured.</param>
