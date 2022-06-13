1cf41a4cb58083d4741789b5f6b76982928d508e
Bernard Normier <bernard@zeroc.com>
2022-06-13 10:32:45 -0400

    Improve doc-comment

--- src/IceRpc/IConnection.cs
+++ src/IceRpc/IConnection.cs
@@ -17,9 +17,10 @@ public interface IConnection
     /// </summary>
     NetworkConnectionInformation? NetworkConnectionInformation { get; }
 
-    /// <summary>Adds a callback that will be executed when the closure of this connection is initiated. If the
-    /// connection is already shutting down or closed, this callback is executed immediately with this connection and
-    /// an instance of <see cref="ConnectionClosedException"/>.</summary>
+    /// <summary>Adds a callback that will be executed when the closure of this connection is initiated. The closure of
+    /// a connection can be initiated by a local call to Abort or ShutdownAsync, by the shutdown of the remote peer, or
+    /// by a transport error. If the connection is already shutting down or closed, this callback is executed
+    /// synchronously with this connection and an instance of <see cref="ConnectionClosedException"/>.</summary>
     /// <param name="callback">The callback to execute. It must not block or throw any exception.</param>
     void OnClose(Action<IConnection, Exception> callback);
 
