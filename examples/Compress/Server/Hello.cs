396d6717bf8ddecf2b9acbdd823b8529fbdd3371
Reece Humphreys <reece@zeroc.com>
2022-06-14 09:32:27 -0400

    Compression example (#1379)

---
+++ examples/Compress/Server/Hello.cs
@@ -0,0 +1,15 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using IceRpc.Features;
+using IceRpc.Slice;
+
+namespace Demo;
+
+public class Hello : Service, IHello
+{
+    public ValueTask<string> SayHelloAsync(string name, IFeatureCollection features, CancellationToken cancel)
+    {
+        Console.WriteLine($"{name} says hello!");
+        return new($"Hello, {name}!");
+    }
+}
---
+++ examples/Compress/Server/Program.cs
@@ -0,0 +1,26 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using Demo;
+using IceRpc;
+
+using var cancellationSource = new CancellationTokenSource();
+
+// Adding deflate middleware to the router
+Router router = new Router().UseDeflate();
+router.Map<IHello>(new Hello());
+
+await using var server = new Server(router);
+
+// Shuts down the server on Ctrl+C or Ctrl+Break
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    _ = server.ShutdownAsync();
+};
+
+// Start the server
+server.Listen();
+
+Console.WriteLine("Server is waiting for connections...");
+
+await server.ShutdownComplete;
