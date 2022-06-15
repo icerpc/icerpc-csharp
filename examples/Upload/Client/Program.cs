6e0175ad1dca143e49ec646fb24e8370ac5120be
Reece Humphreys <reece@zeroc.com>
2022-06-15 15:35:34 -0400

    Uploading file example (#1387)

---
+++ examples/Upload/Client/Program.cs
@@ -0,0 +1,24 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using Demo;
+using IceRpc;
+using System.IO.Pipelines;
+
+// Establish the connection to the server
+await using var connection = new ClientConnection("icerpc://127.0.0.1");
+IUploaderPrx uploader = UploaderPrx.FromConnection(connection);
+
+// Create a FileStream for the image to be uploaded. This stream will be disposed by IceRPC when it completes the
+// `PipeReader`.
+var image = new FileStream("Client/images/Earth.png", FileMode.Open);
+
+Console.WriteLine("Uploading image of the Earth...");
+
+// Wrap the FileStream in a PipeReader.
+PipeReader reader = PipeReader.Create(image);
+
+// Begin streaming the data to the server.
+// The IceRpc runtime will automatically complete the PipeReader for the client once the await returns.
+await uploader.UploadImageAsync(reader);
+
+Console.WriteLine("Image of the Earth uploaded");
