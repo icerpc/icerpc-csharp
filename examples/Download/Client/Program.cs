f623ebb33a428927f2c8571405edb17660552961
Reece Humphreys <reece@zeroc.com>
2022-06-16 15:40:39 -0400

    Updated comments

--- examples/Download/Client/Program.cs
+++ examples/Download/Client/Program.cs
@@ -11,14 +11,13 @@ IDownloaderPrx downloader = DownloaderPrx.FromConnection(connection);
 // Receive the stream from the server.
 PipeReader image = await downloader.DownloadImageAsync();
 
-// AsStream has a parameter `leaveOpen` which is set to `false` by default. When the stream is disposed, if
-// leaveOpen` is `false` then the PipeReader used to create the stream is completed.
+// The stream returned by AsStream completes `image` when it's disposed.
 using Stream imageStream = image.AsStream();
 
 // Create the file, or overwrite if the file exists.
 using FileStream fs = File.Create("Client/downloads/downloaded_earth.png");
 
-// Copy the image to the file stream.
+// Copy the image stream to the file stream.
 await imageStream.CopyToAsync(fs);
 
 Console.WriteLine("Image downloaded");
