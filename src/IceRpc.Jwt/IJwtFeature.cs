d5dc8228b86fbfb89768ab8ff55bb7ebac7c4a40
Jose <pepone@users.noreply.github.com>
2022-06-20 22:17:23 +0200

    Update src/IceRpc.Jwt/IJwtFeature.cs
    Co-authored-by: Joe George <joe@externl.com>

--- src/IceRpc.Jwt/IJwtFeature.cs
+++ src/IceRpc.Jwt/IJwtFeature.cs
@@ -4,7 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 
 namespace IceRpc.Jwt;
 
-/// <summary>A feature that represents a Jwt token that can be transmitted in an IceRPC field.</summary>
+/// <summary>A feature that represents a JWT (JSON Web Token) that can be transmitted in an IceRPC field.</summary>
 public interface IJwtFeature
 {
     /// <summary>Gets or sets the value of this JWT feature.</summary>
