0d5eea7635e87bd22ec44392d62100e76e590541
Joe George <joe@zeroc.com>
2022-08-04 10:24:43 -0400

    Add support for file-scoped namespace declarations (#1568)
    Closes #1560

--- tools/slicec-cs/src/module_visitor.rs
+++ tools/slicec-cs/src/module_visitor.rs
@@ -28,6 +28,8 @@ impl Visitor for ModuleVisitor<'_> {
 
 impl ModuleVisitor<'_> {
     fn module_code_block(&mut self, module: &Module, module_prefix: Option<String>) -> CodeBlock {
+        let code_blocks = self.generated_code.remove_scoped(module);
+
         let module_identifier = match &module_prefix {
             Some(prefix) => {
                 // If there is a prefix the previous module was empty and we keep the prefix in the
@@ -42,7 +44,6 @@ impl ModuleVisitor<'_> {
             },
         };
 
-        let code_blocks = self.generated_code.remove_scoped(module);
         // If this module has any code blocks the submodules are mapped to namespaces inside the
         // current namespace (not using a prefix), otherwise if the current module doesn't
         // contain any code blocks we map the submodules with this module as a prefix.
@@ -62,14 +63,35 @@ impl ModuleVisitor<'_> {
             .collect();
 
         if let Some(vec) = code_blocks {
-            let mut builder = ContainerBuilder::new("namespace", &module_identifier);
+            // Generate file-scoped namespace declaration if
+            // - no other code blocks have been generated from another module
+            // - all scoped code blocks have been consumed (can be no other modules or sub-modules)
+            // - no submodules code for this module
+            // - we are in a top-level module or,
+            // - a submodule who's parent has no generated code code (ie. module_prefix.is_some())
+            if self.generated_code.code_blocks.is_empty()
+                && self.generated_code.scoped_code_blocks.is_empty()
+                && submodules_code.is_empty()
+                && (module_prefix.is_some() || module.is_top_level())
+            {
+                let mut code_block = CodeBlock::new();
+                writeln!(code_block, "namespace {};", module_identifier);
 
-            for code in vec {
-                builder.add_block(code);
+                for code in vec {
+                    code_block.add_block(&code);
+                }
+
+                code_block
+            } else {
+                let mut builder = ContainerBuilder::new("namespace", &module_identifier);
+
+                for code in vec {
+                    builder.add_block(code);
+                }
+
+                builder.add_block(submodules_code);
+                builder.build()
             }
-
-            builder.add_block(submodules_code);
-            builder.build()
         } else {
             submodules_code
         }
