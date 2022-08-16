d0fdf46a9ddc1a71654ec40cf6314f64dae30dbe
Bernard Normier <bernard@zeroc.com>
2022-08-16 17:41:08 -0400

    Cleanup

--- examples/Retry/Client/Program.cs
+++ examples/Retry/Client/Program.cs
@@ -43,6 +43,7 @@ var pipeline = new Pipeline()
     .UseLogger(loggerFactory)
     .Into(connectionCache);
 
+// We use a logger to ensure proper ordering of the messages on the console.
 var logger = loggerFactory.CreateLogger("IceRpc.RetryExample");
 
 string helloServiceAddress = "icerpc://127.0.0.1:10000/hello?alt-server=127.0.0.1:10001";
@@ -67,18 +68,19 @@ if (Console.ReadLine() is string name)
             await Task.Delay(TimeSpan.FromSeconds(3), cancel);
         }
     }
-    catch (DispatchException ex)
+    catch (DispatchException dispatchException)
     {
-        // The request failed because we reached the allowed max attempts or because all server addresses were excluded due
-        // to the failure retry policy.
-        Console.WriteLine(ex);
+        // The request failed because we reached the allowed max attempts or because all server addresses were excluded
+        // due to the failure retry policy.
+        logger.LogException(dispatchException);
     }
     catch (OperationCanceledException)
     {
+        // Expected, from Ctrl+C.
     }
 }
 
-internal static partial class LoggerExtensions
+internal static partial class RetryExampleLoggerExtensions
 {
     [LoggerMessage(
         EventId = 1,
@@ -93,4 +95,11 @@ internal static partial class LoggerExtensions
         Level = LogLevel.Information,
         Message = "Looping in 3 seconds, press Ctrl+C to exit\n")]
     internal static partial void LogLooping(this ILogger logger);
+
+    [LoggerMessage(
+        EventId = 3,
+        EventName = "Exception",
+        Level = LogLevel.Error,
+        Message = "Invocation failed with an exception, exiting")]
+    internal static partial void LogException(this ILogger logger, Exception exception);
 }
