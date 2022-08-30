3da972c8be9baf41721be1b4a3d20ff694bf0ea9
Bernard Normier <bernard@zeroc.com>
2022-08-30 09:45:07 -0400

    Convert TransportConnectionInformation into a record class (#1692)

--- src/IceRpc/Internal/LogClientProtocolConnectionFactoryDecorator.cs
+++ src/IceRpc/Internal/LogClientProtocolConnectionFactoryDecorator.cs
@@ -40,8 +40,7 @@ internal class LogClientProtocolConnectionFactoryDecorator : IClientProtocolConn
             {
                 TransportConnectionInformation result = await _decoratee.ConnectAsync(cancellationToken)
                     .ConfigureAwait(false);
-                Debug.Assert(result.LocalNetworkAddress is not null);
-                Debug.Assert(result.RemoteNetworkAddress is not null);
+
                 _localNetworkAddress = result.LocalNetworkAddress;
                 ClientEventSource.Log.ConnectSuccess(ServerAddress, _localNetworkAddress, result.RemoteNetworkAddress);
                 return result;
