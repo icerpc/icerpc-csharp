01f1bc53f9fdb565ef9dd5b84d5b92f8ccbaf1c9
Jose <pepone@users.noreply.github.com>
2022-09-01 20:51:41 +0200

    Test for canceling reads on outgoing request payload (#1725)

--- tests/IceRpc.Tests/InvocationTests.cs
+++ tests/IceRpc.Tests/InvocationTests.cs
@@ -4,6 +4,7 @@ using IceRpc.Slice;
 using IceRpc.Tests.Common;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
+using System.IO.Pipelines;
 
 namespace IceRpc.Tests;
 
@@ -38,11 +39,15 @@ public class InvocationTests
         IncomingResponse response = await callbackInvoker!.InvokeAsync(request);
 
         // Assert
-        var exception = await response.DecodeFailureAsync(
+        RemoteException exception = await response.DecodeFailureAsync(
             request,
             new ServiceProxy(callbackInvoker, callback.ServiceAddress));
-        Assert.That(exception, Is.InstanceOf<DispatchException>());
-        Assert.That(((DispatchException)exception).ErrorCode, Is.EqualTo(DispatchErrorCode.ServiceNotFound));
+        Assert.Multiple(
+            () =>
+            {
+                Assert.That(exception, Is.InstanceOf<DispatchException>());
+                Assert.That(((DispatchException)exception).ErrorCode, Is.EqualTo(DispatchErrorCode.ServiceNotFound));
+            });
     }
 
     /// <summary>Verifies that a callback on a connection without dispatcher does not accept requests with the icerpc
@@ -74,5 +79,62 @@ public class InvocationTests
         Assert.That(
             async () => await callbackInvoker!.InvokeAsync(request, cts.Token),
             Throws.InstanceOf<OperationCanceledException>());
+    }
+
+    [Test]
+    public async Task Cancel_the_payload_reads_while_the_server_is_reading_the_arguments_fails_with_dispatch_exception()
+    {
+        // Arrange
+        await using ServiceProvider provider = new ServiceCollection()
+            .AddColocTest(new InlineDispatcher(
+                async (request, cancellationToken) =>
+                {
+                    await request.Payload.ReadAllAsync(cancellationToken);
+                    return new OutgoingResponse(request);
+                }))
+            .BuildServiceProvider(validateScopes: true);
+
+        provider.GetRequiredService<Server>().Listen();
+        ClientConnection connection = provider.GetRequiredService<ClientConnection>();
+
+        var pipe = new Pipe();
+        await pipe.Writer.WriteAsync(new ReadOnlyMemory<byte>(new byte[] { 0x1, 0x2, 0x3 }));
+
+        var request = new OutgoingRequest(new ServiceAddress(new Uri("icerpc:/test")));
+        request.Payload = new CancelPendingReadPipeReader(pipe.Reader);
+
+        // Act
+        IncomingResponse responnse = await connection.InvokeAsync(request);
+
+        // Assert
+        RemoteException exception = await responnse.DecodeFailureAsync(request, new ServiceProxy());
+        Assert.Multiple(
+            () =>
+            {
+                Assert.That(responnse.ResultType, Is.EqualTo(ResultType.Failure));
+                Assert.That(exception, Is.TypeOf<DispatchException>());
+                DispatchException dispatchException = (DispatchException)exception;
+                Assert.That(dispatchException.ErrorCode, Is.EqualTo(DispatchErrorCode.UnhandledException));
+            });
+    }
+
+    // A pipe reader that calls CancelPendingRead after the first read
+    private class CancelPendingReadPipeReader : PipeReader
+    {
+        private readonly PipeReader _decoratee;
+
+        internal CancelPendingReadPipeReader(PipeReader decoratee) => _decoratee = decoratee;
+
+        public override void AdvanceTo(SequencePosition consumed) => _decoratee.AdvanceTo(consumed);
+        public override void AdvanceTo(SequencePosition consumed, SequencePosition examined) => _decoratee.AdvanceTo(consumed, examined);
+        public override void CancelPendingRead() => _decoratee.CancelPendingRead();
+        public override void Complete(Exception? exception = null) => _decoratee.Complete(exception);
+        public override ValueTask<ReadResult> ReadAsync(CancellationToken cancellationToken = default)
+        {
+            var result = _decoratee.ReadAsync(cancellationToken);
+            CancelPendingRead();
+            return result;
+        }
+        public override bool TryRead(out ReadResult result) => _decoratee.TryRead(out result);
     }
 }
