a546a60500bcc47022ea76dea88181055695ee05
Jose <jose@zeroc.com>
2022-09-21 18:44:52 +0200

    No repeat

--- tests/IceRpc.Tests/ClientConnectionTests.cs
+++ tests/IceRpc.Tests/ClientConnectionTests.cs
@@ -97,7 +97,6 @@ public class ClientConnectionTests
     }
 
     [Test, TestCaseSource(nameof(Protocols))]
-    [Repeat(500)]
     public async Task Connection_invoke_reconnect_after_underlying_connection_shutdown(Protocol protocol)
     {
         // Arrange
