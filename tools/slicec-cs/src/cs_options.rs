f9b38be751fbd124c750df7365a2049b8b4bc070
Reece Humphreys <reece@zeroc.com>
2022-09-28 16:20:20 -0400

    Clap migration

--- tools/slicec-cs/src/cs_options.rs
+++ tools/slicec-cs/src/cs_options.rs
@@ -1,18 +1,18 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+use clap::Parser;
 use slice::command_line::SliceOptions;
-use structopt::StructOpt;
 
 // Note: StructOpt uses the doc-comments of fields to populate the '--help' output of slicec-cs.
 //       boolean flags automatically default to false, and strings automatically default to empty.
 
 /// This struct is responsible for parsing the command line options specific to 'slicec-cs'.
 /// The option parsing capabilities are generated on the struct by the `StructOpt` macro.
-#[derive(StructOpt, Debug)]
-#[structopt(name = "slicec-cs", version = "0.1.0", rename_all = "kebab-case", about = DESCRIPTION)]
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about=DESCRIPTION, rename_all = "kebab-case")]
 pub struct CsOptions {
     // Import the options common to all slice compilers.
-    #[structopt(flatten)]
+    #[clap(flatten)]
     pub slice_options: SliceOptions,
 }
 
