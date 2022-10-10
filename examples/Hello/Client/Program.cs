fb014b7d474cdc24959d938fa2a4195b88357ad1
Bernard Normier <bernard@zeroc.com>
2022-10-10 10:34:49 -0400

    Rename hello -> helloProxy

--- examples/Hello/Client/Program.cs
+++ examples/Hello/Client/Program.cs
@@ -5,7 +5,7 @@ using IceRpc;
 
 await using var connection = new ClientConnection(new Uri("icerpc://127.0.0.1"));
 
-var hello = new HelloProxy(connection);
-string greeting = await hello.SayHelloAsync(Environment.UserName);
+var helloProxy = new HelloProxy(connection);
+string greeting = await helloProxy.SayHelloAsync(Environment.UserName);
 
 Console.WriteLine(greeting);
