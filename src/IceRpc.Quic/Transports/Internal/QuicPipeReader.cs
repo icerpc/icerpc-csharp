41e7145967263acc356385f103efdb7d8d6eff71
Bernard Normier <bernard@zeroc.com>
2022-10-13 13:30:04 -0700

    Simplify QuicPipeReader

--- src/IceRpc.Quic/Transports/Internal/QuicPipeReader.cs
+++ src/IceRpc.Quic/Transports/Internal/QuicPipeReader.cs
@@ -18,11 +18,11 @@ internal class QuicPipeReader : PipeReader
     private readonly Action _completedCallback;
     private readonly IMultiplexedStreamErrorCodeConverter _errorCodeConverter;
     private Exception? _exception;
+    private volatile bool _isCompleted;
     private readonly Pipe _pipe;
-    private int _state;
     private readonly QuicStream _stream;
 
-    public bool IsCompleted => _state.HasFlag(State.Completed);
+    public bool IsCompleted => _isCompleted;
 
     public override void AdvanceTo(SequencePosition consumed) =>
         _pipe.Reader.AdvanceTo(consumed);
@@ -34,20 +34,18 @@ internal class QuicPipeReader : PipeReader
 
     public override void Complete(Exception? exception = null)
     {
-        if (_state.TrySetFlag(State.Completed))
-        {
-            // Abort the read side of the stream with the error code corresponding to the exception.
-            _stream.Abort(QuicAbortDirection.Read, (long)_errorCodeConverter.ToErrorCode(exception));
+        _pipe.Reader.Complete(exception);
 
-            _pipe.Reader.Complete();
-            Abort(exception);
+        // TODO: it appears we need to call Abort even when exception is null, which does not make sense.
+        Abort(exception);
 
-            // Cleanup resources.
-            _abortCts.Dispose();
+        // Cleanup resources.
+        _abortCts.Dispose();
 
-            // Notify the stream of the reader completion.
-            _completedCallback();
-        }
+        // Notify the stream of the reader completion.
+        _completedCallback();
+
+        _isCompleted = true;
     }
 
     public override ValueTask<ReadResult> ReadAsync(CancellationToken cancellationToken = default) =>
@@ -85,31 +83,17 @@ internal class QuicPipeReader : PipeReader
                 {
                     while (true)
                     {
-                        // Make sure the pipe writer is not completed by Abort while it's being used.
-                        _ = _state.TrySetFlag(State.PipeWriterInUse);
-                        try
-                        {
-                            Memory<byte> buffer = _pipe.Writer.GetMemory();
+                        Memory<byte> buffer = _pipe.Writer.GetMemory();
 
-                            // We don't cancel QuicStream.ReadAsync since its cancellation aborts the stream reads under
-                            // the hood. See https://github.com/dotnet/runtime/issues/72607
-                            // TODO: add support for ValueTask.WaitAsync?
-                            int byteCount = await _stream.ReadAsync(buffer, CancellationToken.None).AsTask().WaitAsync(
-                                _abortCts.Token).ConfigureAwait(false);
-                            _pipe.Writer.Advance(byteCount);
-                            if (byteCount == 0)
-                            {
-                                _pipe.Writer.Complete();
-                                return;
-                            }
-                        }
-                        finally
+                        // We don't cancel QuicStream.ReadAsync since its cancellation aborts the stream reads under
+                        // the hood. See https://github.com/dotnet/runtime/issues/72607
+                        // TODO: add support for ValueTask.WaitAsync?
+                        int byteCount = await _stream.ReadAsync(buffer, CancellationToken.None).AsTask().WaitAsync(
+                            _abortCts.Token).ConfigureAwait(false);
+                        _pipe.Writer.Advance(byteCount);
+                        if (byteCount == 0)
                         {
-                            if (_state.HasFlag(State.PipeWriterCompleted))
-                            {
-                                _pipe.Writer.Complete(_exception);
-                            }
-                            _state.ClearFlag(State.PipeWriterInUse);
+                            return;
                         }
 
                         FlushResult flushResult = await _pipe.Writer.FlushAsync(_abortCts.Token).ConfigureAwait(false);
@@ -134,44 +118,36 @@ internal class QuicPipeReader : PipeReader
                 }
                 catch (OperationCanceledException)
                 {
-                    // Abort called.
+                    // Abort already called.
                 }
                 catch (ObjectDisposedException)
                 {
-                    // Stream disposed.
+                    // Stream disposed after completion of this QuicPipeReader.
                 }
                 catch (Exception exception)
                 {
                     Abort(new TransportException(TransportErrorCode.Unspecified, exception));
                 }
+                finally
+                {
+                    _pipe.Writer.Complete(_exception);
+                }
             });
     }
 
+    // Note that the exception has 2 separate purposes: transmit an error code to the peer _and_ throw this exception
+    // from the current or next ReadAsync.
     internal void Abort(Exception? exception)
     {
-        Interlocked.CompareExchange(ref _exception, exception, null);
-
-        if (_state.TrySetFlag(State.PipeWriterCompleted))
+        try
         {
+            _exception ??= exception;
+            _stream.Abort(QuicAbortDirection.Read, (long)_errorCodeConverter.ToErrorCode(exception));
             _abortCts.Cancel();
-            if (!_state.HasFlag(State.PipeWriterInUse))
-            {
-                _pipe.Writer.Complete(exception);
-            }
         }
-    }
-
-    /// <summary>The state enumeration is used to ensure the reader is not used after it's completed and to ensure that
-    /// the internal pipe writer isn't completed concurrently when it's being used by the read task.</summary>
-    private enum State : int
-    {
-        /// <summary><see cref="Complete" /> was called on this Slic pipe reader.</summary>
-        Completed = 1,
-
-        /// <summary>Data is being written to the internal pipe writer.</summary>
-        PipeWriterInUse = 2,
-
-        /// <summary>The internal pipe writer was completed by <see cref="Abort" />.</summary>
-        PipeWriterCompleted = 4
+        catch (ObjectDisposedException)
+        {
+            // ignored
+        }
     }
 }
