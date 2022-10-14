7ae663463e4b6d572bc76f4e2ac1f0e44602d7b9
Bernard Normier <bernard@zeroc.com>
2022-10-13 20:04:17 -0400

    Update doc-comment

--- src/IceRpc/Transports/IMultiplexedStream.cs
+++ src/IceRpc/Transports/IMultiplexedStream.cs
@@ -8,8 +8,8 @@ namespace IceRpc.Transports;
 public interface IMultiplexedStream : IDuplexPipe
 {
     /// <summary>Gets the stream ID.</summary>
-    /// <exception cref="InvalidOperationException">Raised if the stream is not started. Local streams are not
-    /// started until data is written. A remote stream is always started.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if the stream is not started. Local streams are not started
+    /// until data is written. A remote stream is always started.</exception>
     ulong Id { get; }
 
     /// <summary>Gets a value indicating whether the stream is bidirectional.</summary>
@@ -29,8 +29,15 @@ public interface IMultiplexedStream : IDuplexPipe
     /// <summary>Gets a task that completes when writes are closed.</summary>
     Task WritesClosed { get; }
 
-    /// <summary>Aborts the stream by completing the <see cref="IDuplexPipe.Input" /> and <see
-    /// cref="IDuplexPipe.Output" /> with the given exception.</summary>
-    /// <param name="exception">The completion exception.</param>
+    /// <summary>Aborts the stream. The implementation converts <paramref name="exception"/> into an error code using
+    /// the configured <see cref="IMultiplexedStreamErrorCodeConverter" /> and transmits this error code to the remote
+    /// peer. Outstanding and subsequent calls to <see cref="PipeReader.ReadAsync(CancellationToken)" />,
+    /// <see cref="PipeWriter.WriteAsync" /> and <see cref="PipeWriter.FlushAsync(CancellationToken)" /> on
+    /// <see cref="IDuplexPipe.Input" /> and <see cref="IDuplexPipe.Output" /> throw this exception as well.</summary>
+    /// <param name="exception">The abort exception.</param>
+    /// <remarks><see cref="Abort" /> can be called while other threads are reading or writing
+    /// <see cref="IDuplexPipe.Input" /> resp. <see cref="IDuplexPipe.Output" />. <see cref="Abort" /> can be called
+    /// multiple times, and even concurrently. Only the first call has an effect.</remarks>
+    /// <seealso cref="MultiplexedConnectionOptions" />
     void Abort(Exception exception);
 }
