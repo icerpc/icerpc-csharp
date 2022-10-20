115747686a5d4c9b987328e2152590cb52188a20
Jose <pepone@users.noreply.github.com>
2022-10-20 13:00:57 +0200

    Add HelloQuic example (#1936)

---
+++ examples/HelloQuic/Server/Program.cs
@@ -0,0 +1,42 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using IceRpc;
+using IceRpc.Transports;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Demo;
+
+[System.Runtime.Versioning.SupportedOSPlatform("macOS")]
+[System.Runtime.Versioning.SupportedOSPlatform("linux")]
+[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+public static class Program
+{
+    static async Task Main()
+    {
+        var hello = new Hello();
+        await using var server = new Server(
+            new ServerOptions
+            {
+                ServerAuthenticationOptions = new SslServerAuthenticationOptions
+                {
+                    ServerCertificate = new X509Certificate2("../../certs/server.p12", "password")
+                },
+                ConnectionOptions = new ConnectionOptions
+                {
+                    Dispatcher = hello
+                }
+            },
+            multiplexedServerTransport: new QuicServerTransport());
+
+        // Shuts down the server on Ctrl+C
+        Console.CancelKeyPress += (sender, eventArgs) =>
+        {
+            eventArgs.Cancel = true;
+            _ = server.ShutdownAsync();
+        };
+
+        server.Listen();
+        await server.ShutdownComplete;
+    }
+}
