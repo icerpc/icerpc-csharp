0fd33ff7d809dba5f68257345cd983b646090a69
Jose <jose@zeroc.com>
2022-10-21 17:09:04 +0200

    Add test to reject quic client connection

--- tests/IceRpc.Quic.Tests/Transports/QuicTransportTests.cs
+++ tests/IceRpc.Quic.Tests/Transports/QuicTransportTests.cs
@@ -1,5 +1,6 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+using IceRpc.Internal;
 using IceRpc.Transports;
 using IceRpc.Transports.Internal;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,7 @@ using NUnit.Framework;
 using System.Diagnostics;
 using System.Net.Quic;
 using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
 namespace IceRpc.Tests.Transports;
 
@@ -58,5 +60,54 @@ public class QuicTransportTests
                 provider.GetRequiredService<IListener<IMultiplexedConnection>>().ServerAddress,
                 provider.GetRequiredService<IOptions<MultiplexedConnectionOptions>>().Value,
                 provider.GetRequiredService<SslClientAuthenticationOptions>());
+    }
+
+    [Test]
+    public async Task Connection_connec_fails_when_server_rejects_the_certificate()
+    {
+        // Arrange
+        IServiceCollection services = new ServiceCollection().AddQuicTest();
+        var sslServerAuthenticationOptions = new SslServerAuthenticationOptions
+        {
+            ClientCertificateRequired = true,
+            ServerCertificate = new X509Certificate2("../../../certs/server.p12", "password"),
+            // Reject the client certificate
+            RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => false,
+        };
+        var clientAuthenticationOptions = new SslClientAuthenticationOptions
+        {
+            ClientCertificates = new X509CertificateCollection
+            {
+                new X509Certificate2("../../../certs/client.p12", "password")
+            },
+            // First connection is rejected, the following connections are accepted.
+            RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => true,
+        };
+        await using ServiceProvider provider = services.BuildServiceProvider(validateScopes: true);
+
+        var serverTransport = new QuicServerTransport();
+        var clientTransport = new QuicClientTransport();
+        var options = new MultiplexedConnectionOptions();
+        options.StreamErrorCodeConverter = IceRpcProtocol.Instance.MultiplexedStreamErrorCodeConverter;
+
+        var listener = serverTransport.Listen(
+            new ServerAddress(Protocol.IceRpc) { Host = "127.0.0.1", Port = 0 },
+            options,
+            sslServerAuthenticationOptions);
+        QuicMultiplexedConnection connection1 = CreateClientConnection();
+
+        // Act
+        var acceptTask = listener.AcceptAsync(default);
+
+        // Assert
+        Assert.That(
+            async () => await connection1.ConnectAsync(default),
+            Throws.TypeOf<TransportException>());
+
+        QuicMultiplexedConnection CreateClientConnection() =>
+            (QuicMultiplexedConnection)clientTransport.CreateConnection(
+                listener.ServerAddress,
+                options,
+                clientAuthenticationOptions);
     }
 }
