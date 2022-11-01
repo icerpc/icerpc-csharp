83f8859a5adc3efe0436198b14144407002c575f
Jose <jose@zeroc.com>
2022-11-01 21:57:33 +0100

    Cleanup

--- tests/IceRpc.Conformance.Tests/Transports/DuplexTransportSslAuthenticationConformanceTests.cs
+++ tests/IceRpc.Conformance.Tests/Transports/DuplexTransportSslAuthenticationConformanceTests.cs
@@ -14,6 +14,47 @@ public abstract class DuplexTransportSslAuthenticationConformanceTests
 {
     /// <summary>Verifies that the server connection establishment will fail with <see cref="AuthenticationException" />
     /// when the client certificate is not trusted.</summary>
+    [Test]
+    public async Task Ssl_client_connection_connect_fails_when_server_provides_untrusted_certificate()
+    {
+        // Arrange
+        await using ServiceProvider provider = CreateServiceCollection()
+            .AddSingleton(
+                new SslServerAuthenticationOptions
+                {
+                    ServerCertificate = new X509Certificate2("../../../certs/server.p12", "password"),
+                })
+            .AddSingleton(
+                new SslClientAuthenticationOptions
+                {
+                    RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => false
+                })
+            .BuildServiceProvider(validateScopes: true);
+
+        var listener = provider.GetRequiredService<IListener<IDuplexConnection>>();
+
+        var clientConnection = provider.GetRequiredService<IDuplexConnection>();
+
+        // Start the TLS handshake by calling connect on the client and server connections and wait for the
+        // connection establishment.
+        var clientConnectTask = clientConnection.ConnectAsync(default);
+        (IDuplexConnection serverConnection, _) = await listener.AcceptAsync(default);
+        var serverConnectTask = serverConnection.ConnectAsync(default);
+        byte[] buffer = new byte[1];
+
+        // Act/Assert
+        Assert.That(async () => await clientConnectTask, Throws.TypeOf<AuthenticationException>());
+        Assert.That(
+            async () =>
+            {
+                await serverConnectTask;
+                await serverConnection.ReadAsync(new byte[1], CancellationToken.None);
+            },
+            Throws.TypeOf<TransportException>());
+        }
+
+    /// <summary>Verifies that the server connection establishment will fail with <see cref="AuthenticationException" />
+    /// when the client certificate is not trusted.</summary>
     [System.Diagnostics.CodeAnalysis.SuppressMessage(
         "Security",
         "CA5359:Do Not Disable Certificate Validation",
@@ -61,41 +102,6 @@ public abstract class DuplexTransportSslAuthenticationConformanceTests
                 await clientConnection.ReadAsync(new byte[1], CancellationToken.None);
             },
             Throws.TypeOf<TransportException>());
-    }
-
-    /// <summary>Verifies that the server connection establishment will fail with <see cref="AuthenticationException" />
-    /// when the client certificate is not trusted.</summary>
-    [Test]
-    public async Task Ssl_client_connection_connect_fails_when_server_provides_untrusted_certificate()
-    {
-        // Arrange
-        await using ServiceProvider provider = CreateServiceCollection()
-            .AddSingleton(
-                new SslServerAuthenticationOptions
-                {
-                    ServerCertificate = new X509Certificate2("../../../certs/server.p12", "password"),
-                })
-            .AddSingleton(
-                new SslClientAuthenticationOptions
-                {
-                    RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => false
-                })
-            .BuildServiceProvider(validateScopes: true);
-
-        var listener = provider.GetRequiredService<IListener<IDuplexConnection>>();
-
-        var clientConnection = provider.GetRequiredService<IDuplexConnection>();
-
-        // Start the TLS handshake by calling connect on the client and server connections and wait for the
-        // connection establishment.
-        var clientConnectTask = clientConnection.ConnectAsync(default);
-        (IDuplexConnection serverConnection, _) = await listener.AcceptAsync(default);
-        var serverConnectTask = serverConnection.ConnectAsync(default);
-        byte[] buffer = new byte[1];
-
-        // Act/Assert
-        Assert.That(async () => await clientConnectTask, Throws.TypeOf<AuthenticationException>());
-        Assert.That(async () => await serverConnectTask, Throws.Nothing);
     }
 
     /// <summary>Creates the service collection used for the duplex transport conformance tests.</summary>
