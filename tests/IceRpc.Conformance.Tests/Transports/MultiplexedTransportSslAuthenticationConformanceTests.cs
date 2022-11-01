2a4577d4e110232e1080e13041f6b04710a026f6
Jose <jose@zeroc.com>
2022-11-01 22:02:58 +0100

    Ssl test fixes

--- tests/IceRpc.Conformance.Tests/Transports/MultiplexedTransportSslAuthenticationConformanceTests.cs
+++ tests/IceRpc.Conformance.Tests/Transports/MultiplexedTransportSslAuthenticationConformanceTests.cs
@@ -12,6 +12,41 @@ namespace IceRpc.Conformance.Tests;
 /// <summary>Conformance tests for the multiplexed transports SslAuthentication.</summary>
 public abstract class MultiplexedTransportSslAuthenticationConformanceTests
 {
+
+    /// <summary>Verifies that the server connection establishment will fail with <see cref="AuthenticationException" />
+    /// when the client certificate is not trusted.</summary>
+    [Test]
+    public async Task Ssl_client_connection_connect_fails_when_server_provides_untrusted_certificate()
+    {
+        // Arrange
+        await using ServiceProvider provider = CreateServiceCollection()
+            .AddSingleton(
+                new SslServerAuthenticationOptions
+                {
+                    ServerCertificate = new X509Certificate2("../../../certs/server.p12", "password"),
+                })
+            .AddSingleton(
+                new SslClientAuthenticationOptions
+                {
+                    RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => false
+                })
+            .BuildServiceProvider(validateScopes: true);
+
+        var listener = provider.GetRequiredService<IListener<IMultiplexedConnection>>();
+
+        var clientConnection = provider.GetRequiredService<IMultiplexedConnection>();
+
+        // Start the TLS handshake by calling connect on the client and server connections and wait for the
+        // connection establishment.
+        var clientConnectTask = clientConnection.ConnectAsync(default);
+        (IMultiplexedConnection serverConnection, _) = await listener.AcceptAsync(default);
+        var serverConnectTask = serverConnection.ConnectAsync(default);
+
+        // Act/Assert
+        Assert.That(async () => await clientConnectTask, Throws.TypeOf<AuthenticationException>());
+        Assert.That(async () => await serverConnectTask, Throws.TypeOf<TransportException>());
+    }
+
     /// <summary>Verifies that the server connection establishment will fail with <see cref="AuthenticationException" />
     /// when the client certificate is not trusted.</summary>
     [System.Diagnostics.CodeAnalysis.SuppressMessage(
@@ -47,47 +82,16 @@ public abstract class MultiplexedTransportSslAuthenticationConformanceTests
 
         // Start the TLS handshake by calling connect on the client and server connections and wait for the
         // connection establishment.
-        _ = clientConnection.ConnectAsync(default);
+        var clientConnectTask = clientConnection.ConnectAsync(default);
         await using IMultiplexedConnection serverConnection = (await listener.AcceptAsync(default)).Connection;
 
         // Act/Assert
         Assert.That(
             async () => await serverConnection.ConnectAsync(default),
             Throws.TypeOf<AuthenticationException>());
-    }
-
-    /// <summary>Verifies that the server connection establishment will fail with <see cref="AuthenticationException" />
-    /// when the client certificate is not trusted.</summary>
-    [Test]
-    public async Task Ssl_client_connection_connect_fails_when_server_provides_untrusted_certificate()
-    {
-        // Arrange
-        await using ServiceProvider provider = CreateServiceCollection()
-            .AddSingleton(
-                new SslServerAuthenticationOptions
-                {
-                    ServerCertificate = new X509Certificate2("../../../certs/server.p12", "password"),
-                })
-            .AddSingleton(
-                new SslClientAuthenticationOptions
-                {
-                    RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => false
-                })
-            .BuildServiceProvider(validateScopes: true);
-
-        var listener = provider.GetRequiredService<IListener<IMultiplexedConnection>>();
-
-        var clientConnection = provider.GetRequiredService<IMultiplexedConnection>();
-
-        // Start the TLS handshake by calling connect on the client and server connections and wait for the
-        // connection establishment.
-        var clientConnectTask = clientConnection.ConnectAsync(default);
-        (IMultiplexedConnection serverConnection, _) = await listener.AcceptAsync(default);
-        var serverConnectTask = serverConnection.ConnectAsync(default);
-
-        // Act/Assert
-        Assert.That(async () => await clientConnectTask, Throws.TypeOf<AuthenticationException>());
-        Assert.That(async () => await serverConnectTask, Throws.Nothing);
+        Assert.That(
+            async () => await clientConnectTask,
+            Throws.TypeOf<TransportException>());
     }
 
     /// <summary>Creates the service collection used for the duplex transport conformance tests.</summary>
