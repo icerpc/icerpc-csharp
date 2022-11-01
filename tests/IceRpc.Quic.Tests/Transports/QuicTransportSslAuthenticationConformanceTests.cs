74787d089bfa2517be184c3f5df58f940aed07bc
Jose <jose@zeroc.com>
2022-11-01 20:06:26 +0100

    Fix Quic test build failure

--- tests/IceRpc.Quic.Tests/Transports/QuicTransportSslAuthenticationConformanceTests.cs
+++ tests/IceRpc.Quic.Tests/Transports/QuicTransportSslAuthenticationConformanceTests.cs
@@ -7,43 +7,30 @@ using IceRpc.Transports;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using NUnit.Framework;
-using System;
-using System.Diagnostics;
 
 namespace IceRpc.Tests.Transports;
 
 /// <summary>Conformance tests for the Ssl duplex transport.</summary>
-[System.Runtime.Versioning.SupportedOSPlatform("macOS")]
-[System.Runtime.Versioning.SupportedOSPlatform("linux")]
-[System.Runtime.Versioning.SupportedOSPlatform("windows")]
 [Parallelizable(ParallelScope.All)]
 public class QuicTransportSslAuthenticationConformanceTests : MultiplexedTransportSslAuthenticationConformanceTests
 {
-    [OneTimeSetUp]
-    public void FixtureSetUp()
-    {
-        if (!QuicConnection.IsSupported)
-        {
-            Assert.Ignore("Quic is not supported on this platform");
-        }
-        Trace.Listeners.Add(new ConsoleTraceListener());
-    }
-
     protected override IServiceCollection CreateServiceCollection()
     {
         var services = new ServiceCollection()
-            .AddSslAuthenticationOptions()
             .AddMultiplexedTransportClientServerTest(new Uri("icerpc://127.0.0.1:0/"))
-            .AddSingleton(provider =>
-            .AddSingleton<IMultiplexedServerTransport>(provider =>
-                new QuicServerTransport(
-                    provider.GetRequiredService<IOptionsMonitor<QuicServerTransportOptions>>().Get("server")))
-            .AddSingleton<IMultiplexedClientTransport>(provider =>
-                new QuicClientTransport(
-                    provider.GetRequiredService<IOptionsMonitor<QuicClientTransportOptions>>().Get("client")));
+            .AddSingleton<IDuplexServerTransport>(provider => new TcpServerTransport(new TcpServerTransportOptions()))
+            .AddSingleton<IDuplexClientTransport>(provider => new TcpClientTransport())
+            .AddSingleton<IMultiplexedServerTransport>(
+                    provider => new SlicServerTransport(
+                        provider.GetRequiredService<IOptionsMonitor<SlicTransportOptions>>().Get("server"),
+                        provider.GetRequiredService<IDuplexServerTransport>()))
+            .AddSingleton<IMultiplexedClientTransport>(
+                provider => new SlicClientTransport(
+                    provider.GetRequiredService<IOptionsMonitor<SlicTransportOptions>>().Get("client"),
+                    provider.GetRequiredService<IDuplexClientTransport>()));
 
-        services.AddOptions<QuicServerTransportOptions>("client");
-        services.AddOptions<QuicClientTransportOptions>("server");
+        services.AddOptions<SlicTransportOptions>("client");
+        services.AddOptions<SlicTransportOptions>("server");
         services.AddOptions<MultiplexedConnectionOptions>().Configure(
             options => options.StreamErrorCodeConverter = IceRpcProtocol.Instance.MultiplexedStreamErrorCodeConverter);
 
