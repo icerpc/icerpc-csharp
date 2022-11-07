94f414e9b9a5331b40fcc0ebc146374bb3b303ee
Benoit Foucher <benoit@zeroc.com>
2022-11-07 17:48:12 +0100

    Support for failing the test on `Debug.Assert` (#2054)

---
+++ tests/IceRpc.Tests.Common/AssertTraceListener.cs
@@ -0,0 +1,27 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace IceRpc.Tests.Common;
+
+public class AssertTraceListener : DefaultTraceListener
+{
+    private static readonly AssertTraceListener _instance = new();
+
+    public override void Fail(string? message) => Assert.Fail(message);
+
+    public override void Fail(string? message, string? detailMessage)
+    {
+        if (detailMessage is null || detailMessage.Length == 0)
+        {
+            Assert.Fail(message);
+        }
+        else
+        {
+            Assert.Fail(message, detailMessage);
+        }
+    }
+
+    public static void Setup() => Trace.Listeners[0] = _instance;
+}
