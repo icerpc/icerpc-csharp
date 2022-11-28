a1fbf995b87c5265a96330ad1b3dc84041a72e9a
Jose <jose@zeroc.com>
2022-11-28 17:37:20 +0100

    tag format definition - Fix #2163

---
+++ src/IceRpc/Slice/TagFormat.cs
@@ -0,0 +1,36 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+namespace IceRpc.Slice;
+
+/// <summary>With Slice1, each tagged parameter has a specific tag format. This tag format describes how the data is
+/// encoded and how it can be skipped by the decoding code if the tagged parameter is present in the buffer but is
+/// not known to the receiver.</summary>
+public enum TagFormat : int
+{
+    /// <summary>A fixed size numeric encoded on 1 byte such as bool or uint8.</summary>
+    F1 = 0,
+
+    /// <summary>A fixed size numeric encoded on 2 bytes such as int16.</summary>
+    F2 = 1,
+
+    /// <summary>A fixed size numeric encoded on 4 bytes such as int32 or float32.</summary>
+    F4 = 2,
+
+    /// <summary>A fixed size numeric encoded on 8 bytes such as int64 or float64.</summary>
+    F8 = 3,
+
+    /// <summary>A variable-length size encoded on 1 or 5 bytes.</summary>
+    Size = 4,
+
+    /// <summary>A variable-length size followed by size bytes.</summary>
+    VSize = 5,
+
+    /// <summary>A fixed length size (encoded on 4 bytes) followed by size bytes.</summary>
+    FSize = 6,
+
+    /// <summary>Represents a class, but is no longer encoded or decoded.</summary>
+    Class = 7,
+
+    /// <summary>Pseudo non-encoded format: like VSize but the size is optimized out.</summary>
+    OptimizedVSize = 8,
+}
