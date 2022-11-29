4f248ead88c135a93b20d175badcc26e1351eb5b
Bernard Normier <bernard@zeroc.com>
2022-11-29 09:57:18 -0500

    Reduce use of type IDs (#2173)

--- tools/slicec-cs/src/visitors/exception_visitor.rs
+++ tools/slicec-cs/src/visitors/exception_visitor.rs
@@ -36,8 +36,11 @@ impl Visitor for ExceptionVisitor<'_> {
 
         exception_class_builder
             .add_comment("summary", doc_comment_message(exception_def))
-            .add_type_id_attribute(exception_def)
             .add_container_attributes(exception_def);
+
+        if exception_def.supported_encodings().supports(&Encoding::Slice1) {
+            exception_class_builder.add_type_id_attribute(exception_def);
+        }
 
         if let Some(base) = exception_def.base_exception() {
             exception_class_builder.add_base(base.escape_scoped_identifier(namespace));
