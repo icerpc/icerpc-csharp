d8f1dfb47b7114bf25023bb3a1942d6640406e02
Jose <pepone@users.noreply.github.com>
2022-12-07 21:13:43 +0100

    Update src/IceRpc/OutgoingFrame.cs
    Co-authored-by: Reece Humphreys <reecewh@icloud.com>

--- src/IceRpc/OutgoingFrame.cs
+++ src/IceRpc/OutgoingFrame.cs
@@ -22,7 +22,7 @@ public abstract class OutgoingFrame
         {
             _payloadContinuation = Protocol.SupportsPayloadContinuation || value is null ?
                 value : throw new NotSupportedException(
-                    $"The {Protocol}' protocol does not support payload continuation'.");
+                    $"The '{Protocol}' protocol does not support payload continuation'.");
         }
     }
 
