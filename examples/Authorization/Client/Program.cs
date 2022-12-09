9453b702a238ff4a9b6e4a321b02ca3e370a127b
Joe George <joe@zeroc.com>
2022-12-09 14:40:07 -0500

    Update some comments

--- examples/Authorization/Client/Program.cs
+++ examples/Authorization/Client/Program.cs
@@ -5,10 +5,10 @@ using IceRpc;
 
 await using var connection = new ClientConnection(new Uri("icerpc://127.0.0.1"));
 
-// A `Hello` proxy that doesn't use any authentication
+// A `Hello` proxy that doesn't use any authentication. An authentication token is not needed to call `SayHello`.
 var unauthenticatedHelloProxy = new HelloProxy(connection, new Uri("icerpc:/hello"));
 
-// Unauthenticated hello; prints generic greeting
+// Unauthenticated hello; prints generic greeting.
 Console.WriteLine(await unauthenticatedHelloProxy.SayHelloAsync());
 
 // A `Session` proxy that doesn't use any authentication. An authentication token is not need to login.
@@ -17,7 +17,7 @@ var sessionProxy = new SessionProxy(connection, new Uri("icerpc:/session"));
 // Get an authentication token. The token is used to authenticate future requests.
 byte[] token = await sessionProxy.LoginAsync("friend");
 
-// Add an interceptor to the invocation pipeline that inserts the token into a request field
+// Add an interceptor to the invocation pipeline that inserts the token into a request field.
 Pipeline authenticatedPipeline = new Pipeline()
     .Use(next => new SessionInterceptor(next, token))
     .Into(connection);
@@ -29,11 +29,11 @@ var helloProxy = new HelloProxy(authenticatedPipeline, new Uri("icerpc:/hello"))
 // A `HelloAdmin`that uses the authentication pipeline. An authentication token is needed to change the greeting.
 var helloAdminProxy = new HelloAdminProxy(authenticatedPipeline, new Uri("icerpc:/helloAdmin/"));
 
-// Authenticated hello
+// Authenticated hello.
 Console.WriteLine(await helloProxy.SayHelloAsync());
 
-// Change the greeting using the authentication token
+// Change the greeting using the authentication token.
 await helloAdminProxy.ChangeGreetingAsync("Bonjour");
 
-// Authenticated hello with updated greeting
+// Authenticated hello with updated greeting.
 Console.WriteLine(await helloProxy.SayHelloAsync());
