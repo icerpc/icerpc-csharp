088a23c54550c363cb184d55ee08525959e80edb
Joe George <joe@externl.com>
2022-12-09 11:17:02 -0500

    Update examples/Authorization/Client/Session.cs
    Co-authored-by: Jose  <pepone@users.noreply.github.com>

--- examples/Authorization/Client/Session.cs
+++ examples/Authorization/Client/Session.cs
@@ -13,13 +13,13 @@ public class SessionData
     public byte[]? Token { get; set; }
 
     public Func<IInvoker, IInvoker> Interceptor =>
-        next => new InlineInvoker(async (request, cancellationToken) =>
+        next => new InlineInvoker((request, cancellationToken) =>
         {
             if (Token is not null)
             {
                 request.Fields = request.Fields.With(
                     (RequestFieldKey)100, new ReadOnlySequence<byte>(Token));
             }
-            return await next.InvokeAsync(request, cancellationToken);
+            return next.InvokeAsync(request, cancellationToken);
         });
 }
