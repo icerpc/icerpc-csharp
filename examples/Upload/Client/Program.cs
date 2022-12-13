8e42f54063d30ab51e6e0d7dda22cec13eb124e7
Jose <pepone@users.noreply.github.com>
2022-12-13 16:45:45 +0100

    Update examples/Upload/Client/Program.cs
    Co-authored-by: Reece Humphreys <reecewh@icloud.com>

--- examples/Upload/Client/Program.cs
+++ examples/Upload/Client/Program.cs
@@ -10,7 +10,7 @@ var uploader = new UploaderProxy(connection);
 
 Console.WriteLine("Uploading image of the Earth...");
 
-// Create a pipe reader wrapping the image we want to upload, the pipe reader takes owner ship of the
+// Create a pipe reader that wraps the image we want to upload. The pipe reader takes ownership of the
 // file stream and disposes it once it is completed by the IceRPC runtime.
 var reader = PipeReader.Create(new FileStream("Client/images/Earth.png", FileMode.Open));
 
