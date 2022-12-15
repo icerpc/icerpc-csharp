b38536f91d78952acc435293ca0ffaaf2df37884
Jose <jose@zeroc.com>
2022-12-15 09:38:43 +0100

    Fix review comments

--- examples/Authorization/Client/SessionInterceptor.cs
+++ examples/Authorization/Client/SessionInterceptor.cs
@@ -11,17 +11,15 @@ internal class SessionInterceptor : IInvoker
     private readonly IInvoker _next;
     private readonly Guid _token;
 
+    public Task<IncomingResponse> InvokeAsync(OutgoingRequest request, CancellationToken cancellationToken)
+    {
+        request.Fields = request.Fields.With(SessionFieldKey.Value, new ReadOnlySequence<byte>(_token.ToByteArray()));
+        return _next.InvokeAsync(request, cancellationToken);
+    }
+
     internal SessionInterceptor(IInvoker next, Guid token)
     {
         _next = next;
         _token = token;
-    }
-
-    public Task<IncomingResponse> InvokeAsync(
-        OutgoingRequest request,
-        CancellationToken cancellationToken)
-    {
-        request.Fields = request.Fields.With(SessionFieldKey.Value, new ReadOnlySequence<byte>(_token.ToByteArray()));
-        return _next.InvokeAsync(request, cancellationToken);
     }
 }
