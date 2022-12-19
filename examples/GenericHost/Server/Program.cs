cb4825b41bd57d91dc6e230b7b20ddcd7c94d3b8
Bernard Normier <bernard@zeroc.com>
2022-12-19 11:00:54 -0500

    Move host service classes to their own files

--- examples/GenericHost/Server/Program.cs
+++ examples/GenericHost/Server/Program.cs
@@ -52,29 +52,3 @@ IHost host = Host.CreateDefaultBuilder(args)
 
 // Run hosted program.
 host.Run();
-
-/// <summary>The server hosted service is ran and managed by the .NET Generic Host</summary>
-public class ServerHostedService : IHostedService, IAsyncDisposable
-{
-    // The IceRPC server accepts connections from IceRPC clients.
-    private readonly Server _server;
-
-    public ServerHostedService(Server server) => _server = server;
-
-    public ValueTask DisposeAsync()
-    {
-        GC.SuppressFinalize(this);
-        return _server.DisposeAsync();
-    }
-
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        // Start listening for client connections.
-        _server.Listen();
-        return Task.CompletedTask;
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken) =>
-        // Shuts down the IceRPC server when the hosted service is stopped.
-        _server.ShutdownAsync(cancellationToken);
-}
