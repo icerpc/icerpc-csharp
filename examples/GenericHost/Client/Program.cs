d01f11d3066d2fe3b165d9e77c8588a6b729ca56
Bernard Normier <bernard@zeroc.com>
2022-12-19 15:11:54 -0500

    Simplify GenericHost example (#2337)

--- examples/GenericHost/Client/Program.cs
+++ examples/GenericHost/Client/Program.cs
@@ -1,5 +1,6 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+using GenericHostExample;
 using IceRpc;
 using IceRpc.Builder;
 using Microsoft.Extensions.Configuration;
@@ -9,94 +10,51 @@ using System.Diagnostics;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-namespace GenericHostExample;
+// Build generic host.
+IHost host = Host.CreateDefaultBuilder(args)
+    // Set the content root path to the build directory of the client (e.g.: Client/bin/Debug/net7.0)
+    .UseContentRoot(AppContext.BaseDirectory)
 
-public static class Program
-{
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+    // Configures the .NET Generic Host services.
+    .ConfigureServices((hostContext, services) =>
+    {
+        // Add the ClientHostedService to the hosted services of the .NET Generic Host.
+        services.AddHostedService<ClientHostedService>();
 
-    /// <summary>Creates the HostBuilder used to build the .NET Generic Host.</summary>
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            // Set the content root path to the build directory of the server (e.g.: Client/bin/Debug/net7.0)
-            .UseContentRoot(AppContext.BaseDirectory)
-
-            // Configures the .NET Generic Host services.
-            .ConfigureServices((hostContext, services) =>
+        // Bind the client connection options to the "appsettings.json" configuration "Client" section,
+        // and add a Configure callback to configure its authentication options.
+        services
+            .AddOptions<ClientConnectionOptions>()
+            .Bind(hostContext.Configuration.GetSection("Client"))
+            .Configure(options =>
             {
-                // Add the ClientHostedService to the hosted services of the .NET Generic Host.
-                services.AddHostedService<ClientHostedService>();
+                // Configure the authentication options
+                var rootCA = new X509Certificate2(
+                    hostContext.Configuration.GetValue<string>("CertificateAuthoritiesFile")!);
 
-                // Bind the client connection options to the "appsettings.json" configuration "Client" section,
-                // and add a Configure callback to configure its authentication options.
-                services
-                    .AddOptions<ClientConnectionOptions>()
-                    .Bind(hostContext.Configuration.GetSection("Client"))
-                    .Configure(options =>
+                options.ClientAuthenticationOptions = new SslClientAuthenticationOptions
+                {
+                    // A certificate validation callback that uses the configured certificate authorities file
+                    // to validate the peer certificates.
+                    RemoteCertificateValidationCallback = (sender, certificate, chain, errors) =>
                     {
-                        // Configure the authentication options
-                        var rootCA = new X509Certificate2(
-                            hostContext.Configuration.GetValue<string>("CertificateAuthoritiesFile")!);
-
-                        options.ClientAuthenticationOptions = new SslClientAuthenticationOptions
-                        {
-                            // A certificate validation callback that uses the configured certificate authorities file
-                            // to validate the peer certificates.
-                            RemoteCertificateValidationCallback = (sender, certificate, chain, errors) =>
-                            {
-                                using var customChain = new X509Chain();
-                                customChain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
-                                customChain.ChainPolicy.DisableCertificateDownloads = true;
-                                customChain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
-                                customChain.ChainPolicy.CustomTrustStore.Add(rootCA);
-                                return customChain.Build((X509Certificate2)certificate!);
-                            }
-                        };
-                    });
-
-                services
-                    .AddSingleton(_ => new ActivitySource("IceRpc"))
-                    .AddIceRpcClientConnection()
-                    .AddIceRpcInvoker(builder => builder.UseTelemetry().UseLogger().Into<ClientConnection>())
-                    .AddIceRpcProxy<IHelloProxy, HelloProxy>();
+                        using var customChain = new X509Chain();
+                        customChain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+                        customChain.ChainPolicy.DisableCertificateDownloads = true;
+                        customChain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+                        customChain.ChainPolicy.CustomTrustStore.Add(rootCA);
+                        return customChain.Build((X509Certificate2)certificate!);
+                    }
+                };
             });
 
-    /// <summary>The hosted client service is ran and managed by the .NET Generic Host.</summary>
-    public class ClientHostedService : BackgroundService
-    {
-        // The host application lifetime is used to stop the .NET Generic Host.
-        private readonly IHostApplicationLifetime _applicationLifetime;
+        services
+            .AddSingleton(_ => new ActivitySource("IceRpc"))
+            .AddIceRpcClientConnection()
+            .AddIceRpcInvoker(builder => builder.UseTelemetry().UseLogger().Into<ClientConnection>())
+            .AddIceRpcProxy<IHelloProxy, HelloProxy>();
+    })
+    .Build();
 
-        private readonly ClientConnection _connection;
-
-        // A proxy to the remote Hello service.
-        private readonly IHelloProxy _hello;
-
-        public ClientHostedService(
-            IHelloProxy hello,
-            ClientConnection connection,
-            IHostApplicationLifetime applicationLifetime)
-        {
-            _applicationLifetime = applicationLifetime;
-            _connection = connection;
-            _hello = hello;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            try
-            {
-                string greeting = await _hello.SayHelloAsync(Environment.UserName, cancellationToken: stoppingToken);
-                Console.WriteLine(greeting);
-                await _connection.ShutdownAsync(stoppingToken);
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine($"failed to connect to the server:\n{exception}");
-            }
-
-            // Stop the generic host once the invocation is done.
-            _applicationLifetime.StopApplication();
-        }
-    }
-}
+// Run hosted program.
+host.Run();
