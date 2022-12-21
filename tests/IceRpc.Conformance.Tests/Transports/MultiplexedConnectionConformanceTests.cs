be27d4da88f3aba6e79fb2623e105b31d9e318db
Bernard Normier <bernard@zeroc.com>
2022-12-21 16:18:09 -0500

    Add test

--- tests/IceRpc.Conformance.Tests/Transports/MultiplexedConnectionConformanceTests.cs
+++ tests/IceRpc.Conformance.Tests/Transports/MultiplexedConnectionConformanceTests.cs
@@ -491,6 +491,48 @@ public abstract class MultiplexedConnectionConformanceTests
     }
 
     [Test]
+    public async Task Disposing_the_server_connection_aborts_the_client_connection()
+    {
+        // Arrange
+        await using ServiceProvider provider = CreateServiceCollection()
+            .AddMultiplexedTransportTest()
+            .BuildServiceProvider(validateScopes: true);
+        var clientConnection = provider.GetRequiredService<IMultiplexedConnection>();
+        var listener = provider.GetRequiredService<IListener<IMultiplexedConnection>>();
+        await using IMultiplexedConnection serverConnection =
+            await MultiplexedConformanceTestsHelper.ConnectAndAcceptConnectionAsync(listener, clientConnection);
+
+        // Act
+        await serverConnection.DisposeAsync();
+
+        // Assert
+        Assert.That(
+            async () => _ = await clientConnection.AcceptStreamAsync(default),
+            Throws.InstanceOf<IceRpcException>().With.Property("IceRpcError").EqualTo(IceRpcError.ConnectionAborted));
+    }
+
+    [Test]
+    public async Task Disposing_the_client_connection_aborts_the_server_connection()
+    {
+        // Arrange
+        await using ServiceProvider provider = CreateServiceCollection()
+            .AddMultiplexedTransportTest()
+            .BuildServiceProvider(validateScopes: true);
+        var clientConnection = provider.GetRequiredService<IMultiplexedConnection>();
+        var listener = provider.GetRequiredService<IListener<IMultiplexedConnection>>();
+        await using IMultiplexedConnection serverConnection =
+            await MultiplexedConformanceTestsHelper.ConnectAndAcceptConnectionAsync(listener, clientConnection);
+
+        // Act
+        await clientConnection.DisposeAsync();
+
+        // Assert
+        Assert.That(
+            async () => _ = await serverConnection.AcceptStreamAsync(default),
+            Throws.InstanceOf<IceRpcException>().With.Property("IceRpcError").EqualTo(IceRpcError.ConnectionAborted));
+    }
+
+    [Test]
     public async Task Disposing_the_connection_closes_the_streams()
     {
         // Arrange
