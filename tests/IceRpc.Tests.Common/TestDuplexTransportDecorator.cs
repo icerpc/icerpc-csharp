474568d5b95f97d1aaf162c073dffd7c4f9c171e
Benoit Foucher <benoit@zeroc.com>
2023-01-06 17:58:42 +0100

    More review fixes

--- tests/IceRpc.Tests.Common/TestDuplexTransportDecorator.cs
+++ tests/IceRpc.Tests.Common/TestDuplexTransportDecorator.cs
@@ -122,133 +122,133 @@ public class TestDuplexServerTransportDecorator : IDuplexServerTransport
 
         internal TestDuplexListenerDecorator(IListener<IDuplexConnection> decoratee) => _decoratee = decoratee;
     }
+}
 
-    public sealed class TestDuplexConnectionDecorator : IDuplexConnection
+public sealed class TestDuplexConnectionDecorator : IDuplexConnection
+{
+    public ServerAddress ServerAddress => _decoratee.ServerAddress;
+
+    public Task DisposeCalled => _disposeCalledTcs.Task;
+
+    public DuplexTransportOperation FailOperation { get; set; }
+
+    public DuplexTransportOperation HoldOperation
     {
-        public ServerAddress ServerAddress => _decoratee.ServerAddress;
-
-        public Task DisposeCalled => _disposeCalledTcs.Task;
-
-        public DuplexTransportOperation FailOperation { get; set; }
-
-        public DuplexTransportOperation HoldOperation
+        get => _holdOperation;
+        set
         {
-            get => _holdOperation;
-            set
+            _holdOperation = value;
+
+            if (_holdOperation.HasFlag(DuplexTransportOperation.Connect))
             {
-                _holdOperation = value;
+                _holdConnectTcs = new();
+            }
+            else
+            {
+                _holdConnectTcs.TrySetResult();
+            }
 
-                if (_holdOperation.HasFlag(DuplexTransportOperation.Connect))
-                {
-                    _holdConnectTcs = new();
-                }
-                else
-                {
-                    _holdConnectTcs.TrySetResult();
-                }
+            if (_holdOperation.HasFlag(DuplexTransportOperation.Shutdown))
+            {
+                _holdShutdownTcs = new();
+            }
+            else
+            {
+                _holdShutdownTcs.TrySetResult();
+            }
 
-                if (_holdOperation.HasFlag(DuplexTransportOperation.Shutdown))
-                {
-                    _holdShutdownTcs = new();
-                }
-                else
-                {
-                    _holdShutdownTcs.TrySetResult();
-                }
+            if (_holdOperation.HasFlag(DuplexTransportOperation.Read))
+            {
+                _holdReadTcs = new();
+            }
+            else
+            {
+                _holdReadTcs.TrySetResult();
+            }
 
-                if (_holdOperation.HasFlag(DuplexTransportOperation.Read))
-                {
-                    _holdReadTcs = new();
-                }
-                else
-                {
-                    _holdReadTcs.TrySetResult();
-                }
-
-                if (_holdOperation.HasFlag(DuplexTransportOperation.Write))
-                {
-                    _holdWriteTcs = new();
-                }
-                else
-                {
-                    _holdWriteTcs.TrySetResult();
-                }
+            if (_holdOperation.HasFlag(DuplexTransportOperation.Write))
+            {
+                _holdWriteTcs = new();
+            }
+            else
+            {
+                _holdWriteTcs.TrySetResult();
             }
         }
+    }
 
-        private readonly IDuplexConnection _decoratee;
-        private readonly TaskCompletionSource _disposeCalledTcs = new();
-        private TaskCompletionSource _holdConnectTcs = new();
-        private DuplexTransportOperation _holdOperation;
-        private TaskCompletionSource _holdReadTcs = new();
-        private TaskCompletionSource _holdShutdownTcs = new();
-        private TaskCompletionSource _holdWriteTcs = new();
+    private readonly IDuplexConnection _decoratee;
+    private readonly TaskCompletionSource _disposeCalledTcs = new();
+    private TaskCompletionSource _holdConnectTcs = new();
+    private DuplexTransportOperation _holdOperation;
+    private TaskCompletionSource _holdReadTcs = new();
+    private TaskCompletionSource _holdShutdownTcs = new();
+    private TaskCompletionSource _holdWriteTcs = new();
 
-        public async Task<TransportConnectionInformation> ConnectAsync(CancellationToken cancellationToken)
+    public async Task<TransportConnectionInformation> ConnectAsync(CancellationToken cancellationToken)
+    {
+        if (FailOperation.HasFlag(DuplexTransportOperation.Connect))
         {
-            if (FailOperation.HasFlag(DuplexTransportOperation.Connect))
-            {
-                throw new IceRpcException(IceRpcError.IceRpcError, "Test transport connect failure");
-            }
-            await _holdConnectTcs.Task.WaitAsync(cancellationToken);
-            return await _decoratee.ConnectAsync(cancellationToken);
+            throw new IceRpcException(IceRpcError.IceRpcError, "Test transport connect failure");
         }
+        await _holdConnectTcs.Task.WaitAsync(cancellationToken);
+        return await _decoratee.ConnectAsync(cancellationToken);
+    }
 
-        public void Dispose()
-        {
-            _disposeCalledTcs.TrySetResult();
-            _decoratee.Dispose();
+    public void Dispose()
+    {
+        _disposeCalledTcs.TrySetResult();
+        _decoratee.Dispose();
 
-            _holdConnectTcs.TrySetResult();
-            _holdReadTcs.TrySetResult();
-            _holdShutdownTcs.TrySetResult();
-            _holdWriteTcs.TrySetResult();
-        }
+        _holdConnectTcs.TrySetResult();
+        _holdReadTcs.TrySetResult();
+        _holdShutdownTcs.TrySetResult();
+        _holdWriteTcs.TrySetResult();
+    }
 
-        public async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken)
-        {
-            await CheckFailAndHoldAsync();
+    public async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+    {
+        await CheckFailAndHoldAsync();
 
-            int count = await _decoratee.ReadAsync(buffer, cancellationToken);
+        int count = await _decoratee.ReadAsync(buffer, cancellationToken);
 
-            // Check again fail/hold condition in case the configuration was changed while AcceptAsync was pending.
-            await CheckFailAndHoldAsync();
+        // Check again fail/hold condition in case the configuration was changed while AcceptAsync was pending.
+        await CheckFailAndHoldAsync();
 
-            return count;
+        return count;
 
-            async Task CheckFailAndHoldAsync()
-            {
-                if (FailOperation.HasFlag(DuplexTransportOperation.Read))
-                {
-                    throw new IceRpcException(IceRpcError.IceRpcError, "Test transport read failure");
-                }
-                await _holdReadTcs.Task.WaitAsync(cancellationToken);
-            }
-        }
-
-        public async Task ShutdownAsync(CancellationToken cancellationToken)
-        {
-            if (FailOperation.HasFlag(DuplexTransportOperation.Shutdown))
-            {
-                throw new IceRpcException(IceRpcError.IceRpcError, "Test transport shutdown failure");
-            }
-            await _holdShutdownTcs.Task.WaitAsync(cancellationToken);
-            await _decoratee.ShutdownAsync(cancellationToken);
-        }
-
-        public async ValueTask WriteAsync(
-            IReadOnlyList<ReadOnlyMemory<byte>> buffers,
-            CancellationToken cancellationToken)
+        async Task CheckFailAndHoldAsync()
         {
             if (FailOperation.HasFlag(DuplexTransportOperation.Read))
             {
-                throw new IceRpcException(IceRpcError.IceRpcError, "Test transport write failure");
+                throw new IceRpcException(IceRpcError.IceRpcError, "Test transport read failure");
             }
-            await _holdWriteTcs.Task.WaitAsync(cancellationToken);
-
-            await _decoratee.WriteAsync(buffers, cancellationToken);
+            await _holdReadTcs.Task.WaitAsync(cancellationToken);
         }
-
-        internal TestDuplexConnectionDecorator(IDuplexConnection decoratee) => _decoratee = decoratee;
     }
+
+    public async Task ShutdownAsync(CancellationToken cancellationToken)
+    {
+        if (FailOperation.HasFlag(DuplexTransportOperation.Shutdown))
+        {
+            throw new IceRpcException(IceRpcError.IceRpcError, "Test transport shutdown failure");
+        }
+        await _holdShutdownTcs.Task.WaitAsync(cancellationToken);
+        await _decoratee.ShutdownAsync(cancellationToken);
+    }
+
+    public async ValueTask WriteAsync(
+        IReadOnlyList<ReadOnlyMemory<byte>> buffers,
+        CancellationToken cancellationToken)
+    {
+        if (FailOperation.HasFlag(DuplexTransportOperation.Read))
+        {
+            throw new IceRpcException(IceRpcError.IceRpcError, "Test transport write failure");
+        }
+        await _holdWriteTcs.Task.WaitAsync(cancellationToken);
+
+        await _decoratee.WriteAsync(buffers, cancellationToken);
+    }
+
+    internal TestDuplexConnectionDecorator(IDuplexConnection decoratee) => _decoratee = decoratee;
 }
