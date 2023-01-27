ae5340a9d6baa8c76a28b1700a23d8c55f260641
Jose <jose@zeroc.com>
2023-01-27 16:05:36 +0100

    Wait for shutdown before disposing connections - Fix #2618

--- src/IceRpc/Internal/MetricsProtocolConnectionDecorator.cs
+++ src/IceRpc/Internal/MetricsProtocolConnectionDecorator.cs
@@ -12,7 +12,6 @@ internal class MetricsProtocolConnectionDecorator : IProtocolConnection
     private readonly bool _logStart;
     private readonly IProtocolConnection _decoratee;
     private readonly Metrics _metrics;
-    private volatile Task? _shutdownTask;
     private TransportConnectionInformation? _connectionInformation;
 
     public async Task<(TransportConnectionInformation ConnectionInformation, Task ShutdownRequested)> ConnectAsync(
@@ -43,20 +42,6 @@ internal class MetricsProtocolConnectionDecorator : IProtocolConnection
     public async ValueTask DisposeAsync()
     {
         await _decoratee.DisposeAsync().ConfigureAwait(false);
-
-        // Wait for _shutdownTask's completion
-        if (_shutdownTask is Task shutdownTask)
-        {
-            try
-            {
-                await shutdownTask.ConfigureAwait(false);
-            }
-            catch
-            {
-                // observe and ignore any exception
-            }
-        }
-
         if (_connectionInformation is not null)
         {
             _metrics.ConnectionStop();
@@ -66,41 +51,16 @@ internal class MetricsProtocolConnectionDecorator : IProtocolConnection
     public Task<IncomingResponse> InvokeAsync(OutgoingRequest request, CancellationToken cancellationToken) =>
         _decoratee.InvokeAsync(request, cancellationToken);
 
-    public Task ShutdownAsync(CancellationToken cancellationToken)
+    public async Task ShutdownAsync(CancellationToken cancellationToken)
     {
         try
         {
-            _shutdownTask = PerformShutdownAsync(_decoratee.ShutdownAsync(cancellationToken));
-            return _shutdownTask;
-        }
-        // catch exceptions thrown synchronously by _decoratee.ShutdownAsync
-        catch (InvalidOperationException)
-        {
-            // Thrown if ConnectAsync wasn't called, or if ShutdownAsync is called twice.
-            throw;
+            await _decoratee.ShutdownAsync(cancellationToken).ConfigureAwait(false);
         }
         catch
         {
             _metrics.ConnectionFailure();
             throw;
-        }
-
-        async Task PerformShutdownAsync(Task decorateeShutdownTask)
-        {
-            try
-            {
-                await decorateeShutdownTask.ConfigureAwait(false);
-            }
-            catch (InvalidOperationException)
-            {
-                // Thrown if ConnectAsync wasn't called, or if ShutdownAsync is called twice.
-                throw;
-            }
-            catch
-            {
-                _metrics.ConnectionFailure();
-                throw;
-            }
         }
     }
 
