d65d23d49d2f29881753e94a385cfce8737d4e89
Jose <jose@zeroc.com>
2023-01-27 19:20:31 +0100

    Catching unobserved task exceptions in tests

--- tests/IceRpc.Tests/SetUpFixture.cs
+++ tests/IceRpc.Tests/SetUpFixture.cs
@@ -8,33 +8,9 @@ namespace IceRpc.Tests;
 [SetUpFixture]
 public sealed class SetUpFixture
 {
-    private static readonly EventHandler<UnobservedTaskExceptionEventArgs> _handler = HandleUnobservedTaskException;
-    private static readonly List<(object?, Exception)> _unobservedTaskExceptions = new();
-
     [OneTimeSetUp]
-    public void OneTimeSetup()
-    {
-        AssertTraceListener.Setup();
-        TaskScheduler.UnobservedTaskException += _handler;
-    }
+    public void OneTimeSetUp() => CommonSetUpFixture.OneTimeSetUp();
 
     [OneTimeTearDown]
-    public void OneTimeTearDown()
-    {
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-        TaskScheduler.UnobservedTaskException -= _handler;
-
-        if (_unobservedTaskExceptions.Count > 0)
-        {
-            Console.Error.WriteLine($"Tests triggered {_unobservedTaskExceptions.Count} unobserved task exceptions");
-            foreach ((object? sender, Exception exception) in _unobservedTaskExceptions)
-            {
-                Console.Error.WriteLine($"Unobserved task exception {sender}:\n{exception}");
-            }
-        }
-    }
-
-    private static void HandleUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e) =>
-        _unobservedTaskExceptions.Add((sender, e.Exception.InnerException ?? e.Exception));
+    public void OneTimeTearDown() => CommonSetUpFixture.OneTimeTearDown();
 }
