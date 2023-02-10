918b1be16754861016f3be3fa9d48ae8e760ab4b
Bernard Normier <bernard@zeroc.com>
2023-02-10 14:12:05 -0500

    Fix comment

--- examples/HelloCore/Server/Program.cs
+++ examples/HelloCore/Server/Program.cs
@@ -3,7 +3,7 @@
 using HelloCoreExample;
 using IceRpc;
 
-// Create a server that will dispatch all requests to the same dispatcher/service, an instance of Chatbot.
+// Create a server that will dispatch all requests to the same dispatcher, an instance of Chatbot.
 await using var server = new Server(new Chatbot());
 server.Listen();
 
