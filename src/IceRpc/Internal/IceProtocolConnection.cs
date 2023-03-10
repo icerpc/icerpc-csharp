80decf980d7a95a340ca01e656ad17e39fbf9c0d
Bernard Normier <bernard@zeroc.com>
2023-03-10 11:52:24 -0500

    Fix IceProtocolConnection to cancel twoway dispatches on read failure

--- src/IceRpc/Internal/IceProtocolConnection.cs
+++ src/IceRpc/Internal/IceProtocolConnection.cs
@@ -1319,6 +1319,11 @@ internal sealed class IceProtocolConnection : IProtocolConnection
                 _sendCloseConnectionFrame = false;
             }
 
+            // ReadFailed is called when the connection is dead or the peer sent us a non-supported frame (e.g. a
+            // batch request). We don't need to allow outstanding twoway dispatches to complete in these situations, so
+            // we cancel these twoway dispatches speed-up the shutdown.
+            _twowayDispatchesCts.Cancel();
+
             _ = _shutdownRequestedTcs.TrySetResult();
         }
     }
