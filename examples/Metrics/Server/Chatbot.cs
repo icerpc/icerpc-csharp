e1716947f758da1ee9adcf82b01f4405e146a0eb
Jose <jose@zeroc.com>
2023-03-13 13:37:01 +0100

    Simplify metrics example

--- examples/Metrics/Server/Chatbot.cs
+++ examples/Metrics/Server/Chatbot.cs
@@ -8,27 +8,12 @@ namespace MetricsExample;
 /// <summary>A Chatbot is an IceRPC service that implements Slice interface 'Hello'.</summary>
 internal class Chatbot : Service, IHelloService
 {
-    private volatile int _totalRequests;
-    private bool _hasOutput;
-
     public ValueTask<string> SayHelloAsync(
         string name,
         IFeatureCollection features,
         CancellationToken cancellationToken)
     {
-        int totalRequests = Interlocked.Increment(ref _totalRequests);
-        if (totalRequests % 1000 == 0)
-        {
-            if (_hasOutput)
-            {
-                Console.SetCursorPosition(0, Console.CursorTop - 1);
-            }
-            else
-            {
-                _hasOutput = true;
-            }
-            Console.WriteLine("{0,-30}: {1}", "Total `SayHelloAsync` dispatches", _totalRequests);
-        }
+        Console.WriteLine($"Dispatching sayHello request {{ name = '{name}' }}");
         return new($"Hello, {name}!");
     }
 }
