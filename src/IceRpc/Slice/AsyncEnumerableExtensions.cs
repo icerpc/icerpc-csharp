5392208baaf0f235105ec84047306c1b717b18f1
Benoit Foucher <benoit@zeroc.com>
2023-03-21 17:11:49 +0100

    More fixes for #2786

--- src/IceRpc/Slice/AsyncEnumerableExtensions.cs
+++ src/IceRpc/Slice/AsyncEnumerableExtensions.cs
@@ -1,5 +1,7 @@
 // Copyright (c) ZeroC, Inc.
 
+using System.Buffers;
+using System.Diagnostics;
 using System.IO.Pipelines;
 
 namespace IceRpc.Slice;
@@ -66,10 +68,38 @@ public static class AsyncEnumerableExtensions
 
         public override void Complete(Exception? exception = null)
         {
+            try
+            {
+                // Cancel MoveNextAsync if it's still running.
+                _cts.Cancel();
+            }
+            catch (ObjectDisposedException)
+            {
+                return; // Complete already called.
+            }
+
             _pipe.Reader.Complete();
             _pipe.Writer.Complete();
             _cts.Dispose();
-            _ = _asyncEnumerator.DisposeAsync().AsTask();
+
+            _ = DisposeEnumeratorAsync();
+
+            async Task DisposeEnumeratorAsync()
+            {
+                // Make sure MoveNextAsync is completed before disposing the enumerator. Calling DisposeAsync on the
+                // enumerator while MoveNextAsync is still running is disallowed.
+                if (_moveNext is not null)
+                {
+                    try
+                    {
+                        await _moveNext.ConfigureAwait(false);
+                    }
+                    catch
+                    {
+                    }
+                }
+                _ = _asyncEnumerator.DisposeAsync().AsTask();
+            }
         }
 
         public void Dispose() => Complete();
@@ -77,33 +107,48 @@ public static class AsyncEnumerableExtensions
         public override async ValueTask<ReadResult> ReadAsync(CancellationToken cancellationToken = default)
         {
             // If no more buffered data to read, fill the pipe with new data.
-            // Also returns true when the writer is completed.
             if (_pipe.Reader.TryRead(out ReadResult readResult))
             {
                 return readResult;
             }
             else
             {
-                bool hasNext;
-                if (_moveNext is null)
-                {
-                    hasNext = await _asyncEnumerator.MoveNextAsync().ConfigureAwait(false);
-                }
-                else
-                {
-                    hasNext = await _moveNext.ConfigureAwait(false);
-                    _moveNext = null;
-                }
+                // If ReadAsync is canceled, cancel the enumerator iteration to ensure MoveNextAsync below completes.
+                using CancellationTokenRegistration registration = cancellationToken.UnsafeRegister(
+                    cts => ((CancellationTokenSource)cts!).Cancel(),
+                    _cts);
 
-                if (hasNext && EncodeElements() is Task<bool> moveNext)
+                bool hasNext;
+                try
                 {
-                    _moveNext = moveNext;
-                    _ = await _pipe.Writer.FlushAsync(cancellationToken).ConfigureAwait(false);
-                    // And the next ReadAsync will await _moveNext.
+                    if (_moveNext is null)
+                    {
+                        hasNext = await _asyncEnumerator.MoveNextAsync().ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        hasNext = await _moveNext.ConfigureAwait(false);
+                        _moveNext = null;
+                    }
+
+                    if (hasNext && EncodeElements() is Task<bool> moveNext)
+                    {
+                        _moveNext = moveNext;
+                        _ = await _pipe.Writer.FlushAsync(cancellationToken).ConfigureAwait(false);
+                        // And the next ReadAsync will await _moveNext.
+                    }
+                    else
+                    {
+                        _pipe.Writer.Complete();
+                    }
                 }
-                else
+                catch (OperationCanceledException exception)
                 {
-                    _pipe.Writer.Complete();
+                    Debug.Assert(exception.CancellationToken == _cts.Token);
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    // CancelPendingRead was called, return a canceled read result.
+                    return new ReadResult(ReadOnlySequence<byte>.Empty, isCanceled: true, isCompleted: false);
                 }
             }
 
