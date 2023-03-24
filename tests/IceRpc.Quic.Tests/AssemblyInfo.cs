3c143fb4001a04c73576ea67e13996777429b536
Benoit Foucher <benoit@zeroc.com>
2023-03-24 15:32:00 +0100

    Fix for #2622 (#2811)

--- tests/IceRpc.Quic.Tests/AssemblyInfo.cs
+++ tests/IceRpc.Quic.Tests/AssemblyInfo.cs
@@ -4,8 +4,12 @@ using System.Runtime.Versioning;
 
 // TODO: we don't use a 8 s timeout to workaround connection establishment delays that occur with the
 // Connect_fails_if_listener_is_disposed test. The https://github.com/dotnet/runtime/issues/77310 issue is possibly the
-// cause of this delay.
+// cause of this delay. The issue is fixed with .NET 8.
+#if NET8_0_OR_GREATER
+[assembly: NUnit.Framework.Timeout(8000)]
+#else
 [assembly: NUnit.Framework.Timeout(16000)]
+#endif
 
 // We need these attributes because the .NET QUIC APIs have the same.
 [assembly: SupportedOSPlatform("linux")]
