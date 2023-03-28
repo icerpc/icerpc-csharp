caf6f4476a18635635306872cfc5a5ec2af0eb05
Bernard Normier <bernard@zeroc.com>
2023-03-28 09:51:32 -0400

    Move IdentityPath to module Ice (#2827)

--- src/IceRpc/Slice/IceIdentityPathSliceEncoderExtensions.cs
+++ src/IceRpc/Slice/IceIdentityPathSliceEncoderExtensions.cs
@@ -1,15 +1,16 @@
 // Copyright (c) ZeroC, Inc.
 
+using IceRpc.Slice;
 using IceRpc.Slice.Internal;
 
-namespace IceRpc.Slice;
+namespace IceRpc;
 
 /// <summary>Provides an extension method for encoding a path as an Ice identity.</summary>
-public static class IdentityPathSliceEncoderExtensions
+public static class IceIdentityPathSliceEncoderExtensions
 {
     /// <summary>Encodes a path as an Ice identity.</summary>
     /// <param name="encoder">The Slice encoder.</param>
     /// <param name="value">The path to encode as an Ice identity.</param>
-    public static void EncodeIdentityPath(this ref SliceEncoder encoder, string value) =>
+    public static void EncodeIceIdentityPath(this ref SliceEncoder encoder, string value) =>
         Identity.Parse(value).Encode(ref encoder);
 }
