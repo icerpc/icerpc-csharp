411b462301c9b17aec2945393004633ddbe23aa8
Bernard Normier <bernard@zeroc.com>
2023-04-27 11:11:52 -0400

    Update interop examples (#3069)

--- examples/Interop/IceGrid/Client/Program.cs
+++ examples/Interop/IceGrid/Client/Program.cs
@@ -6,16 +6,11 @@ using IceRpc.Ice;
 using IceRpc.Retry;
 using Microsoft.Extensions.Logging;
 
-await using var connectionCache = new ConnectionCache();
-
-// Create a new invocation pipeline
+// Create an invocation pipeline for all our proxies.
 var pipeline = new Pipeline();
 
 // Create a locator proxy with the invocation pipeline as its invoker.
-var locator = new LocatorProxy(pipeline, new Uri("ice://localhost/DemoIceGrid/Locator"));
-
-// Create a hello proxy with the invocation pipeline as its invoker. Note that this proxy has no server address.
-var hello = new HelloProxy(pipeline, new Uri("ice:/hello"));
+var locatorProxy = new LocatorProxy(pipeline, new Uri("ice://localhost/DemoIceGrid/Locator"));
 
 // Create a logger factory that logs to the console.
 using ILoggerFactory loggerFactory = LoggerFactory.Create(
@@ -25,14 +20,20 @@ using ILoggerFactory loggerFactory = LoggerFactory.Create(
         builder.AddSimpleConsole(configure => configure.IncludeScopes = true);
     });
 
-// Add the retry, locator, and logger interceptors to the pipeline
+// Create a connection cache. A locator interceptor is typically installed in an invocation pipeline that flows into
+// a connection cache.
+await using var connectionCache = new ConnectionCache();
+
+// Add the retry, locator, and logger interceptors to the pipeline.
 pipeline = pipeline
     .UseRetry(new RetryOptions(), loggerFactory)
-    .UseLocator(locator, loggerFactory)
+    .UseLocator(locatorProxy, loggerFactory)
     .UseLogger(loggerFactory)
     .Into(connectionCache);
 
-// Interactive prompt to the user
+// Create a hello proxy with the invocation pipeline as its invoker. Note that this proxy has no server address.
+var helloProxy = new HelloProxy(pipeline, new Uri("ice:/hello"));
+
 menu();
 string? line = null;
 do
@@ -46,10 +47,12 @@ do
         switch (line)
         {
             case "t":
-                await hello.SayHelloAsync();
+                // The locator interceptor calls the locator during this invocation to resolve `/hello` into one or more
+                // server addresses; the locator interceptor caches successful resolutions.
+                await helloProxy.SayHelloAsync();
                 break;
             case "s":
-                await hello.ShutdownAsync();
+                await helloProxy.ShutdownAsync();
                 break;
             case "x":
                 break;
@@ -62,9 +65,9 @@ do
                 break;
         };
     }
-    catch (Exception ex)
+    catch (Exception exception)
     {
-        await Console.Error.WriteLineAsync(ex.ToString());
+        await Console.Error.WriteLineAsync(exception.ToString());
     }
 } while (line != "x");
 
