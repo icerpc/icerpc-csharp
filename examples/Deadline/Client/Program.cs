c45ba0b55d8bb585425d0742dc9c275dbf0e2720
Jose <jose@zeroc.com>
2023-05-01 14:27:41 +0200

    Fix review comments

--- examples/Deadline/Client/Program.cs
+++ examples/Deadline/Client/Program.cs
@@ -7,16 +7,15 @@ using IceRpc.Features;
 
 await using var connection = new ClientConnection(new Uri("icerpc://localhost"));
 
-// Create an invocation pipeline, that uses the Deadline interceptor and has a default timeout of 500 ms.
+// Create an invocation pipeline, that uses the deadline interceptor and has a default timeout of 500 ms.
 Pipeline pipeline = new Pipeline()
     .UseDeadline(defaultTimeout: TimeSpan.FromMilliseconds(500))
     .Into(connection);
 
 var greeterProxy = new GreeterProxy(pipeline);
 
-// The SlowChatbot service implementation used in this example delays the sending of the response 1 second, which is
-// greater than the configured default timeout of 500 ms. The invocation would be canceled and the deadline interceptor
-// would throw TimeoutException.
+// In this example, the implementation of the greet operation takes about 1 second, and the deadline
+// interceptor makes sure the invocation throws TimeoutException after 500 ms.
 try
 {
     _ = await greeterProxy.GreetAsync(Environment.UserName);
@@ -25,7 +24,6 @@ catch (TimeoutException exception)
 {
     Console.WriteLine(exception.Message);
 }
-
 
 // The next invocation uses the deadline feature to configure a higher deadline, which wont be canceled before than the
 // SlowChatbot sends a response.
