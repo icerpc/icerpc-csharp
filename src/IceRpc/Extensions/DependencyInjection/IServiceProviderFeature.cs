5a53eb1a2384f9f4ddf447c6de249bb0355b321f
Jose <jose@zeroc.com>
2023-05-03 23:20:52 +0200

    Move service provider feature to dependency injection extensions

--- src/IceRpc/Extensions/DependencyInjection/IServiceProviderFeature.cs
+++ src/IceRpc/Extensions/DependencyInjection/IServiceProviderFeature.cs
@@ -1,6 +1,6 @@
 // Copyright (c) ZeroC, Inc.
 
-namespace IceRpc.Features;
+namespace IceRpc.Extensions.DependencyInjection;
 
 /// <summary>A feature that gives access to the service provider. When transmitted with an
 /// <see cref="IncomingRequest" />, this service provider is typically the service provider of the async scope created
