5aed23eb5ca59997548cc11be04cfb1bea57ff05
Bernard Normier <bernard@zeroc.com>
2023-05-03 17:51:56 -0400

    Remove UseLogger ILogger overloads (#3126)

--- src/IceRpc.Logger/LoggerPipelineExtensions.cs
+++ src/IceRpc.Logger/LoggerPipelineExtensions.cs
@@ -5,22 +5,14 @@ using Microsoft.Extensions.Logging;
 
 namespace IceRpc;
 
-/// <summary>This class provides extension methods to add the logger interceptor to a <see cref="Pipeline" />.
-/// </summary>
+/// <summary>Provides an extension method to add a logger interceptor to a <see cref="Pipeline" />.</summary>
 public static class LoggerPipelineExtensions
 {
-    /// <summary>Adds a <see cref="LoggerInterceptor" /> to the pipeline.</summary>
-    /// <param name="pipeline">The pipeline being configured.</param>
-    /// <param name="logger">The logger to log to.</param>
-    /// <returns>The pipeline being configured.</returns>
-    public static Pipeline UseLogger(this Pipeline pipeline, ILogger logger) =>
-        pipeline.Use(next => new LoggerInterceptor(next, logger));
-
-    /// <summary>Adds a <see cref="LoggerInterceptor" /> to the pipeline.</summary>
+    /// <summary>Adds a <see cref="LoggerInterceptor" /> to this pipeline.</summary>
     /// <param name="pipeline">The pipeline being configured.</param>
     /// <param name="loggerFactory">The logger factory used to create a <see cref="ILogger{TCategoryName}" /> for
     /// <see cref="LoggerInterceptor" />.</param>
     /// <returns>The pipeline being configured.</returns>
     public static Pipeline UseLogger(this Pipeline pipeline, ILoggerFactory loggerFactory) =>
-        pipeline.UseLogger(loggerFactory.CreateLogger<LoggerInterceptor>());
+        pipeline.Use(next => new LoggerInterceptor(next, loggerFactory.CreateLogger<LoggerInterceptor>()));
 }
--- src/IceRpc.Logger/LoggerRouterExtensions.cs
+++ src/IceRpc.Logger/LoggerRouterExtensions.cs
@@ -5,22 +5,14 @@ using Microsoft.Extensions.Logging;
 
 namespace IceRpc;
 
-/// <summary>This class provides extension methods to add the logger middleware to a <see cref="Router" />.
-/// </summary>
+/// <summary>Provides an extension method to add a logger middleware to a <see cref="Router" />.</summary>
 public static class LoggerRouterExtensions
 {
-    /// <summary>Adds a <see cref="LoggerMiddleware" /> to this router.</summary>
-    /// <param name="router">The router being configured.</param>
-    /// <param name="logger">The logger to log to.</param>
-    /// <returns>The router being configured.</returns>
-    public static Router UseLogger(this Router router, ILogger logger) =>
-        router.Use(next => new LoggerMiddleware(next, logger));
-
     /// <summary>Adds a <see cref="LoggerMiddleware" /> to this router.</summary>
     /// <param name="router">The router being configured.</param>
     /// <param name="loggerFactory">The logger factory used to create a <see cref="ILogger{TCategoryName}" /> for
     /// <see cref="LoggerMiddleware" />.</param>
     /// <returns>The router being configured.</returns>
     public static Router UseLogger(this Router router, ILoggerFactory loggerFactory) =>
-       router.UseLogger(loggerFactory.CreateLogger<LoggerMiddleware>());
+       router.Use(next => new LoggerMiddleware(next, loggerFactory.CreateLogger<LoggerMiddleware>()));
 }
