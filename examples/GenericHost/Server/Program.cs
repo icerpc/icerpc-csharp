01dbd3576ac0b3c07c8de5e6f83855fac8a3cda5
Bernard Normier <bernard@zeroc.com>
2023-05-03 12:29:06 -0400

    Create separate builder for GenericHost example (#3120)

--- examples/GenericHost/Server/Program.cs
+++ examples/GenericHost/Server/Program.cs
@@ -10,8 +10,8 @@ using System.Diagnostics;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-// Configure and build the host.
-IHost host = Host.CreateDefaultBuilder(args)
+// Configure the host.
+var hostBuilder = Host.CreateDefaultBuilder(args)
     // Set the content root path to the build directory of the server (e.g.: Server/bin/Debug/net7.0)
     .UseContentRoot(AppContext.BaseDirectory)
 
@@ -49,8 +49,10 @@ IHost host = Host.CreateDefaultBuilder(args)
                 .UseTelemetry()
                 .UseLogger()
                 .Map<IGreeterService>());
-    })
-    .Build();
+    });
+
+// Build the host.
+using IHost host = hostBuilder.Build();
 
 // Run hosted program.
 host.Run();
