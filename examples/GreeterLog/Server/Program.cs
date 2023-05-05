006851172b87267b1003827121b16cf7ae101b0e
Bernard Normier <bernard@zeroc.com>
2023-05-05 15:22:50 -0400

    Update formatting

--- examples/GreeterLog/Server/Program.cs
+++ examples/GreeterLog/Server/Program.cs
@@ -1,18 +1,22 @@
 // Copyright (c) ZeroC, Inc.
 
-using Microsoft.Extensions.Logging;
 using GreeterLogExample;
 using IceRpc;
+using Microsoft.Extensions.Logging;
 
 // Create a simple console logger factory and configure the log level for category IceRpc.
-using ILoggerFactory loggerFactory = LoggerFactory.Create(
-    builder => builder.AddSimpleConsole().AddFilter("IceRpc", LogLevel.Debug));
+using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+    builder
+        .AddSimpleConsole()
+        .AddFilter("IceRpc", LogLevel.Debug));
 
 // Create a router (dispatch pipeline) and install the logger middleware. This middleware logs dispatches using category
 // `IceRpc.Logger.LoggerMiddleware`.
 // We also map our implementation of `IGreeterService` at the default path for this service:
 // `/GreeterLogExample.Greeter`
-Router router = new Router().UseLogger(loggerFactory).Map<IGreeterService>(new Chatbot());
+Router router = new Router()
+    .UseLogger(loggerFactory)
+    .Map<IGreeterService>(new Chatbot());
 
 // Create a server that logs message to a logger with category `IceRpc.Server`.
 await using var server = new Server(router, logger: loggerFactory.CreateLogger<Server>());
