bc055bc998e17a306f3ededce801d686f7747411
Jose <jose@zeroc.com>
2023-05-05 17:34:02 +0200

    Add metrics interceptor - Fix #3135

--- examples/Metrics/Client/Program.cs
+++ examples/Metrics/Client/Program.cs
@@ -4,8 +4,9 @@ using MetricsExample;
 using IceRpc;
 
 await using var connection = new ClientConnection(new Uri("icerpc://localhost"));
+Pipeline pipeline = new Pipeline().UseMetrics().Into(connection);
 
-var greeter = new GreeterProxy(connection);
+var greeter = new GreeterProxy(pipeline);
 
 double requestsPerSecond = 20;
 Console.WriteLine($"Sending {requestsPerSecond} requests per second...");
