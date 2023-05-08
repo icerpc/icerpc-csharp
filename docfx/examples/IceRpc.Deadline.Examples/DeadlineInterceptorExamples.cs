8fc5cec5d1cb8d62c2ddd501ea9953b72c288e6d
Jose <jose@zeroc.com>
2023-05-08 21:15:55 +0200

    Cleanup

--- docfx/examples/IceRpc.Deadline.Examples/DeadlineInterceptorExamples.cs
+++ docfx/examples/IceRpc.Deadline.Examples/DeadlineInterceptorExamples.cs
@@ -17,7 +17,6 @@ public static class DeadlineInterceptorExamples
         #endregion
     }
 
-
     public static async Task UseDeadlineWithDefaultTimeout()
     {
         #region UseDeadlineWithDefaultTimeout
