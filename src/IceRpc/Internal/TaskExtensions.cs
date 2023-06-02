23fccf7c633032316e11668ae62c1d422b261033
Bernard Normier <bernard@zeroc.com>
2023-06-02 11:33:40 -0400

    Fix review comments

--- src/IceRpc/Internal/TaskExtensions.cs
+++ src/IceRpc/Internal/TaskExtensions.cs
@@ -5,27 +5,29 @@ namespace IceRpc.Internal;
 internal static class TaskExtensions
 {
     /// <summary>Converts this task into a linked cancellation token.</summary>
-    /// <param name="task">The task that upon successful completion cancels the linked token.</param>
+    /// <param name="task">The task that upon completion cancels the linked token.</param>
     /// <param name="token">The source token.</param>
-    /// <returns>A cancellation token that is canceled when <paramref name="task" /> completes successfully or
-    /// <paramref name="token" /> is canceled. If the task is canceled or fails, the returned token is not canceled.
-    /// </returns>
+    /// <returns>A cancellation token that is canceled when <paramref name="task" /> completes or
+    /// <paramref name="token" /> is canceled.</returns>
     internal static CancellationToken AsCancellationToken(this Task task, CancellationToken token)
     {
-#pragma warning disable CA2000
         var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(token);
-#pragma warning restore CA2000
 
         CancellationToken linkedToken = linkedCts.Token;
-
-        // CancellationToken.None because we need to run CancelOnSuccessAsync to dispose linkedCts.
-        _ = Task.Run(CancelOnSuccessAsync, CancellationToken.None);
+        _ = CancelOnCompleteAsync();
         return linkedToken;
 
-        async Task CancelOnSuccessAsync()
+        async Task CancelOnCompleteAsync()
         {
             using CancellationTokenSource cts = linkedCts; // takes ownership of linkedCts
-            await task.ConfigureAwait(false);
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch
+            {
+                // It's ok for task to complete with an exception.
+            }
             cts.Cancel();
         }
     }
