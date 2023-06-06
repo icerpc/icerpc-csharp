e9cc871f328ccdda24a5a8d840a7ee92a980d756
Bernard Normier <bernard@zeroc.com>
2023-06-06 10:25:37 -0400

    Fix doc-comment

--- src/IceRpc/IncomingFrame.cs
+++ src/IceRpc/IncomingFrame.cs
@@ -13,10 +13,10 @@ public class IncomingFrame
     public IConnectionContext ConnectionContext { get; set; }
 
     /// <summary>Gets or sets the payload of this frame.</summary>
-    /// <value>The payload of this frame. Defaults to a <see cref="PipeReader" /> that returns an empty
-    /// sequence.</value>
-    /// <remarks>IceRPC completes the payload <see cref="PipeReader" /> with the <see
-    /// cref="PipeReader.Complete(Exception?)" /> method. It never calls <see
+    /// <value>The payload of this frame. Defaults to a <see cref="PipeReader" /> that returns an empty sequence.
+    /// </value>
+    /// <remarks>IceRPC and Slice complete the payload <see cref="PipeReader" /> with the <see
+    /// cref="PipeReader.Complete(Exception?)" /> method. They never call <see
     /// cref="PipeReader.CompleteAsync(Exception?)" />. The implementation of <see
     /// cref="PipeReader.Complete(Exception?)" /> should not block.</remarks>
     public PipeReader Payload { get; set; } = EmptyPipeReader.Instance;
