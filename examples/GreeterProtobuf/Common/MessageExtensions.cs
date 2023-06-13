000d3f7cde37fb37ca2db4cae8b7b360b71e2a9d
Benoit Foucher <benoit@zeroc.com>
2023-06-13 17:58:10 +0200

    No need to complete the payload

--- examples/GreeterProtobuf/Common/MessageExtensions.cs
+++ examples/GreeterProtobuf/Common/MessageExtensions.cs
@@ -10,12 +10,10 @@ public static class MessageExtensions
     /// <summary>Merges data from a <see cref="PipeReader" /> into an existing message.</summary>
     /// <param name="message">The <see cref="IMessage" /> to merge the data into.</param>
     /// <param name="reader">The <see cref="PipeReader" /> containing the protobuf-encoded binary data to merge.</param>
-    /// <remarks>This method completes the given <see cref="PipeReader" />.</remarks>
     public static async Task MergeFromAsync(this IMessage message, PipeReader reader)
     {
         using var stream = new MemoryStream();
         await reader.CopyToAsync(stream).ConfigureAwait(false);
-        reader.Complete();
         stream.Seek(0, SeekOrigin.Begin);
         message.MergeFrom(stream);
     }
