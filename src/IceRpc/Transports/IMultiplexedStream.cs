43a144942043efbbbeac607bda07df2e9c178b61
Benoit Foucher <benoit@zeroc.com>
2023-06-14 11:01:18 +0200

    Doc fix

--- src/IceRpc/Transports/IMultiplexedStream.cs
+++ src/IceRpc/Transports/IMultiplexedStream.cs
@@ -4,7 +4,7 @@ using System.IO.Pipelines;
 
 namespace IceRpc.Transports;
 
-/// <summary>A multiplexed stream enables byte data exchange over a multiplexed transport. </summary>
+/// <summary>A multiplexed stream enables byte data exchange over a multiplexed transport.</summary>
 /// <remarks>The implementation of the <see cref="IDuplexPipe" /> interface is required to return a <see
 /// cref="ReadOnlySequencePipeWriter" /> for the <see cref="IDuplexPipe.Output" />.</remarks>
 public interface IMultiplexedStream : IDuplexPipe
