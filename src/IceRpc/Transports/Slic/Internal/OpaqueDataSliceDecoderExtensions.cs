ba6a7d9ad3c9cd9cfa54d71db327f31a7ab63fbf
Bernard Normier <bernard@zeroc.com>
2023-06-19 17:19:01 -0400

    Make OpaqueData*Extensions internal

--- src/IceRpc/Transports/Slic/Internal/OpaqueDataSliceDecoderExtensions.cs
+++ src/IceRpc/Transports/Slic/Internal/OpaqueDataSliceDecoderExtensions.cs
@@ -4,12 +4,12 @@ using IceRpc.Slice;
 
 namespace IceRpc.Transports.Slic.Internal;
 
-/// <summary>Provides an extension method for decoding a 64-bit opaque data value into a <see langword="long"
-/// />.</summary>
-public static class OpaqueDataSliceDecoderExtensions
+/// <summary>Provides an extension method for decoding a 64-bit opaque data value into a <see langword="long"/>.
+/// </summary>
+internal static class OpaqueDataSliceDecoderExtensions
 {
     /// <summary>Decodes a 64-bit opaque data value.</summary>
     /// <param name="decoder">The Slice decoder.</param>
     /// <returns>The opaque data value decoded as a <see langword="long"/>.</returns>
-    public static long DecodeOpaqueData(this ref SliceDecoder decoder) => decoder.DecodeInt64();
+    internal static long DecodeOpaqueData(this ref SliceDecoder decoder) => decoder.DecodeInt64();
 }
--- src/IceRpc/Transports/Slic/Internal/OpaqueDataSliceEncoderExtensions.cs
+++ src/IceRpc/Transports/Slic/Internal/OpaqueDataSliceEncoderExtensions.cs
@@ -4,12 +4,12 @@ using IceRpc.Slice;
 
 namespace IceRpc.Transports.Slic.Internal;
 
-/// <summary>Provides an extension method for encoding a <see langword="long" /> into a 64-bit opaque data
-/// value.</summary>
-public static class OpaqueDataSliceEncoderExtensions
+/// <summary>Provides an extension method for encoding a <see langword="long" /> into a 64-bit opaque data value.
+/// </summary>
+internal static class OpaqueDataSliceEncoderExtensions
 {
     /// <summary>Encodes a <see langword="long" /> as a 64-bit opaque data value.</summary>
     /// <param name="encoder">The Slice encoder.</param>
     /// <param name="value">The value to encode.</param>
-    public static void EncodeOpaqueData(this ref SliceEncoder encoder, long value) => encoder.EncodeInt64(value);
+    internal static void EncodeOpaqueData(this ref SliceEncoder encoder, long value) => encoder.EncodeInt64(value);
 }
