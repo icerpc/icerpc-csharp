65fb2596077900ad7f1947789e0ed0337a041adc
Jose <pepone@users.noreply.github.com>
2023-06-22 15:30:19 +0200

    Vscode warnings (#3415)

--- docfx/template/ManagedReference.extension.js
+++ docfx/template/ManagedReference.extension.js
@@ -2,19 +2,19 @@ exports.postTransform = function (model) {
 
     if(model.isEnum) {
 
-        var childrens = model.children[0].children;
+        let children = model.children[0].children;
 
         // Extract the enumerator value and set the "_enum_value" property.
-        childrens.forEach((item) => {
+        children.forEach((item) => {
             const regex = /[\w\d]+\s?=\s?(\d+),?/gm;
-            var m = regex.exec(item.syntax.content[0].value);
+            let m = regex.exec(item.syntax.content[0].value);
             if (m !== null) {
                 item._enum_value = parseInt(m[1]);
             }
         });
 
         // Sort the enumerators in ascending order based on their values."
-        model.children[0].children = childrens.sort((lhs, rhs) => (lhs._enum_value - rhs._enum_value));
+        model.children[0].children = children.sort((lhs, rhs) => (lhs._enum_value - rhs._enum_value));
     }
 
     return model;
