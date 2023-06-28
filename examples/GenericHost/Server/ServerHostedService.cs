b2652335ee81a47f63deb7e10a6adacd561884b2
Jose <jose@zeroc.com>
2023-06-28 13:27:58 +0200

    Generic host Server disposal - Fix #3433

--- examples/GenericHost/Server/ServerHostedService.cs
+++ examples/GenericHost/Server/ServerHostedService.cs
@@ -6,18 +6,12 @@ using Microsoft.Extensions.Hosting;
 namespace GenericHostServer;
 
 /// <summary>The server hosted service is ran and managed by the .NET Generic Host</summary>
-public class ServerHostedService : IHostedService, IAsyncDisposable
+public class ServerHostedService : IHostedService
 {
     // The IceRPC server accepts connections from IceRPC clients.
     private readonly Server _server;
 
     public ServerHostedService(Server server) => _server = server;
-
-    public ValueTask DisposeAsync()
-    {
-        GC.SuppressFinalize(this);
-        return _server.DisposeAsync();
-    }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
