b7c047829193088a467248f074290f5a2566e4a3
Jose <jose@zeroc.com>
2023-07-05 13:30:21 +0200

    Document Stream Id schema

--- src/IceRpc/Transports/IMultiplexedStream.cs
+++ src/IceRpc/Transports/IMultiplexedStream.cs
@@ -13,6 +13,11 @@ public interface IMultiplexedStream : IDuplexPipe
     /// <value>The stream ID.</value>
     /// <exception cref="InvalidOperationException">Thrown if the stream is not started. Local streams are not started
     /// until data is written. A remote stream is always started.</exception>
+    /// <remarks>The stream Id indentifies a stream within a connection, the least significant bit of a stream ID
+    /// identifies the initiator of the stream, for client initiated streams this bit is set to 0 and for server
+    /// initiated streams is set to 1. The second least significant bit identifies if the stream is bidirectional with
+    /// this bit set to 0, or unidirectional with this bit set to 1. Successive streams of each type are created with
+    /// numerically increasing stream IDs. This is the same as the stream Id schema used by QUIC.</remarks>
     ulong Id { get; }
 
     /// <summary>Gets a value indicating whether the stream is bidirectional.</summary>
