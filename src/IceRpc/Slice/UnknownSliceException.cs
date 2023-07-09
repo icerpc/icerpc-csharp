fa62b83a27a522993748675dae92135ab36d3db3
Bernard Normier <bernard@zeroc.com>
2023-07-09 14:46:13 -0400

    Make ctor internal

--- src/IceRpc/Slice/UnknownSliceException.cs
+++ src/IceRpc/Slice/UnknownSliceException.cs
@@ -10,15 +10,15 @@ public sealed class UnknownSliceException : SliceException
     /// <summary>Gets the Slice type ID of this unknown Slice exception.</summary>
     public string TypeId { get; }
 
-    /// <summary>Constructs an unknown Slice exception with the provided type ID and message.</summary>
-    /// <param name="typeId">The Slice type ID of the exception.</param>
-    /// <param name="message">A message that describes the exception.</param>
-    public UnknownSliceException(string typeId, string? message = null)
-        : base(message) =>
-        TypeId = typeId;
-
     /// <inheritdoc/>
     // We can't encode such an exception because we don't preserve exception slices during decoding.
     [EditorBrowsable(EditorBrowsableState.Never)]
     protected override void EncodeCore(ref SliceEncoder encoder) => throw new NotSupportedException();
+
+    /// <summary>Constructs an unknown Slice exception with the provided type ID and message.</summary>
+    /// <param name="typeId">The Slice type ID of the exception.</param>
+    /// <param name="message">A message that describes the exception.</param>
+    internal UnknownSliceException(string typeId, string? message = null)
+        : base(message) =>
+        TypeId = typeId;
 }
