cf1438fcae9bb8f3636426298e691df074c9474e
Benoit Foucher <benoit@zeroc.com>
2023-07-10 12:11:03 +0200

    Workaround for slow Quic tests when running on Linux/OpenSSL 3.0 (#3484)

--- tests/IceRpc.Quic.Tests/AssemblyInfo.cs
+++ tests/IceRpc.Quic.Tests/AssemblyInfo.cs
@@ -11,6 +11,9 @@ using System.Runtime.Versioning;
 [assembly: NUnit.Framework.Timeout(16000)]
 #endif
 
+// TODO: Remove once OpenSSL performance issues are resolved (https://github.com/openssl/openssl/issues/17627).
+[assembly: NUnit.Framework.LevelOfParallelism(2)]
+
 // We need these attributes because the .NET QUIC APIs have the same.
 [assembly: SupportedOSPlatform("linux")]
 [assembly: SupportedOSPlatform("macOS")]
--- tests/IceRpc.Tests/AssemblyInfo.cs
+++ tests/IceRpc.Tests/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 // Copyright (c) ZeroC, Inc.
 
 [assembly: NUnit.Framework.Timeout(8000)]
+
+// Limit the maximum number of NUnit workers to workaround failures when running with high number of workers
+[assembly: NUnit.Framework.LevelOfParallelism(10)]
