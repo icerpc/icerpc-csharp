d6ec606b33075999432bca1a559914311071a533
Bernard Normier <bernard@zeroc.com>
2023-07-12 08:53:10 -0400

    Update src/Slice/SliceEncoding.cs
    Co-authored-by: Jose  <pepone@users.noreply.github.com>

--- src/Slice/SliceEncoding.cs
+++ src/Slice/SliceEncoding.cs
@@ -8,6 +8,6 @@ public enum SliceEncoding : byte
     /// <summary>Slice encoding version 1. It's identical to the Ice encoding version 1.1.</summary>
     Slice1 = 1,
 
-    /// <summary>Slice encoding version 2..</summary>
+    /// <summary>Slice encoding version 2.</summary>
     Slice2 = 2,
 }
