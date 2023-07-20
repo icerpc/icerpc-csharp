3452de273a9b8ee997b30287f91e583b9f9b66ce
Jose <jose@zeroc.com>
2023-07-20 13:32:55 +0200

    Fix typo

--- src/IceRpc/Internal/IceDefinitions.cs
+++ src/IceRpc/Internal/IceDefinitions.cs
@@ -17,7 +17,7 @@ internal static class IceDefinitions
     internal static readonly byte[] Magic = "IceP"u8.ToArray(); // 'I', 'c', 'e', 'P'
 
     // 4-bytes after magic that provide the protocol version (always 1.0 for an ice frame) and the encoding of the
-    // frame header (always set to 1.0 with the an ice frame, even though we use Slice1).
+    // frame header (always set to 1.0 with an ice frame, even though we use Slice1).
     internal static readonly byte[] ProtocolBytes = new byte[] { 1, 0, 1, 0 };
 
     internal static readonly IcePrologue CloseConnectionFrame = new(
