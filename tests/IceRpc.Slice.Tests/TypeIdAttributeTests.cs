f79aa984cb2b47233f7f6f1659796a6fda6709bd
Bernard Normier <bernard@zeroc.com>
2023-08-19 15:42:19 -0400

    Remove GetAllSliceTypeIds

--- tests/IceRpc.Slice.Tests/TypeIdAttributeTests.cs
+++ tests/IceRpc.Slice.Tests/TypeIdAttributeTests.cs
@@ -8,32 +8,6 @@ namespace IceRpc.Slice.Tests.TypeIdAttributeTestNamespace;
 
 public sealed class TypeIdAttributeTests
 {
-    /// <summary>Provides test case data for the <see cref="Get_all_slice_type_ids" /> test.</summary>
-    private static IEnumerable<TestCaseData> GetAllSliceTypeIdsSource
-    {
-        get
-        {
-            foreach ((Type type, string[] expected) in _allTypeIds)
-            {
-                yield return new TestCaseData(type, expected);
-            }
-        }
-    }
-
-    private static readonly Dictionary<Type, string[]> _allTypeIds = new()
-    {
-        [typeof(IceObjectProxy)] = new string[] { "::Ice::Object" },
-        [typeof(PingableProxy)] = new string[] { "::IceRpc::Slice::Tests::Pingable" },
-        [typeof(IMyDerivedInterface)] = new string[]
-        {
-            "::IceRpc::Slice::Tests::TypeIdAttributeTestNamespace::MyDerivedInterface",
-            "::IceRpc::Slice::Tests::TypeIdAttributeTestNamespace::MyInterface",
-            "::IceRpc::Slice::Tests::TypeIdAttributeTestNamespace::myOtherInterface",
-        },
-        [typeof(ServerAddress)] = Array.Empty<string>(),
-        [typeof(ServiceAddress)] = Array.Empty<string>(),
-    };
-
     /// <summary>Verifies that interface types generated from Slice definitions have the expected type ID.</summary>
     /// <param name="type">The <see cref="Type" /> of the generated type to test.</param>
     /// <param name="expected">The expected type ID.</param>
@@ -48,13 +22,6 @@ public sealed class TypeIdAttributeTests
     {
         string? typeId = type.GetSliceTypeId();
         Assert.That(typeId, Is.EqualTo(expected));
-    }
-
-    [Test, TestCaseSource(nameof(GetAllSliceTypeIdsSource))]
-    public void Get_all_slice_type_ids(Type type, string[] expected)
-    {
-        string[] typeIds = type.GetAllSliceTypeIds();
-        Assert.That(typeIds, Is.EqualTo(expected));
     }
 
     /// <summary>Verifies that types generated from Slice definitions have the expected default path.</summary>
