843b6b85306ffb70c1124c56e27e70802a810371
Jose <pepone@users.noreply.github.com>
2023-10-12 16:31:23 +0200

    Connect_fails_if_listener_is_disposed test simplifications (#3685)

--- tests/IceRpc.Tests/AssemblyInfo.cs
+++ tests/IceRpc.Tests/AssemblyInfo.cs
@@ -1,6 +1,3 @@
 // Copyright (c) ZeroC, Inc.
 
 [assembly: NUnit.Framework.Timeout(8000)]
-
-// Limit the maximum number of NUnit workers to workaround failures when running with high number of workers
-[assembly: NUnit.Framework.LevelOfParallelism(10)]
