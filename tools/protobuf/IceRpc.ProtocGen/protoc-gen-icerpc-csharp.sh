a0d0bdb619f94094b807be615303d302648188e7
Jose <jose@zeroc.com>
2023-10-18 13:51:37 +0200

    Fix IceRpc.ProtocGen bash script

--- tools/protobuf/IceRpc.ProtocGen/protoc-gen-icerpc-csharp.sh
+++ tools/protobuf/IceRpc.ProtocGen/protoc-gen-icerpc-csharp.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dotnet $DIR/IceRpc.ProtocGen.dll -- "$@"
+set -ue
+
+# Everything in this script is relative to the directory containing this script.
+# This works most of the time but is not perfect. It will fail if the script is sourced or if the script is
+# executed from a symlink.
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+dotnet IceRpc.ProtocGen.dll -- "$@"
