3f90d74686e0b39a97e83af8e5036972ebffc7d8
Jose <pepone@users.noreply.github.com>
2023-10-18 20:50:20 +0200

    Fix Inconsistent line endings in Service.g.cs (#3703)

--- src/IceRpc.Slice.Generators/Internal/StringExtensions.cs
+++ src/IceRpc.Slice.Generators/Internal/StringExtensions.cs
@@ -1,12 +1,30 @@
 // Copyright (c) ZeroC, Inc.
 
+using System.Runtime.InteropServices;
+
 namespace IceRpc.Slice.Generators.Internal;
 
 internal static class StringExtensions
 {
-    internal static string WithIndent(this string contents, string indent) =>
-        string.Join("\n", contents.Split('\n').Select(value => $"{indent}{value}")).Trim();
+    private static readonly string _newLine = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+
+    // TODO Once generators can target .NET 8 we can use String.ReplaceLineEndings and remove this implementation.
+    internal static string ReplaceLineEndings(this string contents)
+    {
+        if (contents.IndexOf("\r\n") != -1)
+        {
+            contents = contents.Replace("\r\n", "\n");
+        }
+        if (_newLine != "\n")
+        {
+            contents = contents.Replace("\n", _newLine);
+        }
+        return contents;
+    }
 
     internal static string TrimTrailingWhiteSpaces(this string contents) =>
         string.Join("\n", contents.Split('\n').Select(value => value.TrimEnd()));
+
+    internal static string WithIndent(this string contents, string indent) =>
+        string.Join("\n", contents.Split('\n').Select(value => $"{indent}{value}")).Trim();
 }
