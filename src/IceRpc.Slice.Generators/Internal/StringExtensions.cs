829d1dd1def8be09626ea3e6057704b19645eacc
Jose <jose@zeroc.com>
2023-10-18 20:29:36 +0200

    Sort methods alphabetically

--- src/IceRpc.Slice.Generators/Internal/StringExtensions.cs
+++ src/IceRpc.Slice.Generators/Internal/StringExtensions.cs
@@ -8,12 +8,6 @@ internal static class StringExtensions
 {
     private static readonly string _newLine = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
 
-    internal static string WithIndent(this string contents, string indent) =>
-        string.Join("\n", contents.Split('\n').Select(value => $"{indent}{value}")).Trim();
-
-    internal static string TrimTrailingWhiteSpaces(this string contents) =>
-        string.Join("\n", contents.Split('\n').Select(value => value.TrimEnd()));
-
     // TODO Once generators can target .NET 8 we can use String.ReplaceLineEndings and remove this implementation.
     internal static string ReplaceLineEndings(this string contents)
     {
@@ -27,4 +21,10 @@ internal static class StringExtensions
         }
         return contents;
     }
+
+    internal static string TrimTrailingWhiteSpaces(this string contents) =>
+        string.Join("\n", contents.Split('\n').Select(value => value.TrimEnd()));
+
+    internal static string WithIndent(this string contents, string indent) =>
+        string.Join("\n", contents.Split('\n').Select(value => $"{indent}{value}")).Trim();
 }
