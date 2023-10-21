9fded7d9dc367d44ff8eef1fd5a05f64afed19cc
Bernard Normier <bernard@zeroc.com>
2023-10-20 20:41:26 -0400

    Update DiagnosticDescriptor title

--- src/IceRpc.Protobuf.Generators/Internal/DiagnosticDescriptors.cs
+++ src/IceRpc.Protobuf.Generators/Internal/DiagnosticDescriptors.cs
@@ -8,7 +8,7 @@ internal static class DiagnosticDescriptors
 {
     internal static DiagnosticDescriptor DuplicateRpcName { get; } = new DiagnosticDescriptor(
         id: "PROTO0001",
-        title: "Multiple Protobuf rpc methods cannot have the same name within a service class",
+        title: "An IceRPC service class cannot implement multiple Protobuf rpc methods with the same name",
         messageFormat: "Multiple Protobuf rpc methods named {0} in class {1}",
         category: "ProtobufServiceGenerator",
         DiagnosticSeverity.Error,
