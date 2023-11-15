72484b5e25016886947ca71462585a51e61a2842
Jose <pepone@users.noreply.github.com>
2023-11-15 21:42:36 +0100

    Add test for Protobuf max message length (#3755)

---
+++ tests/IceRpc.Protobuf.Tests/PipeReaderExtensionsTests.cs
@@ -0,0 +1,28 @@
+// Copyright (c) ZeroC, Inc.
+
+using Google.Protobuf.WellKnownTypes;
+using NUnit.Framework;
+using System.IO.Pipelines;
+
+namespace IceRpc.Protobuf.Tests;
+
+[Parallelizable(scope: ParallelScope.All)]
+public partial class PipeReaderExtensionsTests
+{
+    [Test]
+    public void Read_message_throws_invalid_data_exception_when_max_message_length_is_exceeded()
+    {
+        // Arrange
+        var stringValue = new StringValue();
+        stringValue.Value = new string('s', 1024);
+        var pipeReader = stringValue.EncodeAsLengthPrefixedMessage(new PipeOptions(pauseWriterThreshold: 0));
+
+        // Act & Assert
+        Assert.ThrowsAsync<InvalidDataException>(async () =>
+            await pipeReader.DecodeProtobufMessageAsync(
+                StringValue.Parser,
+                maxMessageLength: 1024,
+                CancellationToken.None));
+        pipeReader.Complete();
+    }
+}
