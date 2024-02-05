83ffb09b2e63caa6337a982c9590e51e7ca286a9
Austin Henriksen <austin@zeroc.com>
2024-02-05 11:44:19 -0500

    Simplify functions.

--- tools/slicec-cs/src/slicec_ext/operation_ext.rs
+++ tools/slicec-cs/src/slicec_ext/operation_ext.rs
@@ -3,7 +3,7 @@
 use super::{EntityExt, MemberExt, ParameterExt, ParameterSliceExt};
 use crate::code_gen_util::TypeContext;
 use crate::cs_attributes::CsEncodedReturn;
-use slicec::grammar::{AttributeFunctions, Contained, Operation};
+use slicec::grammar::{AttributeFunctions, Operation};
 
 pub trait OperationExt {
     /// Returns the format that classes should be encoded with.
@@ -26,18 +26,13 @@ impl OperationExt for Operation {
     }
 
     fn invocation_return_task(&self) -> String {
-        let return_members = self.return_members();
-        if return_members.is_empty() {
-            "global::System.Threading.Tasks.Task".to_owned()
-        } else {
-            let namespace = self.parent().namespace();
-            let return_type = match self.return_members().as_slice() {
-                [] => "void".to_owned(),
-                [member] => member.cs_type_string(&namespace, TypeContext::IncomingParam),
-                members => members.to_tuple_type(&namespace, TypeContext::IncomingParam),
-            };
-            format!("global::System.Threading.Tasks.Task<{return_type}>")
-        }
+        let namespace = self.namespace();
+        let return_type = match self.return_members().as_slice() {
+            [] => return "global::System.Threading.Tasks.Task".to_owned(),
+            [member] => member.cs_type_string(&namespace, TypeContext::IncomingParam),
+            members => members.to_tuple_type(&namespace, TypeContext::IncomingParam),
+        };
+        format!("global::System.Threading.Tasks.Task<{return_type}>")
     }
 
     fn dispatch_return_task(&self) -> String {
@@ -45,7 +40,7 @@ impl OperationExt for Operation {
         if return_members.is_empty() {
             "global::System.Threading.Tasks.ValueTask".to_owned()
         } else {
-            let namespace = self.parent().namespace();
+            let namespace = self.namespace();
             let return_type = if self.has_attribute::<CsEncodedReturn>() {
                 if let Some(stream_member) = self.streamed_return_member() {
                     format!(
@@ -57,8 +52,8 @@ impl OperationExt for Operation {
                     "global::System.IO.Pipelines.PipeReader".to_owned()
                 }
             } else {
-                match self.return_members().as_slice() {
-                    [] => "void".to_owned(),
+                match return_members.as_slice() {
+                    [] => unreachable!(),
                     [member] => member.cs_type_string(&namespace, TypeContext::OutgoingParam),
                     members => members.to_tuple_type(&namespace, TypeContext::OutgoingParam),
                 }
