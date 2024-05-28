6e71ae66467b6f02ef226b15db993fa33ea81e95
Austin Henriksen <austin.r.henriksen79@gmail.com>
2024-05-28 11:18:24 -0400

    More word tweaking.

--- tools/slicec-cs/src/cs_util.rs
+++ tools/slicec-cs/src/cs_util.rs
@@ -210,7 +210,7 @@ pub fn format_comment_message(message: &Message, namespace: &str) -> String {
 }
 
 fn xml_escape(text: &str) -> String {
-    // We don't need to escape single-quotes because 'slicec-cs' always generate double-quoted strings.
+    // We don't need to escape single-quote characters because 'slicec-cs' always generates double-quoted strings.
     text.replace('&', "&amp;")
         .replace('<', "&lt;")
         .replace('>', "&gt;")
