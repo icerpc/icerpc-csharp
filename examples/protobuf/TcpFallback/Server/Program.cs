9a148c63ba46cbbe803a4271acee44a633c51780
Jose <jose@zeroc.com>
2024-09-12 12:21:11 +0200

    Mark certificate keys as exportable for compatibility with macOS

--- examples/protobuf/TcpFallback/Server/Program.cs
+++ examples/protobuf/TcpFallback/Server/Program.cs
@@ -24,7 +24,10 @@ await using var quicServer = new Server(
     new SslServerAuthenticationOptions
     {
         ServerCertificateContext = SslStreamCertificateContext.Create(
-            X509CertificateLoader.LoadPkcs12FromFile(certificatePath, password: null),
+            X509CertificateLoader.LoadPkcs12FromFile(
+                certificatePath,
+                password: null,
+                keyStorageFlags: X509KeyStorageFlags.DefaultKeySet),
             additionalCertificates: null)
     },
     multiplexedServerTransport: new QuicServerTransport(),
@@ -37,7 +40,10 @@ await using var tcpServer = new Server(
     new SslServerAuthenticationOptions
     {
         ServerCertificateContext = SslStreamCertificateContext.Create(
-            X509CertificateLoader.LoadPkcs12FromFile(certificatePath, password: null),
+            X509CertificateLoader.LoadPkcs12FromFile(
+                certificatePath,
+                password: null,
+                keyStorageFlags: X509KeyStorageFlags.DefaultKeySet),
             additionalCertificates: null)
     },
     logger: loggerFactory.CreateLogger<Server>());
