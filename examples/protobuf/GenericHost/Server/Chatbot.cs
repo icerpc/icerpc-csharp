ffdb5842d5c3ef7292a152586facedb61f32011b
Jose <jose@zeroc.com>
2025-11-20 15:40:19 +0100

    Fix GenericHost examples

--- examples/protobuf/GenericHost/Server/Chatbot.cs
+++ examples/protobuf/GenericHost/Server/Chatbot.cs
@@ -8,8 +8,13 @@ namespace GenericHostServer;
 
 /// <summary>A Chatbot is an IceRPC service that implements Protobuf service 'Greeter'.</summary>
 [ProtobufService]
+[System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Maintainability",
+    "CA1515:Consider making public types internal",
+    Justification = "The generated side is public")]
 public partial class Chatbot : IGreeterService
 {
+    /// <inheritdoc/>
     public ValueTask<GreetResponse> GreetAsync(
         GreetRequest message,
         IFeatureCollection features,
