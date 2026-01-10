d55cb6c13bf459c471b1ecd9fec273180f3612ab
Bernard Normier <bernard@zeroc.com>
2026-01-09 23:04:20 -0500

    Improve Chatbot message in Retry examples (#4219)

--- examples/slice/Retry/Server/Chatbot.cs
+++ examples/slice/Retry/Server/Chatbot.cs
@@ -18,14 +18,16 @@ internal partial class Chatbot : IGreeterService
 
     public ValueTask<string> GreetAsync(string name, IFeatureCollection features, CancellationToken cancellationToken)
     {
-        Console.WriteLine($"Dispatching greet request {{ name = '{name}' }}");
         // 50% failure/success ratio
         if (RandomNumberGenerator.GetInt32(10) < 5)
         {
+            Console.WriteLine(
+                $"Dispatching greet request {{ name = '{name}' }} => DispatchException(StatusCode.Unavailable)");
             throw new DispatchException(StatusCode.Unavailable);
         }
         else
         {
+            Console.WriteLine($"Dispatching greet request {{ name = '{name}' }} => greeting");
             return new($"Hello, {name}, from server #{_serverNumber}!");
         }
     }
