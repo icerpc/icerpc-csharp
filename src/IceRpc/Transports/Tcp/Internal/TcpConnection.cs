a92ccf105998a28a8910d3f0ee01173d3cf9fa94
Jose <jose@zeroc.com>
2026-03-05 11:23:05 +0100

    Remove Socket.ConnectAsync cancellation workaround
    The workaround for dotnet/runtime#75889 is no longer needed since the
    issue was fixed in .NET 10 (dotnet/runtime#116943).

--- src/IceRpc/Transports/Tcp/Internal/TcpConnection.cs
+++ src/IceRpc/Transports/Tcp/Internal/TcpConnection.cs
@@ -331,10 +331,6 @@ internal class TcpClientConnection : TcpConnection
             // Connect to the peer.
             await Socket.ConnectAsync(_addr, cancellationToken).ConfigureAwait(false);
 
-            // Workaround: a canceled Socket.ConnectAsync call can return successfully but the Socket is closed because
-            // of the cancellation. See https://github.com/dotnet/runtime/issues/75889.
-            cancellationToken.ThrowIfCancellationRequested();
-
             if (_authenticationOptions is not null)
             {
                 _sslStream = new SslStream(new NetworkStream(Socket, false), false);
