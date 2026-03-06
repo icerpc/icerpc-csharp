b599fa0b95af509a8d7133af890f0a6bb210b0b4
Bernard Normier <bernard@zeroc.com>
2026-03-06 13:08:29 -0500

    Split WellKnownTypesTests

--- src/ZeroC.Slice.Codec/AssemblyInfo.cs
+++ src/ZeroC.Slice.Codec/AssemblyInfo.cs
@@ -4,4 +4,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ZeroC.Slice.Codec.Tests")]
 [assembly: InternalsVisibleTo("ZeroC.Slice.IceGen.Tests")]
-[assembly: InternalsVisibleTo("ZeroC.Slice.SlicecGen.Tests")]
