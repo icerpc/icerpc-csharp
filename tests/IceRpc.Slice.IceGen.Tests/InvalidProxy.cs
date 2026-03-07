f71632df153e5846aa96c0a98792cddd7c7bf1cb
Bernard Normier <bernard@zeroc.com>
2026-03-07 16:39:02 -0500

    Move SlicGen and IceGen tests to separate assemblies

---
+++ tests/IceRpc.Slice.IceGen.Tests/InvalidProxy.cs
@@ -0,0 +1,28 @@
+// Copyright (c) ZeroC, Inc.
+
+namespace IceRpc.Slice.IceGen.Tests;
+
+internal class InvalidProxy : IProxy
+{
+    internal static InvalidProxy Instance { get; } = new InvalidProxy();
+
+    public SliceEncodeOptions? EncodeOptions
+    {
+        get => throw new NotImplementedException();
+        init { }
+    }
+
+    public IInvoker Invoker
+    {
+        get => throw new NotImplementedException();
+        init { }
+    }
+
+    public ServiceAddress ServiceAddress
+    {
+        get => throw new NotImplementedException();
+        init { }
+    }
+
+    private InvalidProxy() { }
+}
