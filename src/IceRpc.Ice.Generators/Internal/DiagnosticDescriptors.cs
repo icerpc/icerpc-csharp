f25a97b3db9a94382a2319f487912ef57a0d04f8
Bernard Normier <bernard@zeroc.com>
2026-03-10 15:52:54 -0400

    Update diagnostic ID for Ice

--- src/IceRpc.Ice.Generators/Internal/DiagnosticDescriptors.cs
+++ src/IceRpc.Ice.Generators/Internal/DiagnosticDescriptors.cs
@@ -7,7 +7,7 @@ namespace IceRpc.Ice.Generators.Internal;
 internal static class DiagnosticDescriptors
 {
     internal static DiagnosticDescriptor DuplicateOperationNames { get; } = new DiagnosticDescriptor(
-        id: "SLICE0001",
+        id: "ICE0001",
         title: "Multiple Ice operations cannot have the same name within a service class",
         messageFormat: "Multiple Ice operations named {0} in class {1}",
         category: "IceServiceGenerator",
