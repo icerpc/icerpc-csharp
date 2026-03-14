33ae1cc6ffb40649e0a702a6c1aaaa3816b62d8d
Bernard Normier <bernard@zeroc.com>
2026-03-14 15:13:00 -0400

    Cleanup

--- tests/IceRpc.Ice.Codec.Tests/SequenceEncodingTests.cs
+++ tests/IceRpc.Ice.Codec.Tests/SequenceEncodingTests.cs
@@ -12,40 +12,36 @@ namespace IceRpc.Ice.Codec.Tests;
 public class SequenceEncodingTests
 {
     /// <summary>Provides test case data for
-    /// <see cref="Encode_fixed_sized_numeric_sequence(SliceEncoding, IEnumerable{int})" /> test.</summary>
+    /// <see cref="Encode_fixed_sized_numeric_sequence(IEnumerable{int})" /> test.</summary>
     private static IEnumerable<TestCaseData> SequenceLongData
     {
         get
         {
-            foreach (SliceEncoding encoding in new[] { SliceEncoding.Slice1 })
+            foreach (int size in new int[] { 0, 256 })
             {
-                foreach (int size in new int[] { 0, 256 })
-                {
-                    IEnumerable<int> values = Enumerable.Range(0, size).Select(i => i);
+                IEnumerable<int> values = Enumerable.Range(0, size).Select(i => i);
 
-                    yield return new TestCaseData(encoding, values);
-                    yield return new TestCaseData(encoding, ImmutableArray.CreateRange(values));
-                    yield return new TestCaseData(encoding, new ArraySegment<int>(values.ToArray()));
-                    yield return new TestCaseData(encoding, values.ToArray());
-                };
-            }
+                yield return new TestCaseData(values);
+                yield return new TestCaseData(ImmutableArray.CreateRange(values));
+                yield return new TestCaseData(new ArraySegment<int>(values.ToArray()));
+                yield return new TestCaseData(values.ToArray());
+            };
         }
     }
 
     /// <summary>Tests <see cref="SliceEncoderExtensions.EncodeSequence{T}(ref SliceEncoder, IEnumerable{T})" /> with a
     /// value type.</summary>
-    /// <param name="encoding">The <see cref="SliceEncoding" /> to use for the encoding.</param>
     /// <param name="expected">The enumerable to be encoded.</param>
     [Test, TestCaseSource(nameof(SequenceLongData))]
-    public void Encode_fixed_sized_numeric_sequence(SliceEncoding encoding, IEnumerable<int> expected)
+    public void Encode_fixed_sized_numeric_sequence(IEnumerable<int> expected)
     {
         var buffer = new MemoryBufferWriter(new byte[1024 * 1024]);
-        var sut = new SliceEncoder(buffer, encoding);
+        var sut = new SliceEncoder(buffer, SliceEncoding.Slice1);
         int size = expected.Count();
 
         sut.EncodeSequence(expected);
 
-        var decoder = new SliceDecoder(buffer.WrittenMemory, encoding);
+        var decoder = new SliceDecoder(buffer.WrittenMemory, SliceEncoding.Slice1);
         Assert.That(decoder.DecodeSize(), Is.EqualTo(size));
         var decoded = new List<int>();
         for (int i = 0; i < size; ++i)
