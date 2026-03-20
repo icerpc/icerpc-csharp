0e67a093cfd68fd055d4193dfd070926157d4c25
Bernard Normier <bernard@zeroc.com>
2026-03-20 14:28:17 -0400

    Use simpler slice2cs codegen (#4327)

--- src/IceRpc/Ice/Codec/IceGeneratedCodeAttribute.cs
+++ src/IceRpc/Ice/Codec/IceGeneratedCodeAttribute.cs
@@ -4,12 +4,12 @@ namespace IceRpc.Ice.Codec;
 
 /// <summary>Represents an assembly attribute for assemblies that contain Ice generated code.</summary>
 [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
-public sealed class IceAttribute : Attribute
+public sealed class IceGeneratedCodeAttribute : Attribute
 {
     /// <summary>Gets the name of the file that contains the Ice definitions.</summary>
     public string SourceFileName { get; }
 
-    /// <summary>Constructs a new instance of <see cref="IceAttribute" />.</summary>
+    /// <summary>Constructs a new instance of <see cref="IceGeneratedCodeAttribute" />.</summary>
     /// <param name="sourceFileName">The name of the source file.</param>
-    public IceAttribute(string sourceFileName) => SourceFileName = sourceFileName;
+    public IceGeneratedCodeAttribute(string sourceFileName) => SourceFileName = sourceFileName;
 }
