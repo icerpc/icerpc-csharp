ab6dc2cfbc53332d5a2195a403168693a22b0243
Bernard Normier <bernard@zeroc.com>
2026-03-27 14:21:25 -0400

    Remove check for QUIC in Default property (#4355)

--- src/IceRpc/Transports/IMultiplexedServerTransport.cs
+++ src/IceRpc/Transports/IMultiplexedServerTransport.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ZeroC, Inc.
 
 using IceRpc.Transports.Quic;
-using System.Net.Quic;
 using System.Net.Security;
 using System.Runtime.Versioning;
 
@@ -18,15 +17,10 @@ public interface IMultiplexedServerTransport
         {
             if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS() || OperatingSystem.IsWindows())
             {
-                if (QuicListener.IsSupported)
-                {
-                    return _quicServerTransport;
-                }
-                throw new NotSupportedException(
-                    "The default QUIC server transport is not available on this system. Please review the Platform Dependencies for QUIC in the .NET documentation.");
+                return _quicServerTransport;
             }
             throw new PlatformNotSupportedException(
-                "The default QUIC server transport is not supported on this platform.");
+                "The default multiplexed server transport, QUIC, is only available on Linux, macOS, and Windows.");
         }
     }
 
