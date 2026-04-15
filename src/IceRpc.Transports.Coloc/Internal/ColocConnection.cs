98e55838c8b560f3e2c16efba824ef725dba8b2d
Jose <jose@zeroc.com>
2026-04-15 19:41:02 +0200

    Reject empty buffer in ColocConnection.ReadAsync (#4481)

--- src/IceRpc.Transports.Coloc/Internal/ColocConnection.cs
+++ src/IceRpc.Transports.Coloc/Internal/ColocConnection.cs
@@ -32,6 +32,11 @@ internal abstract class ColocConnection : IDuplexConnection
     {
         ObjectDisposedException.ThrowIf(_state.HasFlag(State.Disposed), this);
 
+        if (buffer.Length == 0)
+        {
+            throw new ArgumentException($"The {nameof(buffer)} cannot be empty.", nameof(buffer));
+        }
+
         if (_reader is null)
         {
             throw new InvalidOperationException("Reading is not allowed before connection is connected.");
