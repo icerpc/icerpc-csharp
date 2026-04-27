f00cff218e0e0eed06bdb9188231994bfbb99a44
Jose <pepone@users.noreply.github.com>
2026-04-27 17:23:58 +0200

    Qualify nested Protobuf message types in generated code (#4554)

--- src/IceRpc.Protobuf.Generator/MessageDescriptorExtensions.cs
+++ src/IceRpc.Protobuf.Generator/MessageDescriptorExtensions.cs
@@ -9,8 +9,9 @@ internal static class MessageDescriptorExtensions
     internal static string GetType(this MessageDescriptor messageDescriptor, string scope, bool streaming)
     {
         string csharpNamespace = messageDescriptor.File.GetCsharpNamespace();
+        string qualifiedName = messageDescriptor.GetQualifiedCsharpName();
         string csharpType = scope == csharpNamespace ?
-            messageDescriptor.Name : $"global::{csharpNamespace}.{messageDescriptor.Name}";
+            qualifiedName : $"global::{csharpNamespace}.{qualifiedName}";
         if (streaming)
         {
             csharpType = $"global::System.Collections.Generic.IAsyncEnumerable<{csharpType}>";
@@ -21,8 +22,21 @@ internal static class MessageDescriptorExtensions
     internal static string GetParserType(this MessageDescriptor messageDescriptor, string scope)
     {
         string csharpNamespace = messageDescriptor.File.GetCsharpNamespace();
+        string qualifiedName = messageDescriptor.GetQualifiedCsharpName();
         string csharpType = scope == csharpNamespace ?
-            messageDescriptor.Name : $"global::{csharpNamespace}.{messageDescriptor.Name}";
+            qualifiedName : $"global::{csharpNamespace}.{qualifiedName}";
         return $"{csharpType}.Parser";
+    }
+
+    // Google's C# Protobuf generator emits nested message types inside a "Types" container class on each enclosing
+    // message (e.g. message Outer { message Inner {} } becomes Outer.Types.Inner).
+    private static string GetQualifiedCsharpName(this MessageDescriptor messageDescriptor)
+    {
+        string name = messageDescriptor.Name;
+        for (MessageDescriptor? parent = messageDescriptor.ContainingType; parent is not null; parent = parent.ContainingType)
+        {
+            name = $"{parent.Name}.Types.{name}";
+        }
+        return name;
     }
 }
