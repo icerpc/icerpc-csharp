bb2fcd8552f95e84e261a5ff6826af75703c6058
Austin Henriksen <austin@zeroc.com>
2026-05-07 10:37:35 -0400

    Prefer code-tags for where it's actually just code, not links.

--- src/IceRpc.Slice.Generator/OperationExtensions.cs
+++ src/IceRpc.Slice.Generator/OperationExtensions.cs
@@ -131,8 +131,8 @@ internal static class OperationExtensions
             return fn.Build();
         }
 
-        /// <summary>Returns the C# return type for an operation (Task, Task{T}, or Task{tuple}).
-        /// Stream returns are included in the tuple with their stream type.</summary>
+        /// <summary>Returns the C# return type for an operation (<c>Task</c>, <c>Task&lt;T&gt;</c>, or
+        /// <c>Task&lt;tuple&gt;</c>). Stream returns are included in the tuple with their stream type.</summary>
         internal string GetClientReturnType(string currentNamespace) =>
             BuildReturnType("global::System.Threading.Tasks.Task", op, currentNamespace, fieldType: false);
 
