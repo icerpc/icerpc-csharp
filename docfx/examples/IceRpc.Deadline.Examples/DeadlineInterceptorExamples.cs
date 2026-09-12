eac5b4bb1fe48ecb8bb0e6053267578eef7a4e8e
Bernard Normier <bernard@zeroc.com>
2026-09-11 20:45:17 -0400

    Add authentication options to the DocFX code snippets (#4935)

--- docfx/examples/IceRpc.Deadline.Examples/DeadlineInterceptorExamples.cs
+++ docfx/examples/IceRpc.Deadline.Examples/DeadlineInterceptorExamples.cs
@@ -1,5 +1,8 @@
 // Copyright (c) ZeroC, Inc.
 
+using System.Security.Cryptography.X509Certificates;
+using static Program;
+
 namespace IceRpc.Deadline.Examples;
 
 // This class provides code snippets used by the doc-comments of the deadline interceptor.
@@ -8,7 +11,13 @@ public static class DeadlineInterceptorExamples
     public static async Task UseDeadline()
     {
         #region UseDeadline
-        await using var connection = new ClientConnection(new Uri("icerpc://localhost"));
+        // The server uses a test certificate, so we trust its root CA. CreateClientAuthenticationOptions
+        // is a helper from examples/common/Program.Authentication.cs in the icerpc-csharp repo.
+        using var rootCA = X509CertificateLoader.LoadCertificateFromFile("certs/cacert.der");
+
+        await using var connection = new ClientConnection(
+            new Uri("icerpc://localhost"),
+            clientAuthenticationOptions: CreateClientAuthenticationOptions(rootCA));
 
         // Create an invocation pipeline, that uses the deadline interceptor.
         Pipeline pipeline = new Pipeline()
@@ -20,7 +29,13 @@ public static class DeadlineInterceptorExamples
     public static async Task UseDeadlineWithDefaultTimeout()
     {
         #region UseDeadlineWithDefaultTimeout
-        await using var connection = new ClientConnection(new Uri("icerpc://localhost"));
+        // The server uses a test certificate, so we trust its root CA. CreateClientAuthenticationOptions
+        // is a helper from examples/common/Program.Authentication.cs in the icerpc-csharp repo.
+        using var rootCA = X509CertificateLoader.LoadCertificateFromFile("certs/cacert.der");
+
+        await using var connection = new ClientConnection(
+            new Uri("icerpc://localhost"),
+            clientAuthenticationOptions: CreateClientAuthenticationOptions(rootCA));
 
         // Create an invocation pipeline, that uses the deadline interceptor and has a default
         // timeout of 500 ms.
