df8f5b7d6c22c53bb31ef12c2bafe8bc2b722f62
Bernard Normier <bernard@zeroc.com>
2026-09-14 15:06:52 -0400

    Add DispatchException.FromException and use it in both protocol connections (#4956)
    What's Changed entry
    
    Area: Core
    
    - Added `DispatchException.FromException`, which converts an exception
    thrown by a dispatch into the
      `DispatchException` the caller receives.
    - With the ice protocol, a dispatch that throws `NotSupportedException`
    now returns the `NotSupported` reply status
      instead of `UnknownException`.
    
    (cherry picked from commit 329e7c59dab8a64b78111c26f995f23f57ac61fe)

--- src/IceRpc/Internal/IceProtocolConnection.cs
+++ src/IceRpc/Internal/IceProtocolConnection.cs
@@ -1080,13 +1080,13 @@ internal sealed class IceProtocolConnection : IProtocolConnection
         }
         catch (Exception exception)
         {
-            if (exception is not DispatchException dispatchException)
-            {
-                StatusCode statusCode = exception is InvalidDataException ?
-                    StatusCode.InvalidData : StatusCode.InternalError;
-                dispatchException = new DispatchException(statusCode, innerException: exception);
-            }
-            response = dispatchException.ToOutgoingResponse(request);
+            var dispatchException = DispatchException.FromException(exception);
+            Debug.Assert(!dispatchException.ConvertToInternalError);
+            response = new OutgoingResponse(
+                request,
+                dispatchException.StatusCode,
+                dispatchException.Message,
+                dispatchException.InnerException);
         }
         finally
         {
