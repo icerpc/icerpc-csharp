9c3c468d28c9a251d71dc5e03d351c369d04d84a
Jose <jose@zeroc.com>
2026-09-15 14:54:09 +0200

    Document symbol identifier escaping

--- src/IceRpc.ServiceGenerator/Internal/SymbolExtensions.cs
+++ src/IceRpc.ServiceGenerator/Internal/SymbolExtensions.cs
@@ -22,6 +22,8 @@ internal static class SymbolExtensions
         return null;
     }
 
+    /// <summary>Gets the symbol name escaped for use as a C# identifier.</summary>
+    /// <remarks>Roslyn symbol names omit the <c>@</c> prefix used by verbatim identifiers.</remarks>
     internal static string GetEscapedName(this ISymbol symbol) =>
         SyntaxFacts.GetKeywordKind(symbol.Name) != SyntaxKind.None ||
         SyntaxFacts.GetContextualKeywordKind(symbol.Name) != SyntaxKind.None ?
